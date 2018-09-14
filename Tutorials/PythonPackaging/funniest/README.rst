c8cdcd8483db785a5dda95a5fdda81b4fc26f864
Steven Burns <smburns47@gmail.com>
2018-09-14 06:47:52 -0700

    Added README.rst

---
+++ Tutorials/PythonPackaging/funniest/README.rst
@@ -0,0 +1,7 @@
+Funniest
+--------
+
+To use (with caution), simply do::
+
+    >>> import funniest
+    >>> print funniest.joke()
--- Tutorials/PythonPackaging/funniest/setup.py
+++ Tutorials/PythonPackaging/funniest/setup.py
@@ -1,8 +1,20 @@
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name='funniest',
       version='0.1',
       description='The funniest joke in the world',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+      keywords='funniest joke comedy flying circus',
       url='http://github.com/storborg/funniest',
       author='Flying Circus',
       author_email='flyingcircus@example.com',
@@ -11,4 +23,5 @@ setup(name='funniest',
       install_requires=[
           'markdown',
       ],
+      include_package_data=True,
       zip_safe=False)
