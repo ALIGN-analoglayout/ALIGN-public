fc6d14050b2723f585babc58b924141aecd76cbe
Steven Burns <smburns47@gmail.com>
2018-09-21 14:10:15 -0700

    Added a larger example

--- Cktgen/cktgen8.py
+++ Cktgen/cktgen8.py
@@ -35,6 +35,12 @@ if __name__ == "__main__":
   adnetl.addInstance( ADI( ndev, "un1", ADITransform.translate( xg(8), yg(1))))
   adnetl.addInstance( ADI( pdev, "up1", mirrorAcrossYAxis( pdev).preMult( ADITransform.translate( xg(1), yg(8)))))
 
+  adnetl.addInstance( ADI( ndev, "un2", ADITransform.translate( xg(5), yg(1))))
+  adnetl.addInstance( ADI( pdev, "up2", mirrorAcrossYAxis( pdev).preMult( ADITransform.translate( xg(1), yg(5)))))
+
+  adnetl.addInstance( ADI( ndev, "un3", ADITransform.translate( xg(3), yg(4))))
+  adnetl.addInstance( ADI( pdev, "up3", mirrorAcrossYAxis( pdev).preMult( ADITransform.translate( xg(4), yg(3)))))
+
   adnetl.connect('un0','g','i0')
   adnetl.connect('un0','d','o0')
   adnetl.connect('un0','s','vss')
@@ -51,6 +57,22 @@ if __name__ == "__main__":
   adnetl.connect('up1','d','o1')
   adnetl.connect('up1','s','vcc')
 
+  adnetl.connect('un2','g','i2')
+  adnetl.connect('un2','d','o2')
+  adnetl.connect('un2','s','vss')
+
+  adnetl.connect('up2','g','i2')
+  adnetl.connect('up2','d','o2')
+  adnetl.connect('up2','s','vcc')
+
+  adnetl.connect('un3','g','i3')
+  adnetl.connect('un3','d','o3')
+  adnetl.connect('un3','s','vss')
+
+  adnetl.connect('up3','g','i3')
+  adnetl.connect('up3','d','o3')
+  adnetl.connect('up3','s','vcc')
+
   adnetl.genNetlist( netl)
 
   netl.newGR( 'i0', Rect( 1, 8, 8, 8), "metal4", tech.halfWidthM4[0]*2)
@@ -64,6 +86,18 @@ if __name__ == "__main__":
 
   netl.newGR( 'o1', Rect( 1, 1, 1, 8), "metal3", tech.halfWidthM3[0]*2)
   netl.newGR( 'o1', Rect( 1, 1, 8, 1), "metal4", tech.halfWidthM4[0]*2)
+
+  netl.newGR( 'i2', Rect( 1, 5, 5, 5), "metal4", tech.halfWidthM4[0]*2)
+  netl.newGR( 'i2', Rect( 5, 1, 5, 5), "metal3", tech.halfWidthM3[0]*2)
+
+  netl.newGR( 'o2', Rect( 1, 1, 1, 5), "metal3", tech.halfWidthM3[0]*2)
+  netl.newGR( 'o2', Rect( 1, 1, 5, 1), "metal4", tech.halfWidthM4[0]*2)
+
+  netl.newGR( 'i3', Rect( 3, 4, 4, 4), "metal4", tech.halfWidthM4[0]*2)
+  netl.newGR( 'i3', Rect( 4, 3, 4, 4), "metal3", tech.halfWidthM3[0]*2)
+
+  netl.newGR( 'o3', Rect( 3, 3, 3, 4), "metal3", tech.halfWidthM3[0]*2)
+  netl.newGR( 'o3', Rect( 3, 3, 4, 3), "metal4", tech.halfWidthM4[0]*2)
 
   pathlib.Path("INPUT").mkdir(parents=True, exist_ok=True)
 
