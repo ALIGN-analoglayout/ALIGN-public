05647bc95120b34c7d656b482272a09013d36571
Steven Burns <smburns47@gmail.com>
2018-10-19 16:50:17 -0700

    Added a simple antenna clean flow

--- Pysat/global_router.py
+++ Pysat/global_router.py
@@ -51,6 +51,7 @@ class Grid:
         self.ny = ny
         self.nets = OrderedDict()
         self.layers = ['metal2','metal3']
+        self.routes = OrderedDict()
 
     def addTerminal( self, net_nm, x, y):
         if net_nm not in self.nets: self.nets[net_nm] = []
@@ -58,6 +59,31 @@ class Grid:
     
     def idx( self, x, y):
         return self.ny*x + y
+
+    def cleanAntennas( self):
+      
+      print( "Phase 1: cleanAntennas")
+      for x in range(self.nx):
+        for y in range(self.ny):
+          for (k,v) in self.per_net_grid.items():
+            for (ly,bv) in v.items():
+              if self.per_net_grid[k][ly].val( self.idx(x,y)) is False:
+                self.s.emit_never( self.per_net_grid[k][ly].var( self.idx(x,y)))
+      self.s.solve()
+      assert self.s.state == 'SAT'
+
+      print( "Phase 2: cleanAntennas")
+      for x in range(self.nx):
+        for y in range(self.ny):
+          for (k,v) in self.per_net_grid.items():
+            for (ly,bv) in v.items():
+              if self.per_net_grid[k][ly].val( self.idx(x,y)) is True:
+                self.s.solve( assumptions=[-self.per_net_grid[k][ly].var( self.idx(x,y))])
+                if self.s.state == 'SAT':
+                  print( "Removing antenna from %s %s %d %d" % (k,ly,x,y))
+                  self.s.emit_never( self.per_net_grid[k][ly].var( self.idx(x,y)))                           
+      self.s.solve()
+      assert self.s.state == 'SAT'
 
     def genMaxCapacityConstraints( self, max_capacity):
       for x in range(self.nx):
@@ -73,17 +99,15 @@ class Grid:
           for x in range(self.nx):
             for y in range(self.ny):
               for (l,ll) in ( (l, ll) for l in self.layers for ll in self.layers if l != ll):
-                for k in self.nets.keys():
+                for k in self.per_net_grid.keys():
                   inps = [ self.per_net_grid[k ][l ].var( self.idx(x,y))] + \
-                         [ self.per_net_grid[kk][ll].var( self.idx(x,y)) for kk in self.nets.keys() if k != kk]
+                         [ self.per_net_grid[kk][ll].var( self.idx(x,y)) for kk in self.per_net_grid.keys() if k != kk]
                   outs_bv = tally.BitVec( self.s, 'cap2_%s_%s_%s_%d_%d' % (l,ll,k,x,y), max_capacity+1)
                   outs = [ outs_bv.var( i) for i in range( max_capacity+1)]
                   self.s.emit_tally( inps, outs)
                   self.s.emit_never( outs_bv.var( max_capacity))
 
     def genRoutes( self):
-        self.routes = OrderedDict()
-
 # All i,l, and z routes for two terminal nets        
         for (k,v) in self.nets.items():
             assert len(v) == 2
@@ -123,22 +147,101 @@ class Grid:
             self.s.emit_at_least_one( [ bv.var() for bv in self.routes[k]])
 
 
+    def genSymmetricRoutes( self, n0, n1):
+        (k0,v0) = n0
+        (k1,v1) = n1
+
+        assert len(v0) == 2
+        assert len(v1) == 2
+
+        # check assumed mirroring about a vertical line
+
+        # y coords the same
+        assert v0[0][1] == v1[1][1] 
+        assert v0[1][1] == v1[0][1] 
+
+        # x coords flip
+        xdist = v1[1][0] - v0[0][0]
+        assert v0[0][0] == xdist - v1[1][0]
+        assert v0[1][0] == xdist - v1[0][0]
+
+        def allStepX( k, v, kk, vv):
+          x0,y0 = v[0]
+          x1,y1 = v[1]
+
+          if x0 > x1:
+            x0,y0,x1,y1 = x1,y1,x0,y0
+
+          for x in range(x0,x1+1):
+            r = tally.BitVar( self.s, '%s_route_x_%d' % ( k, x))
+            self.routes[k].append( r)
+
+            if x != x0: self.emitWire( k, r, "metal2", x0, y0, x, y0)
+            self.emitWire( k, r, "metal3", x, y0, x, y1)
+            if x != x1: self.emitWire( k, r, "metal2", x, y1, x1, y1)
+
+            if x != x0: self.emitWire( kk, r, "metal2", xdist - x, y0, xdist - x0, y0)
+            self.emitWire( kk, r, "metal3", xdist - x, y0, xdist - x, y1)
+            if x != x1: self.emitWire( kk, r, "metal2", xdist - x1, y1, xdist -x, y1)
+
+        def allStepY( k, v, kk, vv):
+          x0,y0 = v[0]
+          x1,y1 = v[1]
+
+          if y0 > y1:
+            x0,y0,x1,y1 = x1,y1,x0,y0
+
+          for y in range(y0,y1+1):
+            r = tally.BitVar( self.s, '%s_route_y_%d' % ( k, y))
+            self.routes[k].append( r)
+
+            if y != y0: self.emitWire( k, r, "metal3", x0, y0, x0, y)
+            self.emitWire( k, r, "metal2", x0, y, x1, y)
+            if y != y1: self.emitWire( k, r, "metal3", x1, y, x1, y1)
+
+            if y != y0: self.emitWire( kk, r, "metal3", xdist - x0, y0, xdist - x0, y)
+            self.emitWire( kk, r, "metal2", xdist - x1, y, xdist - x0, y)
+            if y != y1: self.emitWire( kk, r, "metal3", xdist - x1, y, xdist - x1, y1)
+
+        self.routes[k0] = []
+
+        allStepX( k0, v0, k1, v1)
+        allStepY( k0, v0, k1, v1)
+        self.s.emit_exactly_one( [ bv.var() for bv in self.routes[k0]])
+
+        self.genRoutes()
+
     def semantic( self, max_capacity=1, different_net_max_capacity=None):
         self.s = tally.Tally() 
 
         self.per_net_grid = OrderedDict()
-        for (k,v) in self.nets.items():
+        for k in list(self.nets.keys()) + ['!kor']:
             self.per_net_grid[k] = OrderedDict()
             for ly in self.layers:
                 self.per_net_grid[k][ly] = tally.BitVec( self.s, k + '_' + ly, self.nx * self.ny)
         
         self.genMaxCapacityConstraints( max_capacity=max_capacity)
         if different_net_max_capacity is not None:
-          self.genDifferentNetMaxCapacityConstaints( different_net_max_capacity)
+          self.genDifferentNetMaxCapacityConstraints( different_net_max_capacity)
 
         self.genRoutes()
 
+    def semanticSymmetric( self, max_capacity=1, different_net_max_capacity=None):
+        self.s = tally.Tally() 
 
+        self.per_net_grid = OrderedDict()
+        for k in list(self.nets.keys()) + ['!kor']:
+            self.per_net_grid[k] = OrderedDict()
+            for ly in self.layers:
+                self.per_net_grid[k][ly] = tally.BitVec( self.s, k + '_' + ly, self.nx * self.ny)
+        
+        self.genMaxCapacityConstraints( max_capacity=max_capacity)
+        if different_net_max_capacity is not None:
+          self.genDifferentNetMaxCapacityConstraints( different_net_max_capacity)
+
+        items = list(self.nets.items())
+        assert len(items) == 2
+        self.genSymmetricRoutes( items[0], items[1])
 
     def emitWire( self, k, r, ly, x0, y0, x1, y1):
         print( ly, x0, y0, x1, y1)
@@ -235,7 +338,7 @@ class Grid:
         fp.write( json.dumps( data, indent=2, default=lambda x: encode_GR(tech,x)) + "\n")
 
 
-def test_river_routing( max_capacity=1):
+def test_river_routing( max_capacity=1, different_net_max_capacity=1):
     halfn = 10
     n = 2*halfn
     g = Grid( n, n)
@@ -243,9 +346,40 @@ def test_river_routing( max_capacity=1):
         g.addTerminal( 'a%d' % q, 0,   q)
         g.addTerminal( 'a%d' % q, n-1, q+halfn)
 
-    g.semantic( max_capacity=max_capacity)
+    g.semantic( max_capacity, different_net_max_capacity)
     g.s.solve()
     assert g.s.state == 'SAT'
+
+    g.cleanAntennas()
+
+    g.print_routes()
+    g.print_rasters()
+    g.genWires()
+
+    return g
+
+def test_symmetric( max_capacity=1, different_net_max_capacity=1):
+    g = Grid( 6, 4)
+    g.addTerminal( 'a', 0, 0)
+    g.addTerminal( 'a', 2, 3)
+    g.addTerminal( 'b', 3, 3)
+    g.addTerminal( 'b', 5, 0)
+
+    g.semanticSymmetric( max_capacity, different_net_max_capacity)
+
+    for ly in g.layers:
+      pass
+#      g.s.emit_always( g.per_net_grid['!kor'][ly].var( g.idx(0,3)))
+#      g.s.emit_always( g.per_net_grid['!kor'][ly].var( g.idx(0,2)))
+#      g.s.emit_always( g.per_net_grid['!kor'][ly].var( g.idx(1,0)))
+      g.s.emit_always( g.per_net_grid['!kor'][ly].var( g.idx(1,1)))
+      g.s.emit_always( g.per_net_grid['!kor'][ly].var( g.idx(1,2)))
+      g.s.emit_always( g.per_net_grid['!kor'][ly].var( g.idx(5,3)))
+
+    g.s.solve()
+    assert g.s.state == 'SAT'
+
+    g.cleanAntennas()
 
     g.print_routes()
     g.print_rasters()
@@ -294,7 +428,8 @@ def test_backward_xy():
     g.genWires()
 
 if __name__ == "__main__":
-    g = test_river_routing(1)
+    g = test_river_routing(1,None)
+#    g = test_symmetric(1)
     with open( "mydesign_dr_globalrouting.json", "wt") as fp:
         tech = Tech()
         g.write_globalrouting_json( fp, tech)
