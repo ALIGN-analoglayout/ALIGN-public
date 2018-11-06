183d7bd8c433be0b9caffe0146f26450b0ff53d8
Steven Burns <smburns47@gmail.com>
2018-11-06 09:52:30 -0800

    Refactored

--- CellFabric/fabric.py
+++ CellFabric/fabric.py
@@ -2,12 +2,72 @@
 import json
 
 def write_global_routing_json( fp, terminals):
-
-
     data = { 'bbox' : [0,0,10000,10000], 'globalRoutes' : [], 'globalRouteGrid' : [], 'terminals' : terminals}
     fp.write( json.dumps( data, indent=2) + '\n')
 
-class UnitCell():
+
+class StopPointGrid:
+    def __init__( self, nm, layer, direction, *, width, pitch, offset=0):
+        self.nm = nm
+        self.layer = layer
+        self.direction = direction
+        assert direction in ['v','h']
+        self.width = width
+        self.pitch = pitch
+        self.offset = offset
+        assert pitch > width > 0
+
+        self.grid = []
+        self.legalStopVector = []
+        self.legalStopIndices = set()
+
+    def addGridPoint( self, value, isLegal):
+        self.grid.append( value)
+        self.legalStopVector.append( isLegal)
+        if isLegal:
+            self.legalStopIndices.add( len(self.grid)-1)
+
+    @property
+    def n( self):
+        return len(self.grid)-1
+
+    def value( self, idx):
+        whole = idx // self.n
+        fract = idx % self.n
+        while fract < 0:
+            whole -= 1
+            fract += self.n
+        assert fract in self.legalStopIndices
+        return whole * self.grid[-1] + self.grid[fract]
+
+    def segment( self, netName, center, bIdx, eIdx):
+        c = center*self.pitch + self.offset
+        c0 = c - self.width//2
+        c1 = c + self.width//2
+        if self.direction == 'h':
+            rect = [ self.value(bIdx), c0, self.value(eIdx), c1]
+        else:
+            rect = [ c0, self.value(bIdx), c1, self.value(eIdx)]
+        return { 'netName' : netName, 'layer' : self.layer, 'rect' : rect}
+
+class UnitCell:
+
+    @property
+    def m0Grid( self): return self.m0.grid
+    @property
+    def m0GridStopIndices( self): return self.m0.legalStopIndices
+    @property
+    def m1Grid( self): return self.m1.grid
+    @property
+    def m1GridStopIndices( self): return self.m1.legalStopIndices
+    @property
+    def polyGrid( self): return self.poly.grid
+    @property
+    def polyGridStopIndices( self): return self.poly.legalStopIndices
+    @property
+    def diffconGrid( self): return self.diffcon.grid
+    @property
+    def diffconGridStopIndices( self): return self.diffcon.legalStopIndices
 
     def __init__( self):
         self.terminals = []
@@ -23,99 +83,53 @@ class UnitCell():
         self.polyWidth = 200
 
         stoppoint = (self.diffconWidth//2 + self.polyOffset-self.polyWidth//2)//2
-        self.m0Grid = [ 0, stoppoint, self.polyOffset, self.diffconPitch-stoppoint, self.diffconPitch]
-        self.m0GridStops = [False,True,False,True,False]
-        self.m0GridStopIndices = set([1,3])
+        self.m0 = StopPointGrid( 'm0', 'metal0', 'h', width=self.m0Width, pitch=self.m0Pitch)
+        self.m0.addGridPoint( 0,                           False)
+        self.m0.addGridPoint( stoppoint,                   True)
+        self.m0.addGridPoint( self.polyOffset,             False)
+        self.m0.addGridPoint( self.diffconPitch-stoppoint, True)
+        self.m0.addGridPoint( self.diffconPitch,           False)
 
         stoppoint = (self.m0Width//2 + self.m0Pitch-self.m0Width//2)//2
-        self.m1Grid = [ 0, stoppoint, 2*self.m0Pitch, 4*self.m0Pitch-stoppoint, 4*self.m0Pitch]
-        self.m1GridStops = [False,True,False,True,False]
-        self.m1GridStopIndices = set([1,3])
+        self.m1 = StopPointGrid( 'm1', 'metal1', 'v', width=self.m1Width, pitch=self.m1Pitch)
+        self.m1.addGridPoint( 0,                        False)
+        self.m1.addGridPoint( stoppoint,                True)
+        self.m1.addGridPoint( 2*self.m0Pitch,           False)
+        self.m1.addGridPoint( 4*self.m0Pitch-stoppoint, True)
+        self.m1.addGridPoint( 4*self.m0Pitch,           False)
 
-        self.polyGrid = [ 0, stoppoint, 2*self.m0Pitch, 4*self.m0Pitch-stoppoint, 4*self.m0Pitch]
-        self.polyGridStops = [False,True,False,True,False]
-        self.polyGridStopIndices = set([1,3])
+        self.poly = StopPointGrid( 'poly', 'poly', 'v', width=self.polyWidth, pitch=self.polyPitch, offset=self.polyOffset)
+        self.poly.addGridPoint( 0,                        False)
+        self.poly.addGridPoint( stoppoint,                True)
+        self.poly.addGridPoint( 2*self.m0Pitch,           False)
+        self.poly.addGridPoint( 4*self.m0Pitch-stoppoint, True)
+        self.poly.addGridPoint( 4*self.m0Pitch,           False)
 
-        self.diffconGrid = [ 0, stoppoint, 2*self.m0Pitch, 4*self.m0Pitch-stoppoint, 4*self.m0Pitch]
-        self.diffconGridStops = [False,True,False,True,False]
-        self.diffconGridStopIndices = set([1,3])
+        self.diffcon = StopPointGrid( 'diffcon', 'diffcon', 'v', width=self.diffconWidth, pitch=self.diffconPitch)
+        self.diffcon.addGridPoint( 0,                        False)
+        self.diffcon.addGridPoint( stoppoint,                True)
+        self.diffcon.addGridPoint( 2*self.m0Pitch,           False)
+        self.diffcon.addGridPoint( 4*self.m0Pitch-stoppoint, True)
+        self.diffcon.addGridPoint( 4*self.m0Pitch,           False)
         
 
     @property
-    def polyOffset( self):
-        return self.polyPitch // 2
+    def polyOffset( self): return self.polyPitch // 2
 
-    def m0x( self, x):
-        xWhole = x // (len(self.m0Grid)-1)
-        xFract = x % (len(self.m0Grid)-1)
-        while xFract < 0:
-            xWhole -= 1
-            xFract += len(self.m0Grid)-1
-        assert xFract in self.m0GridStopIndices
-        return xWhole * self.m0Grid[-1] + self.m0Grid[xFract]
+    def m0x( self, x): return self.m0.value( x)
+    def m1y( self, y): return self.m1.value( y)
+    def polyy( self, y): return self.poly.value( y)
+    def diffcony( self, y): return self.diffcon.value( y)
 
-    def m1y( self, y):
-        yWhole = y // (len(self.m1Grid)-1)
-        yFract = y % (len(self.m1Grid)-1)
-        while yFract < 0:
-            yWhole -= 1
-            yFract += len(self.m1Grid)-1
-        assert yFract in self.m1GridStopIndices
-        return yWhole * self.m1Grid[-1] + self.m1Grid[yFract]
-
-    def polyy( self, y):
-        yWhole = y // (len(self.polyGrid)-1)
-        yFract = y % (len(self.polyGrid)-1)
-        while yFract < 0:
-            yWhole -= 1
-            yFract += len(self.polyGrid)-1
-        assert yFract in self.polyGridStopIndices
-        return yWhole * self.polyGrid[-1] + self.polyGrid[yFract]
-
-    def diffcony( self, y):
-        yWhole = y // (len(self.diffconGrid)-1)
-        yFract = y % (len(self.diffconGrid)-1)
-        while yFract < 0:
-            yWhole -= 1
-            yFract += len(self.diffconGrid)-1
-        assert yFract in self.diffconGridStopIndices
-        return yWhole * self.diffconGrid[-1] + self.diffconGrid[yFract]
-
-    def m0Segment( self, netName, x0, x1, y):
-        yc = y*self.m0Pitch
-        y0 = yc - self.m0Width//2
-        y1 = yc + self.m0Width//2
-        rect = [ self.m0x(x0), y0, self.m0x(x1), y1]
-        segment = { 'netName' : netName, 'layer' : 'metal0', 'rect' : rect}
+    def addSegment( self, grid, netName, c, bIdx, eIdx):
+        segment = grid.segment( netName, c, bIdx, eIdx)
         self.terminals.append( segment)
         return segment
-
-    def m1Segment( self, netName, x, y0, y1):
-        xc = x*self.m1Pitch
-        x0 = xc - self.m1Width//2
-        x1 = xc + self.m1Width//2
-        rect = [x0, self.m1y(y0), x1, self.m1y(y1)]
-        segment = { 'netName' : netName, 'layer' : 'metal1', 'rect' : rect}
-        self.terminals.append( segment)
-        return segment
-
-    def polySegment( self, netName, x, y0, y1):
-        xc = x*self.polyPitch + self.polyOffset
-        x0 = xc - self.polyWidth//2
-        x1 = xc + self.polyWidth//2
-        rect = [x0, self.polyy(y0), x1, self.polyy(y1)]
-        segment = { 'netName' : netName, 'layer' : 'poly', 'rect' : rect}
-        self.terminals.append( segment)
-        return segment
-
-    def diffconSegment( self, netName, x, y0, y1):
-        xc = x*self.diffconPitch
-        x0 = xc - self.diffconWidth//2
-        x1 = xc + self.diffconWidth//2
-        rect = [x0, self.diffcony(y0), x1, self.diffcony(y1)]
-        segment = { 'netName' : netName, 'layer' : 'diffcon', 'rect' : rect}
-        self.terminals.append( segment)
-        return segment
+        
+    def m0Segment( self, netName, x0, x1, y): return self.addSegment( self.m0, netName, y, x0, x1)
+    def m1Segment( self, netName, x, y0, y1): return self.addSegment( self.m1, netName, x, y0, y1)
+    def polySegment( self, netName, x, y0, y1): return self.addSegment( self.poly, netName, x, y0, y1)
+    def diffconSegment( self, netName, x, y0, y1): return self.addSegment( self.diffcon, netName, x, y0, y1)
 
     def unit( self, x, y):
         uc.diffconSegment( 's',   2*x+0, 4*y+1, 4*y+3)
