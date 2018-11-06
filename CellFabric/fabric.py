7cac09dc476199d6108d484942cbf300e8bfb1cb
Steven Burns <smburns47@gmail.com>
2018-11-06 10:40:03 -0800

    More refactoring and renaming

--- CellFabric/fabric.py
+++ CellFabric/fabric.py
@@ -48,23 +48,6 @@ class StopPointGrid:
 
 class UnitCell:
 
-    @property
-    def m0Grid( self): return self.m0.grid
-    @property
-    def m0GridStopIndices( self): return self.m0.legalStopIndices
-    @property
-    def m1Grid( self): return self.m1.grid
-    @property
-    def m1GridStopIndices( self): return self.m1.legalStopIndices
-    @property
-    def polyGrid( self): return self.poly.grid
-    @property
-    def polyGridStopIndices( self): return self.poly.legalStopIndices
-    @property
-    def diffconGrid( self): return self.diffcon.grid
-    @property
-    def diffconGridStopIndices( self): return self.diffcon.legalStopIndices
-
     def computeBbox( self):
         self.bbox = transformation.Rect(None,None,None,None)
         for term in self.terminals:
@@ -77,49 +60,48 @@ class UnitCell:
     def __init__( self):
         self.terminals = []
 
-        self.m0Pitch    = 360
-        self.m1Pitch    = 720 
-        self.polyPitch  = 720
-        self.diffconPitch  = 720
+        m0Pitch  = 360
+        m1Pitch  = 720 
+        plPitch  = 720
+        plOffset = plPitch//2
+        dcPitch  = 720
 
-        self.m1Width = 400
-        self.m0Width = 200
-        self.diffconWidth = 200
-        self.polyWidth = 200
+        assert dcPitch == plPitch
 
-        stoppoint = (self.diffconWidth//2 + self.polyOffset-self.polyWidth//2)//2
-        self.m0 = StopPointGrid( 'm0', 'metal0', 'h', width=self.m0Width, pitch=self.m0Pitch)
-        self.m0.addGridPoint( 0,                           False)
-        self.m0.addGridPoint( stoppoint,                   True)
-        self.m0.addGridPoint( self.polyOffset,             False)
-        self.m0.addGridPoint( self.diffconPitch-stoppoint, True)
-        self.m0.addGridPoint( self.diffconPitch,           False)
+        m1Width = 400
+        m0Width = 200
+        dcWidth = 200
+        plWidth = 200
 
-        stoppoint = (self.m0Width//2 + self.m0Pitch-self.m0Width//2)//2
-        self.m1 = StopPointGrid( 'm1', 'metal1', 'v', width=self.m1Width, pitch=self.m1Pitch)
-        self.m1.addGridPoint( 0,                        False)
-        self.m1.addGridPoint( stoppoint,                True)
-        self.m1.addGridPoint( 2*self.m0Pitch,           False)
-        self.m1.addGridPoint( 4*self.m0Pitch-stoppoint, True)
-        self.m1.addGridPoint( 4*self.m0Pitch,           False)
+        stoppoint = (dcWidth//2 + plOffset-plWidth//2)//2
+        self.m0 = StopPointGrid( 'm0', 'metal0', 'h', width=m0Width, pitch=m0Pitch)
+        self.m0.addGridPoint( 0,                 False)
+        self.m0.addGridPoint( stoppoint,         True)
+        self.m0.addGridPoint( plOffset,          False)
+        self.m0.addGridPoint( dcPitch-stoppoint, True)
+        self.m0.addGridPoint( dcPitch,           False)
 
-        self.poly = StopPointGrid( 'poly', 'poly', 'v', width=self.polyWidth, pitch=self.polyPitch, offset=self.polyOffset)
-        self.poly.addGridPoint( 0,                        False)
-        self.poly.addGridPoint( stoppoint,                True)
-        self.poly.addGridPoint( 2*self.m0Pitch,           False)
-        self.poly.addGridPoint( 4*self.m0Pitch-stoppoint, True)
-        self.poly.addGridPoint( 4*self.m0Pitch,           False)
+        stoppoint = (m0Width//2 + m0Pitch-m0Width//2)//2
+        self.m1 = StopPointGrid( 'm1', 'metal1', 'v', width=m1Width, pitch=m1Pitch)
+        self.m1.addGridPoint( 0,                   False)
+        self.m1.addGridPoint( stoppoint,           True)
+        self.m1.addGridPoint( 2*m0Pitch,           False)
+        self.m1.addGridPoint( 4*m0Pitch-stoppoint, True)
+        self.m1.addGridPoint( 4*m0Pitch,           False)
 
-        self.diffcon = StopPointGrid( 'diffcon', 'diffcon', 'v', width=self.diffconWidth, pitch=self.diffconPitch)
-        self.diffcon.addGridPoint( 0,                        False)
-        self.diffcon.addGridPoint( stoppoint,                True)
-        self.diffcon.addGridPoint( 2*self.m0Pitch,           False)
-        self.diffcon.addGridPoint( 4*self.m0Pitch-stoppoint, True)
-        self.diffcon.addGridPoint( 4*self.m0Pitch,           False)
-        
+        self.pl = StopPointGrid( 'pl', 'poly', 'v', width=plWidth, pitch=plPitch, offset=plOffset)
+        self.pl.addGridPoint( 0,                   False)
+        self.pl.addGridPoint( stoppoint,           True)
+        self.pl.addGridPoint( 2*m0Pitch,           False)
+        self.pl.addGridPoint( 4*m0Pitch-stoppoint, True)
+        self.pl.addGridPoint( 4*m0Pitch,           False)
 
-    @property
-    def polyOffset( self): return self.polyPitch // 2
+        self.dc = StopPointGrid( 'dc', 'diffcon', 'v', width=dcWidth, pitch=dcPitch)
+        self.dc.addGridPoint( 0,                   False)
+        self.dc.addGridPoint( stoppoint,           True)
+        self.dc.addGridPoint( 2*m0Pitch,           False)
+        self.dc.addGridPoint( 4*m0Pitch-stoppoint, True)
+        self.dc.addGridPoint( 4*m0Pitch,           False)
 
     def addSegment( self, grid, netName, c, bIdx, eIdx):
         segment = grid.segment( netName, c, bIdx, eIdx)
@@ -128,18 +110,18 @@ class UnitCell:
         
     def m0Segment( self, netName, x0, x1, y): return self.addSegment( self.m0, netName, y, x0, x1)
     def m1Segment( self, netName, x, y0, y1): return self.addSegment( self.m1, netName, x, y0, y1)
-    def polySegment( self, netName, x, y0, y1): return self.addSegment( self.poly, netName, x, y0, y1)
-    def diffconSegment( self, netName, x, y0, y1): return self.addSegment( self.diffcon, netName, x, y0, y1)
+    def plSegment( self, netName, x, y0, y1): return self.addSegment( self.pl, netName, x, y0, y1)
+    def dcSegment( self, netName, x, y0, y1): return self.addSegment( self.dc, netName, x, y0, y1)
 
     def unit( self, x, y):
         ny = 4
         nx = 8
         ncx = 2
         ncy = 4
-        uc.diffconSegment( 's',   ncx*(x+0)+0, ny*(y+0)+1, ny*(y+1)-1)
-        uc.polySegment(    'g',   ncx*(x+0)+0, ny*(y+0)+1, ny*(y+1)-1)
-        uc.polySegment(    'g',   ncx*(x+0)+1, ny*(y+0)+1, ny*(y+1)-1)
-        uc.diffconSegment( 'd',   ncx*(x+1)+0, ny*(y+0)+1, ny*(y+1)-1)
+        uc.dcSegment( 's', ncx*(x+0)+0, ny*(y+0)+1, ny*(y+1)-1)
+        uc.plSegment( 'g', ncx*(x+0)+0, ny*(y+0)+1, ny*(y+1)-1)
+        uc.plSegment( 'g', ncx*(x+0)+1, ny*(y+0)+1, ny*(y+1)-1)
+        uc.dcSegment( 'd', ncx*(x+1)+0, ny*(y+0)+1, ny*(y+1)-1)
 
         uc.m0Segment( 's', nx*(x+0)-3, nx*(x+0)+3, ncy*(y+0)+1)
         uc.m0Segment( 's', nx*(x+0)-3, nx*(x+0)+3, ncy*(y+1)-1)
