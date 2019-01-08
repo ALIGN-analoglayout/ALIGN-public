5954dae600828702675af723612511339daa3b6c
Steven Burns <smburns47@gmail.com>
2019-01-08 14:58:17 -0800

    bottom-up.sh runs correctly from a blank volume

--- Pysat/bottom-up.sh
+++ Pysat/bottom-up.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-for i in dp1x dp2x dp4x mirrors wcap comp diff lane top
+for i in dp1x dp2x dp4x mirrors
 do
-    python ${i}.py && ./flow-${i}.sh
-    docker run --mount source=equalizerInputVol,target=/INPUT ubuntu bash -c 'cd /INPUT && tar cvf - ${i}_interface.json' | tar xvf -
+    ./flow-${i}.sh
+    docker run --mount source=equalizerInputVol,target=/INPUT ubuntu bash -c "cd /INPUT && tar cvf - ${i}_interface.json" | tar xvf -
+done
+
+for i in ctle wcap comp diff lane top
+do
+    python ${i}.py
+    ./flow-${i}.sh
+    docker run --mount source=equalizerInputVol,target=/INPUT ubuntu bash -c "cd /INPUT && tar cvf - ${i}_interface.json" | tar xvf -
 done
 
 
