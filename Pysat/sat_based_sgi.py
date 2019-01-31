7f56e1d15b7700e92d0cbef3d2609468cd8c84cb
Steve Burns <smburns47@gmail.com>
2019-01-31 12:47:59 -0800

    Added a larger testcase

--- Pysat/sat_based_sgi.py
+++ Pysat/sat_based_sgi.py
@@ -5,10 +5,10 @@ import networkx.algorithms.isomorphism
 def test_monomorphic_but_not_isomorphic():
     g = nx.Graph()
     g.add_nodes_from( [0, 1, 2])
-    g.add_edges_from( [(0,1), (0,2)])
+    g.add_edges_from( [(0,1), (0,2), (1,2)])
     h = nx.Graph()
     h.add_nodes_from( [0, 1, 2])
-    h.add_edges_from( [(0,1), (0,2), (1,2)])
+    h.add_edges_from( [(0,1), (0,2)])
 
     m = networkx.algorithms.isomorphism.GraphMatcher( g, h)
     assert not m.subgraph_is_isomorphic()
@@ -77,6 +77,28 @@ def gen_mirror_bank( nmirrors):
         
     return h
 
+def gen_mirror_bank2( nmirrors):
+    h = nx.Graph()
+
+    node_count = 1 + 4 * (1 + nmirrors)
+    
+    h.add_nodes_from( list(range(node_count)))
+
+    device_offset = 1 + 2 * (1 + nmirrors)
+    # ref
+    h.add_edges_from( [(0,device_offset), (1,device_offset),   (2,device_offset),
+                                          (1,device_offset+1), (2,device_offset+1)])
+
+    # mirrors
+    for i in range(nmirrors):
+        h.add_edges_from( [(0,device_offset+2*(1+i)),
+                           (2,device_offset+2*(1+i)),
+                           (3+2*i,device_offset+2*(1+i)),
+                           (3+2*i,device_offset+2*(1+i)+1),
+                           (4+2*i,device_offset+2*(1+i)+1)])
+        
+    return h
+
 def test_ssm_mirrors():
     g = gen_mirror_bank( 20)
     h = gen_mirror_bank( 19)
@@ -85,5 +107,16 @@ def test_ssm_mirrors():
 def test_networkx_sgi_mirrors():
     g = gen_mirror_bank( 20)
     h = gen_mirror_bank( 19)
+    m = networkx.algorithms.isomorphism.GraphMatcher( g, h)
+    assert m.subgraph_is_isomorphic()
+
+def test_ssm_mirrors2():
+    g = gen_mirror_bank2( 20)
+    h = gen_mirror_bank2( 19)
+    assert sat_subgraph_monomorphism(g,h)
+
+def test_networkx_sgi_mirrors2():
+    g = gen_mirror_bank2( 20)
+    h = gen_mirror_bank2( 19)
     m = networkx.algorithms.isomorphism.GraphMatcher( g, h)
     assert m.subgraph_is_isomorphic()
