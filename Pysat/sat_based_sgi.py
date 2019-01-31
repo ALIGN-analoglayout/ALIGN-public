c861e4f07af40fe13b8dc3151a822ead3855f121
Steve Burns <smburns47@gmail.com>
2019-01-31 12:34:34 -0800

    Changed g and h around to make interface compatible with networkx

--- Pysat/sat_based_sgi.py
+++ Pysat/sat_based_sgi.py
@@ -14,32 +14,32 @@ def test_monomorphic_but_not_isomorphic():
     assert not m.subgraph_is_isomorphic()
 
 def sat_subgraph_monomorphism( g, h):
-    # g is a subgraph h
-    # each vertex in g maps to some vertex in h
+    # h is a subgraph g
+    # each vertex in h maps to some vertex in g
     # set up two dimensional array
 
     s = tally.Tally()
     mgr = tally.VarMgr( s)
 
     lst = []
-    for n in h.nodes:
-        lst.append( mgr.add_var( tally.BitVec( s, str(n), len(g.nodes))))
+    for n in g.nodes:
+        lst.append( mgr.add_var( tally.BitVec( s, str(n), len(h.nodes))))
         s.emit_at_most_one( lst[-1].vars)
 
-    for (idx,n) in enumerate(g.nodes):
+    for (idx,n) in enumerate(h.nodes):
         l = []
         for bv in lst:
             l.append( bv.vars[idx])
         s.emit_exactly_one( l)
 
-    for eg in g.edges:
-        # if eg in g.edges, then map(eg) must be in h.edges
+    for eh in h.edges:
+        # if eh in h.edges, then map(eh) must be in g.edges
         lits = []
-        for eh in h.edges:
+        for eg in g.edges:
             lits.append( s.add_var())
-            s.emit_and( [ lst[eh[0]].vars[eg[0]], lst[eh[1]].vars[eg[1]]], lits[-1])
+            s.emit_and( [ lst[eg[0]].vars[eh[0]], lst[eg[1]].vars[eh[1]]], lits[-1])
             lits.append( s.add_var())
-            s.emit_and( [ lst[eh[1]].vars[eg[0]], lst[eh[0]].vars[eg[1]]], lits[-1])
+            s.emit_and( [ lst[eg[1]].vars[eh[0]], lst[eg[0]].vars[eh[1]]], lits[-1])
         s.add_clause( lits)
 
     s.solve()
@@ -53,36 +53,37 @@ def sat_subgraph_monomorphism( g, h):
 def test_ssm():
     g = nx.Graph()
     g.add_nodes_from( [0, 1, 2])
-    g.add_edges_from( [(0,1),(0,2)])
-    h = nx.Graph()
-    h.add_nodes_from( [0, 1, 2])
-    h.add_edges_from( [(0,1),(0,2),(1,2)])
-
-    assert sat_subgraph_monomorphism(g,h)
-
-def test_ssm_mirrors():
-    g = nx.Graph()
-    g.add_nodes_from( [0])
-    g.add_edges_from( [])
-    h = nx.Graph()
-    h.add_nodes_from( [0, 1, 2, 3, 4, 5, 6, 7, 8])
-    h.add_edges_from( [(0,5),(0,6),(0,7),(0,8),(1,5),(1,6),(1,7),(1,8),(2,6),(3,7),(4,8)])
-
-    assert sat_subgraph_monomorphism(g,h)
-
-
-if __name__ == "__main__":
-    g = nx.Graph()
-    g.add_nodes_from( [0, 1, 2])
     g.add_edges_from( [(0,1),(0,2),(1,2)])
     h = nx.Graph()
-    h.add_nodes_from( ['a', 'b', 'c'])
-    h.add_edges_from( [('a','b'), ('a','c')])
+    h.add_nodes_from( [0, 1, 2])
+    h.add_edges_from( [(0,1),(0,2)])
 
+    assert sat_subgraph_monomorphism(g,h)
+
+def gen_mirror_bank( nmirrors):
+    h = nx.Graph()
+
+    node_count = 1 + 2 * (1 + nmirrors)
+    
+    h.add_nodes_from( list(range(node_count)))
+
+    device_offset = 1 + 1 + nmirrors
+    # ref
+    h.add_edges_from( [(0,device_offset), (1,device_offset)])
+
+    # mirrors
+    for i in range(nmirrors):
+        h.add_edges_from( [(0,device_offset+1+i),(1,device_offset+1+i),(2+i,device_offset+1+i)])
+        
+    return h
+
+def test_ssm_mirrors():
+    g = gen_mirror_bank( 20)
+    h = gen_mirror_bank( 19)
+    assert sat_subgraph_monomorphism(g,h)
+
+def test_networkx_sgi_mirrors():
+    g = gen_mirror_bank( 20)
+    h = gen_mirror_bank( 19)
     m = networkx.algorithms.isomorphism.GraphMatcher( g, h)
-    print( m.subgraph_is_isomorphic())
-
-    #
-    # Can't currently check monomorphisms:
-    #    So this is false
-    #
+    assert m.subgraph_is_isomorphic()
