055aebf51aade949b0465906e5c02f94ee04fc2a
Steve Burns <smburns47@gmail.com>
2019-02-01 13:27:45 -0800

    Trying to test results

--- Pysat/sat_based_sgi.py
+++ Pysat/sat_based_sgi.py
@@ -48,13 +48,18 @@ def sat_subgraph_monomorphism( g, h):
         for bv in lst:
             print( bv.val())
 
-        for (idx,bv) in enumerate(lst):
+    if False:
+        res_tbl = []
+
+        for (idx,n) in enumerate(h.nodes):
             res = None
-            for i in range(len(h.nodes)):
-                if bv.val(i):
-                    res = i
-            if res is not None:
-                print( idx, res)
+            for (jdx,bv) in enumerate(lst):
+                if bv.val[idx]:
+                    res = jdx
+            assert res is not None
+            res_tbl.append( res)
+
+        print( res_tbl)
 
 
     return s.state == 'SAT'
@@ -154,3 +159,28 @@ def test_pickled_files():
 
 
     assert sat_subgraph_monomorphism(gg,hh)
+
+def xxx_test_pickled_files_nx():
+    g = nx.read_gpickle( "__G1")
+    h = nx.read_gpickle( "__G2")
+
+    def abstract_graph( g):
+        tbl = {}
+        for (idx,n) in enumerate(g.nodes):
+            tbl[n] = idx
+
+        gg = nx.Graph()
+        gg.add_nodes_from( range(len(g.nodes)))
+
+        es = set( (tbl[e[0]],tbl[e[1]]) for e in g.edges)
+        gg.add_edges_from( list(es))
+        return gg
+
+    gg = abstract_graph( g)
+    hh = abstract_graph( h)
+
+    print( len(g.nodes), len(g.edges), len(gg.edges))
+
+
+    m = networkx.algorithms.isomorphism.GraphMatcher( g, h)
+    assert m.subgraph_is_isomorphic()
