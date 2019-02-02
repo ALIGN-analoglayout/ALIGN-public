728a8b3cbf34b7d19fb332db09eebd5fd78ec62b
Steven Burns <smburns47@gmail.com>
2019-02-02 13:29:24 -0800

    Don't need to copy interface

--- Pysat/bottom-up.sh
+++ Pysat/bottom-up.sh
@@ -3,13 +3,11 @@
 for i in dp1x dp2x dp4x mirrors
 do
     ./flow-${i}.sh
-    docker run --mount source=equalizerInputVol,target=/INPUT ubuntu bash -c "cd /INPUT && tar cvf - ${i}_interface.json" | tar xvf -
 done
 
 for i in ctle wcap comp diff lane top
 do
     ./flow-${i}.sh --script ${i}.py
-    docker run --mount source=equalizerInputVol,target=/INPUT ubuntu bash -c "cd /INPUT && tar cvf - ${i}_interface.json" | tar xvf -
 done
 
 
