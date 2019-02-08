6830a6b0742958e0c4a70ef3d88176ebf2a10d76
Steven Burns <smburns47@gmail.com>
2019-02-08 14:21:57 -0800

    Move satplacer to specific setup directory

---
+++ Pysat/satplacer/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='satplacer',
+      version='0.1',
+      description='Simple SAT-based Placer',
+      url='ALIGN-analoglayout/ALIGN.git',
+      author='Steven Burns',
+      author_email='steven.m.burns@intel.com',
+      license='MIT',
+      packages=[],
+      zip_safe=False)
