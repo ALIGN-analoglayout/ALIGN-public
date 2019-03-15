5c0bbc47ae2ab744939992b5fb096f8ebc1d7b20
Steven Burns <smburns47@gmail.com>
2019-03-15 14:25:49 -0700

    Experiment with c++ coverage

--- Build/src/json/test.cpp
+++ Build/src/json/test.cpp
@@ -7,46 +7,55 @@ namespace {
 
   TEST( JSONTestcase, Equals) {
 // create an empty structure (null)
-json j;
+    json j;
 
 // add a number that is stored as double (note the implicit conversion of j to an object)
-j["pi"] = 3.141;
+    j["pi"] = 3.141;
 
 // add a Boolean that is stored as bool
-j["happy"] = true;
+    j["happy"] = true;
 
 // add a string that is stored as std::string
-j["name"] = "Niels";
+    j["name"] = "Niels";
 
 // add another null object by passing nullptr
-j["nothing"] = nullptr;
+    j["nothing"] = nullptr;
 
 // add an object inside the object
-j["answer"]["everything"] = 42;
+    j["answer"]["everything"] = 42;
 
 // add an array that is stored as std::vector (using an initializer list)
-j["list"] = { 1, 0, 2 };
+    j["list"] = { 1, 0, 2 };
 
 // add another object (using an initializer list of pairs)
-j["object"] = { {"currency", "USD"}, {"value", 42.99} };
+    j["object"] = { {"currency", "USD"}, {"value", 42.99} };
 
 // instead, you could also write (which looks very similar to the JSON above)
-json j2 = {
-  {"pi", 3.141},
-  {"happy", true},
-  {"name", "Niels"},
-  {"nothing", nullptr},
-  {"answer", {
-    {"everything", 42}
-  }},
-  {"list", {1, 0, 2}},
-  {"object", {
-    {"currency", "USD"},
-    {"value", 42.99}
-  }}
-};
+    json j2 = {
+      {"pi", 3.141},
+      {"happy", true},
+      {"name", "Niels"},
+      {"nothing", nullptr},
+      {"answer", {
+	  {"everything", 42}
+	}},
+      {"list", {1, 0, 2}},
+      {"object", {
+	  {"currency", "USD"},
+	  {"value", 42.99}
+	}}
+    };
 
- EXPECT_EQ( j, j2);
-}
+    EXPECT_EQ( j, j2);
+  }
+
+  TEST( JSONTestcase, HierObject) {
+    json j;
+    j["hi"] = { {"one", 1}, {"two", 2}};
+
+    EXPECT_EQ( j["hi"]["one"], 1);
+    EXPECT_EQ( j["hi"]["two"], 2);
+
+  }
 
 }
