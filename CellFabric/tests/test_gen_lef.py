62848b4776f8d95469ad57d7743a4dbe8f3b245a
Steven Burns <smburns47@gmail.com>
2019-06-07 09:53:39 -0700

    Better pin handling -- LEF produces pins

--- CellFabric/tests/test_gen_lef.py
+++ CellFabric/tests/test_gen_lef.py
@@ -13,7 +13,7 @@ def test_lef():
 
     with open( json_file_name + "_cand", "rt") as fp0, \
          open( "tests/foo.lef_cand", 'wt') as fp1:
-        cell_fabric.gen_lef.gen_lef_json_fp( fp0, fp1, macro_name=block_name, cell_pin=['G','DA','DB','SA','SB'])
+        cell_fabric.gen_lef.gen_lef_json_fp( fp0, fp1, macro_name=block_name, cell_pin=['DA','DB','S'])
 
     assert filecmp.cmp( "tests/foo.lef_cand", "tests/foo.lef_gold")
 
