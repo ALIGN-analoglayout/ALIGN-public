f0c00f3a5bee6003bdf2978eea8a235738de956d
Parijat Mukherjee <parijat.mukherjee@intel.com>
2019-06-10 14:03:39 -0700

    [drc/test] Move all generator defs to fixture

--- CellFabric/tests/test_canvas_drc.py
+++ CellFabric/tests/test_canvas_drc.py
@@ -1,77 +1,72 @@
 import pytest
 
-from cell_fabric import Canvas, Pdk, Wire
+from cell_fabric import Canvas, Pdk, Wire, Via
 
 @pytest.fixture
 def setup():
     p = Pdk().load('../PDK_Abstraction/FinFET14nm_Mock_PDK/FinFET_Mock_PDK_Abstraction.json')
     c = Canvas(p)
+    c.addGen( Wire( nm='m1', layer='M1', direction='v', clg=None, spg=None))
+    c.addGen( Wire( nm='m2', layer='M2', direction='h', clg=None, spg=None))
+    c.addGen( Via( nm="v1", layer="V1", h_clg=None, v_clg=None))
     return c
 
-def test_min_length_pass_m1(setup):
+def test_min_length_pass_v(setup):
     c = setup
-    c.addGen( Wire( nm='m1', layer='M1', direction='v', clg=None, spg=None))
     # L(300) > MinL(180)
     c.terminals = [{'layer': 'M1', 'netName': 'x', 'rect': [0, 0, 100, 300]}]
     c.gen_data()
     assert c.drc.num_errors == 0
 
-def test_min_length_pass_m2(setup):
+def test_min_length_pass_h(setup):
     c = setup
-    c.addGen( Wire( nm='m2', layer='M2', direction='h', clg=None, spg=None))
     # L(300) > MinL(200)
     c.terminals = [{'layer': 'M2', 'netName': 'x', 'rect': [0, 0, 300, 100]}]
     c.gen_data()
     assert c.drc.num_errors == 0
 
-def test_min_length_fail_m1(setup):
+def test_min_length_fail_v(setup):
     c = setup
-    c.addGen( Wire( nm='m1', layer='M1', direction='v', clg=None, spg=None))
     # L(175) < MinL(180)
     c.terminals = [{'layer': 'M1', 'netName': 'x', 'rect': [0, 0, 100, 175]}]
     c.gen_data()
     assert c.drc.num_errors == 1
 
-def test_min_length_fail_m2(setup):
+def test_min_length_fail_h(setup):
     c = setup
-    c.addGen( Wire( nm='m2', layer='M2', direction='h', clg=None, spg=None))
     # L(190) < MinL(200)
     c.terminals = [{'layer': 'M2', 'netName': 'x', 'rect': [0, 0, 190, 100]}]
     c.gen_data()
     assert c.drc.num_errors == 1
 
-def test_min_spacing_pass_m1(setup):
+def test_min_spacing_pass_v(setup):
     c = setup
-    c.addGen( Wire( nm='m1', layer='M1', direction='v', clg=None, spg=None))
     # space(50) > End-To-End(48)
     c.terminals = [{'layer': 'M1', 'netName': 'x', 'rect': [0, 0, 100, 300]},
                    {'layer': 'M1', 'netName': 'x', 'rect': [0, 350, 100, 650]}]
     c.gen_data()
     assert c.drc.num_errors == 0
 
-def test_min_spacing_pass_m2(setup):
+def test_min_spacing_pass_h(setup):
     c = setup
-    c.addGen( Wire( nm='m2', layer='M2', direction='h', clg=None, spg=None))
     # space(50) > End-To-End(48)
     c.terminals = [{'layer': 'M2', 'netName': 'x', 'rect': [  0, -50, 200, 50]},
                    {'layer': 'M2', 'netName': 'x', 'rect': [250, -50, 600, 50]}]
     c.gen_data()
     assert c.drc.num_errors == 0
 
-def test_min_spacing_fail_m1(setup):
+def test_min_spacing_fail_v(setup):
     c = setup
-    c.addGen( Wire( nm='m1', layer='M1', direction='v', clg=None, spg=None))
     # space(40) < End-To-End(48)
     c.terminals = [{'layer': 'M1', 'netName': 'x', 'rect': [0, 0, 100, 300]},
                    {'layer': 'M1', 'netName': 'x', 'rect': [0, 340, 100, 640]}]
     c.gen_data()
     assert c.drc.num_errors == 1
 
-def test_min_spacing_fail_m2(setup):
+def test_min_spacing_fail_h(setup):
     c = setup
-    c.addGen( Wire( nm='m2', layer='M2', direction='h', clg=None, spg=None))
     # space(40) < End-To-End(48)
     c.terminals = [{'layer': 'M2', 'netName': 'x', 'rect': [  0, -50, 200, 50]},
-                   {'layer': 'M2', 'netName': 'x', 'rect': [240, -50, 600, 50]}] #End-To-End is 48
+                   {'layer': 'M2', 'netName': 'x', 'rect': [240, -50, 600, 50]}]
     c.gen_data()
     assert c.drc.num_errors == 1
