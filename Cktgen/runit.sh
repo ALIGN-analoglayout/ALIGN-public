5776639b3667650660b6b2ca2e5d0f214753f28a
Steven Burns <smburns47@gmail.com>
2019-06-28 14:54:12 -0700

    Should not break old stuff

---
+++ Cktgen/runit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+(cd ../Pysat/satplacer && pytest --capture=no tests/test_lef_v_to_cktgen.py)
+(cd cktgen && cp ../../Pysat/satplacer/tests/__json_cktgen_physical_vga .)
+
+docker run --mount source=equalizerInputVol,target=/INPUT --name sam ubuntu
+docker cp cktgen/__json_cktgen_physical_vga sam:INPUT
+docker rm sam
+
+docker build -t cktgen .
+
+./flow.sh -sgr -p 8086 -iv equalizerInputVol -ov equalizerOutputVol -s "-m cktgen.cktgen_physical_from_json" -src vga -td ../DetailedRouter/DR_COLLATERAL_Generator/hack84 --placer_json INPUT/__json_cktgen_physical_vga --small --no_interface
+
