942a8b216bda7365ffa46ee2d33abeb03eaed361
Steven Burns <smburns47@gmail.com>
2019-07-02 09:40:36 -0700

    Playing with my own global router

--- Cktgen/runit.sh
+++ Cktgen/runit.sh
@@ -5,9 +5,9 @@
 
 docker run --mount source=equalizerInputVol,target=/INPUT --name sam ubuntu
 docker cp cktgen/tests/__json_cktgen_physical_vga sam:INPUT
+docker cp cktgen/tests/vga_global_router_out.json sam:INPUT
 docker rm sam
 
 docker build -t cktgen .
 
 ./flow.sh -sgr -p 8086 -iv equalizerInputVol -ov equalizerOutputVol -s "-m cktgen.cktgen_physical_from_json" -src vga -td ../DetailedRouter/DR_COLLATERAL_Generator/hack84 --placer_json INPUT/__json_cktgen_physical_vga --small --no_interface
-
