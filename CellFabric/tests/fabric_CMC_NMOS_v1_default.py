11aa3fadcfbbf6e7fde5e49218e9a5c66f1fa208
Parijat Mukherjee <parijat.mukherjee@intel.com>
2019-07-02 09:47:17 -0700

    [CanvasNMOS] Attempt Simplification

--- CellFabric/tests/fabric_CMC_NMOS_v1_default.py
+++ CellFabric/tests/fabric_CMC_NMOS_v1_default.py
@@ -13,7 +13,6 @@ class CanvasNMOS(DefaultCanvas):
     def __init__( self, fin_u, fin, finDummy, gate, gateDummy):
         p = Pdk().load('../PDK_Abstraction/FinFET14nm_Mock_PDK/FinFET_Mock_PDK_Abstraction.json') 
         super().__init__(p)
-                                                 
         assert   p['Feol']['v0Pitch'] < 2*p['M2']['Pitch']
 
 ######### Derived Parameters ############        
@@ -28,11 +27,7 @@ class CanvasNMOS(DefaultCanvas):
         self.m2PerUnitCell = self.finsPerUnitCell//2 + 0
         self.unitCellHeight = self.m2PerUnitCell* p['M2']['Pitch']
         unitCellLength = self.gatesPerUnitCell* p['Poly']['Pitch']
-        #dcPitch  = 2* p['M1']['Pitch']               
-        #pcPitch  = self.unitCellHeight//2
-        #activeWidth1 =  p['Fin']['Pitch']*fin_u
         activeWidth =  p['Fin']['Pitch']*fin
-        activeWidth_h = ((gate-1)* p['Poly']['Pitch']) + (p['Feol']['plActive_s']*2)+p['Poly']['Width']
         activeOffset = activeWidth//2 + finDummy* p['Fin']['Pitch']-p['M2']['Pitch']//2+p['Fin']['Offset']
         activePitch = self.unitCellHeight
         RVTPitch = activePitch
@@ -40,23 +35,10 @@ class CanvasNMOS(DefaultCanvas):
         RVTOffset = RVTWidth//2 + finDummy* p['Fin']['Pitch']-p['Fin']['Pitch']//2-p['Feol']['active_enclosure']+p['Fin']['Offset']
 
 ############Include these all ###########
-       # self.extension_x = (self. self.pitch('Poly', 0, p) - self. p['Poly']['Width'])//2  ### Minimum horizontal extension of GCUT past GATE
 
         self.m0 = self.addGen( Wire( 'm0', 'M0', 'v',
                                      clg=UncoloredCenterLineGrid( pitch=   p['Feol']['m0Pitch'], width= p['Feol']['m0Width'], offset= p['Feol']['m0Pitch']//2),
                                      spg=EnclosureGrid( pitch=activePitch, offset=activeOffset, stoppoint=activeWidth//2, check=True)))
-
-        #self.m1 = self.addGen( Wire( 'm1', 'M1', 'v',
-        #                             clg=ColoredCenterLineGrid( colors=['c1','c2'], pitch= p['M1']['Pitch'], width=p['M1']['Width'], offset= p['M1']['Offset']),
-        #                             spg=EnclosureGrid( pitch= p['M2']['Pitch'], offset= p['M2']['Offset'], stoppoint= p['M2']['Width']//2 +p['V1']['VencA_L'], check=True)))
-
-        #self.m2 = self.addGen( Wire( 'm2', 'M2', 'h',
-        #                             clg=ColoredCenterLineGrid( colors=['c2','c1'], pitch= p['M2']['Pitch'], width= p['M2']['Width'], offset= p['M2']['Offset'], repeat=self.m2PerUnitCell),
-        #                             spg=EnclosureGrid(pitch= p['M1']['Pitch'], offset= p['M1']['Offset'], stoppoint= p['M1']['Width']//2 +   p['V1']['VencA_H'], check=True)))
-
-        #self.m3 = self.addGen( Wire( 'm3', 'M3', 'v',
-        #                             clg=ColoredCenterLineGrid( colors=['c1','c2'], pitch= p['M3']['Pitch'], width= p['M3']['Width'], offset= p['M3']['Offset']),
-        #                             spg=EnclosureGrid( pitch= p['M2']['Pitch'], offset= p['M2']['Offset'], stoppoint= p['M2']['Width']//2 +   p['V2']['VencA_H'], check=True)))
 
         self.pl = self.addGen( Wire( 'pl', 'poly', 'v',
                                      clg=UncoloredCenterLineGrid( pitch= p['Poly']['Pitch'], width= p['Poly']['Width'], offset= p['Poly']['Offset']),
@@ -67,23 +49,15 @@ class CanvasNMOS(DefaultCanvas):
                                       clg=UncoloredCenterLineGrid( pitch= p['Fin']['Pitch'], width= p['Fin']['Width'], offset= p['Fin']['Offset']),
                                       spg=SingleGrid( offset=0, pitch=unitCellLength)))
 
+        stoppoint = (gateDummy-1)* p['Poly']['Pitch'] +  p['Poly']['Offset']
+
         self.active = self.addGen( Wire( 'active', 'active', 'h',
                                          clg=UncoloredCenterLineGrid( pitch=activePitch, width=activeWidth, offset=activeOffset),
-                                         spg=CenterLineGrid()))
-        stoppoint = (gateDummy-1)* p['Poly']['Pitch'] +  p['Poly']['Offset']
-        self.active.spg.addCenterLine( 0,         activeWidth_h, False)       
-        self.active.spg.addCenterLine( stoppoint, activeWidth_h,        True)
-        self.active.spg.addCenterLine( unitCellLength-stoppoint, activeWidth_h,         True)
-        self.active.spg.addCenterLine( unitCellLength, activeWidth_h,         False)
+                                         spg=EnclosureGrid( pitch=unitCellLength, offset=0, stoppoint=stoppoint, check=True)))
 
         self.RVT = self.addGen( Wire( 'RVT', 'polycon', 'h',
                                       clg=UncoloredCenterLineGrid( pitch=RVTPitch, width=RVTWidth, offset=RVTOffset),
-                                      spg=CenterLineGrid()))
-
-        self.RVT.spg.addCenterLine( 0,         activeWidth_h, False)       
-        self.RVT.spg.addCenterLine( stoppoint, activeWidth_h,        True)
-        self.RVT.spg.addCenterLine( unitCellLength-stoppoint, activeWidth_h,         True)
-        self.RVT.spg.addCenterLine( unitCellLength, activeWidth_h,         False)
+                                      spg=EnclosureGrid( pitch=unitCellLength, offset=0, stoppoint=stoppoint, check=True)))
 
         self.nselect = self.addGen( Region( 'nselect', 'nselect',
                                             v_grid=CenteredGrid( offset= p['Poly']['Pitch']//2, pitch= p['Poly']['Pitch']),
@@ -96,7 +70,6 @@ class CanvasNMOS(DefaultCanvas):
                                             h_grid=self.fin.clg))
 
         v0x_offset =  p['M2']['Offset'] + (1+finDummy//2)* p['M2']['Pitch']
-        #print( "SMB",   p['Feol']['v0Pitch'],  self.pitch('M2', 0, p))
 
         self.v0 = self.addGen( Via( 'v0', 'V0',
                                     h_clg=CenterLineGrid(),
@@ -107,10 +80,7 @@ class CanvasNMOS(DefaultCanvas):
             self.v0.h_clg.addCenterLine( v0x_offset+i*  p['Feol']['v0Pitch'],    p['Feol']['v0Width'], True)
         self.v0.h_clg.addCenterLine( self.unitCellHeight,    p['Feol']['v0Width'], False)
 
-        #self.v1 = self.addGen( Via( 'v1', 'V1', h_clg=self.m2.clg,  v_clg=self.m1.clg))
-        #self.v2 = self.addGen( Via( 'v2', 'V2', h_clg=self.m2.clg,  v_clg=self.m3.clg))
 
-        
 class UnitCell(CanvasNMOS):
 
     def __init__( self, fin_u, fin, finDummy, gate, gateDummy):
