3e0535874c61116268aacd6f967bb64f3e07faeb
Wenbin Xu <wbxu@tamu.edu>
2019-07-04 21:22:40 -0500

    Fix compile error

--- PlaceRouteHierFlow/main.cpp
+++ PlaceRouteHierFlow/main.cpp
@@ -7,7 +7,7 @@
 #include "./cap_placer/capplacer.h"
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <boost/program_options.hpp>
+//#include <boost/program_options.hpp>
 //#include "./cap_placer/Capdatatype.h"
 
 using std::string;
@@ -20,82 +20,83 @@ double ConstGraph::GAMAR=30;
 double ConstGraph::BETA=100;
 double ConstGraph::SIGMA=1000;
 
-template<class T>
-std::ostream& operator<<(std::ostream& os, const std::vector<T>& v)
-{
-    std::copy(v.begin(), v.end(), std::ostream_iterator<T>(os, " "));
-    return os;
-}
+//template<class T>
+//std::ostream& operator<<(std::ostream& os, const std::vector<T>& v)
+//{
+//    std::copy(v.begin(), v.end(), std::ostream_iterator<T>(os, " "));
+//    return os;
+//}
 
 int main(int argc, char** argv ){
-  //string opath="./Results/";
-  //string fpath=argv[1];
-  //string lfile=argv[2];
-  //string vfile=argv[3];
-  //string mfile=argv[4];
-  //string dfile=argv[5];
-  //string topcell=argv[6];
-  //int numLayout=std::stoi(argv[7]);
-  string opath;
-  string fpath;
-  string lfile;
-  string vfile;
-  string mfile;
-  string dfile;
-  string topcell;
-  int effort;
-  int numLayout;
+  string opath="./Results/";
+  string fpath=argv[1];
+  string lfile=argv[2];
+  string vfile=argv[3];
+  string mfile=argv[4];
+  string dfile=argv[5];
+  string topcell=argv[6];
+  int numLayout=std::stoi(argv[7]);
+  int effort=std::stoi(argv[8]);
+  //string opath;
+  //string fpath;
+  //string lfile;
+  //string vfile;
+  //string mfile;
+  //string dfile;
+  //string topcell;
+  //int effort;
+  //int numLayout;
 
-  boost::program_options::options_description desc("Allowed options");
-  desc.add_options()
-      ("help,h", "produce help message")
-      ("inputDir,I", boost::program_options::value<std::string>(&fpath)->required(),
-            "directory of input data")
-      ("outputDir,O", boost::program_options::value<string>(&opath)->default_value("./Results"),
-            "directory of output data, default folder is './Results'")
-      ("lef", boost::program_options::value<string >(&lfile)->required(), 
-            "LEF file")
-      ("verilog", boost::program_options::value<string >(&vfile)->required(), 
-            "Verilog file")
-      ("map", boost::program_options::value<string >(&mfile)->required(), 
-            "mapping file")
-      ("pdk", boost::program_options::value<string >(&dfile)->required(), 
-            "PDK abstraction file")
-      ("topDesign", boost::program_options::value<string >(&topcell)->required(), 
-            "top name")
-      ("maxLayout", boost::program_options::value<int>(&numLayout)->default_value(1),
-            "maximum number of output layouts, default value is 1")
-      ("effort", boost::program_options::value<int>(&effort)->default_value(0),
-            "optimization effort in range [0, 2], default value is 0")
-      //("verbose,v", boost::program_options::value<int>()->implicit_value(1)->default_value(0),
-      //      "enable verbosity (optionally specify level)")
-  ;
-  boost::program_options::variables_map vm;
+  //boost::program_options::options_description desc("Allowed options");
+  //desc.add_options()
+  //    ("help,h", "produce help message")
+  //    ("inputDir,I", boost::program_options::value<std::string>(&fpath)->required(),
+  //          "directory of input data")
+  //    ("outputDir,O", boost::program_options::value<string>(&opath)->default_value("./Results"),
+  //          "directory of output data, default folder is './Results'")
+  //    ("lef", boost::program_options::value<string >(&lfile)->required(), 
+  //          "LEF file")
+  //    ("verilog", boost::program_options::value<string >(&vfile)->required(), 
+  //          "Verilog file")
+  //    ("map", boost::program_options::value<string >(&mfile)->required(), 
+  //          "mapping file")
+  //    ("pdk", boost::program_options::value<string >(&dfile)->required(), 
+  //          "PDK abstraction file")
+  //    ("topDesign", boost::program_options::value<string >(&topcell)->required(), 
+  //          "top name")
+  //    ("maxLayout", boost::program_options::value<int>(&numLayout)->default_value(1),
+  //          "maximum number of output layouts, default value is 1")
+  //    ("effort", boost::program_options::value<int>(&effort)->default_value(0),
+  //          "optimization effort in range [0, 2], default value is 0")
+  //    //("verbose,v", boost::program_options::value<int>()->implicit_value(1)->default_value(0),
+  //    //      "enable verbosity (optionally specify level)")
+  //;
+  //boost::program_options::variables_map vm;
 
-  try {
-    boost::program_options::store(boost::program_options::command_line_parser(argc, argv).options(desc).run(), vm);
-    if (vm.count("help")) {
-        cout << "USAGE:\n";
-        cout << desc;
-        return 0;
-    }
-    boost::program_options::notify(vm);
+  //try {
+  //  boost::program_options::store(boost::program_options::command_line_parser(argc, argv).options(desc).run(), vm);
+  //  if (vm.count("help")) {
+  //      cout << "USAGE:\n";
+  //      cout << desc;
+  //      return 0;
+  //  }
+  //  boost::program_options::notify(vm);
 
-    std::cout<<"Info: Input directory is "<<fpath<<std::endl;
-    std::cout<<"Info: Output directory is "<<opath<<std::endl;
-    std::cout<<"Info: Maximum number of output layouts "<<numLayout<<std::endl;
-    std::cout<<"Info: LEF file is "<<lfile<<std::endl;
-    std::cout<<"Info: PDK abstraction file is "<<dfile<<std::endl;
-    std::cout<<"Info: Verilog file is "<<vfile<<std::endl;
-    std::cout<<"Info: Mapping file is "<<mfile<<std::endl;
-    std::cout<<"Info: Top design name is "<<topcell<<std::endl;
-    std::cout<<"Info: Optimization effort is "<<effort<<std::endl;
-  }
-  catch(std::exception& e)
-  {
-      cout << e.what() << "\n";
-      return 1;
-  }
+  //  std::cout<<"Info: Input directory is "<<fpath<<std::endl;
+  //  std::cout<<"Info: Output directory is "<<opath<<std::endl;
+  //  std::cout<<"Info: Maximum number of output layouts "<<numLayout<<std::endl;
+  //  std::cout<<"Info: LEF file is "<<lfile<<std::endl;
+  //  std::cout<<"Info: PDK abstraction file is "<<dfile<<std::endl;
+  //  std::cout<<"Info: Verilog file is "<<vfile<<std::endl;
+  //  std::cout<<"Info: Mapping file is "<<mfile<<std::endl;
+  //  std::cout<<"Info: Top design name is "<<topcell<<std::endl;
+  //  std::cout<<"Info: Optimization effort is "<<effort<<std::endl;
+  //}
+  //catch(std::exception& e)
+  //{
+  //    cout << e.what() << "\n";
+  //    return 1;
+  //}
   if(fpath.back()=='/') {fpath.erase(fpath.end()-1);}
   if(opath.back()!='/') {opath+="/";}
 
--- PlaceRouteHierFlow/run_small_in_docker.sh
+++ PlaceRouteHierFlow/run_small_in_docker.sh
@@ -2,6 +2,6 @@
  
 (cd testcase_small; tar cvf - .) | docker run --rm -i --mount source=placerInputVol,target=/PlaceRouteHierFlow/INPUT ubuntu /bin/bash -c "cd /PlaceRouteHierFlow/INPUT; tar xvf -"
 
-docker run --rm --mount source=placerInputVol,target=/PlaceRouteHierFlow/INPUT --mount source=placerOutputVol,target=/PlaceRouteHierFlow/OUTPUT placeroute_image /bin/bash -c "cd /PlaceRouteHierFlow; ./tester -I ./INPUT --lef sc.lef --verilog sc_block.v --map sc.map --pdk NO.rul --topDesign switched_capacitor_filter --maxLayout 1"
+docker run --rm --mount source=placerInputVol,target=/PlaceRouteHierFlow/INPUT --mount source=placerOutputVol,target=/PlaceRouteHierFlow/OUTPUT placeroute_image /bin/bash -c "cd /PlaceRouteHierFlow; ./tester ./INPUT sc.lef sc_block.v sc.map NO.rul switched_capacitor_filter 1 0"
 
 
