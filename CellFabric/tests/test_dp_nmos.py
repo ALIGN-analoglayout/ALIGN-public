53c935606753bf68f80fb06842cf2534cbb26f88
Parijat Mukherjee <parijat.mukherjee@intel.com>
2019-07-10 14:29:00 -0700

    [tests] Make Codacy Happy

--- CellFabric/tests/test_dp_nmos.py
+++ CellFabric/tests/test_dp_nmos.py
@@ -1,8 +1,6 @@
 import json
-import argparse
 import sys
 import datetime
-import itertools
 import pytest
 
 sys.path.append('./Cell_Fabric_FinFET__Mock')
@@ -17,15 +15,14 @@ def setup():
     fin = 12
     x_cells = 4
     y_cells = 2
-    pattern = 1
     gate = 2
     gateDummy = 3 ### Total Dummy gates per unit cell: 2*gateDummy
     finDummy = 4  ### Total Dummy fins per unit cell: 2*finDummy
     gu = gate + 2*gateDummy
-     ##### Routing    
+     ##### Routing
     SDG =(SA, GA, DA, SB, GB, DB) = pattern_generator.pattern.common_centroid(x_cells, gu, gate, gateDummy)
 
-    (S, D, G) = (SA+SB, DA+DB, GA+GB)
+    S = SA+SB
     CcM3 = (min(S)+max(S))//2
 
     uc = fabric_NMOS.UnitCell( fin_u, fin, finDummy, gate, gateDummy, '../PDK_Abstraction/FinFET14nm_Mock_PDK/FinFET_Mock_PDK_Abstraction.json')
@@ -52,19 +49,19 @@ def test_fabric_Dp(setup):
         assert data == data_golden
 
 def test_fabric_gds_json():
-    
+
 
     fn = "tests/__json_dp_nmos"
 
-       
+
     with open( fn + "_cand", "rt") as fp0, \
          open(fn + "_gds_cand", 'wt') as fp1:
         gen_gds_json.translate("test", '', fp0, fp1, datetime.datetime( 2019, 1, 1, 0, 0, 0))
-        
+
     with open( fn + "_gds_cand", "rt") as fp0, \
          open( fn + "_gds_gold", "rt") as fp1:
         cand = json.load( fp0)
         gold = json.load( fp1)
         assert cand == gold
-    
-        
+
+
