43848063e52f8e40fc0d9f9110cb9e0958003079
Parijat Mukherjee <parijat.mukherjee@intel.com>
2019-07-10 14:22:24 -0700

    [NMOS] Enable removeDuplicates netName propagation

--- CellFabric/Cell_Fabric_FinFET__Mock/fabric_NMOS.py
+++ CellFabric/Cell_Fabric_FinFET__Mock/fabric_NMOS.py
@@ -1,5 +1,5 @@
 from primitive import CanvasNMOS
-        
+
 class UnitCell(CanvasNMOS):
 
     def unit( self, x, y, x_cells, y_cells, fin_u, fin, finDummy, gate, gateDummy, SDG, Routing):
@@ -8,43 +8,42 @@ class UnitCell(CanvasNMOS):
         #####   This part generats locations of S/D/G   #####
         gu = self.gatesPerUnitCell
         h = self.m2PerUnitCell
-                       
-        self.addWire( self.active, 'active', None, y, (x,1), (x+1,-1)) 
-        self.addWire( self.RVT,    'RVT',    None, y, (x, 1), (x+1, -1)) 
- 
+
+        self.addWire( self.active, 'active', None, y, (x,1), (x+1,-1))
+        self.addWire( self.RVT,    'RVT',    None, y, (x, 1), (x+1, -1))
+
         for i in range(1,  self.finsPerUnitCell):
             self.addWire( self.fin, 'fin', None,  self.finsPerUnitCell*y+i, x, x+1)
 
-        #####   Gate Placement   #####                       
-        for i in range(gu):        
+        #####   Gate Placement   #####
+        for i in range(gu):
             self.addWire( self.pl, 'g', None, gu*x+i,   (y,0), (y,1))
 
         #####   Nselect Placement   #####
-        if x == x_cells -1 and y == y_cells -1:      
-            self.addRegion( self.nselect, 'ns', None, (0, -1), 0, ((1+x)*gu, -1), (y+1)* self.finsPerUnitCell)    
-                
+        if x == x_cells -1 and y == y_cells -1:
+            self.addRegion( self.nselect, 'ns', None, (0, -1), 0, ((1+x)*gu, -1), (y+1)* self.finsPerUnitCell)
+
         if x_cells-1==x:
             grid_y0 = y*h + finDummy//2-1
             grid_y1 = grid_y0+(fin+2)//2
             for i in G:
-                self.addWire( self.m1, '_', None, i, (grid_y0, -1), (grid_y1, 1))
+                self.addWire( self.m1, None, None, i, (grid_y0, -1), (grid_y1, 1))
             for i in S+D:
-                SD = 'S' if i in S else 'D'
-                self.addWire( self.m1, SD, None, i, (grid_y0, -1), (grid_y1, 1)) 
+                self.addWire( self.m1, None, None, i, (grid_y0, -1), (grid_y1, 1))
                 for j in range((((fin-fin_u)//2 +finDummy+3)//2),self.v0.h_clg.n):
                     self.addVia( self.v0, 'v0', None, i, (y, j))
 
-            #pin = 'VDD' if y%2==0 else 'GND'    
+            #pin = 'VDD' if y%2==0 else 'GND'
             #self.addWire( self.m2, pin, pin, h*(y+1), (0, 1), (x_cells*gu, -1))
-            #self.addWire( self.m2, 'GND', 'GND', 0, (0, 1), (x_cells*gu, -1))                   
+            #self.addWire( self.m2, 'GND', 'GND', 0, (0, 1), (x_cells*gu, -1))
             for (pin, contact, track, m3route) in Routing:
-                self.addWire( self.m2,'_', pin, y*h+track, (min(contact), -1), (max(contact), 1))
+                self.addWire( self.m2, pin, pin, y*h+track, (min(contact), -1), (max(contact), 1))
                 if y_cells > 1:
-                   self.addWire( self.m3,'_', None, m3route, (track, -1), (y*h+track, 1))
-                   self.addVia( self.v2,'_', None, m3route, track)
-                   self.addVia( self.v2,'_', None, m3route, y*h+track)
+                   self.addWire( self.m3, None, None, m3route, (track, -1), (y*h+track, 1))
+                   self.addVia( self.v2, None, None, m3route, track)
+                   self.addVia( self.v2, None, None, m3route, y*h+track)
 
                 for i in contact:
-                    self.addVia( self.v1, '_', None, i, y*h+track) 
-                                                                                                                                               
+                    self.addVia( self.v1, None, None, i, y*h+track)
+
 
--- CellFabric/tests/test_dp_nmos.py
+++ CellFabric/tests/test_dp_nmos.py
@@ -3,6 +3,7 @@ import argparse
 import sys
 import datetime
 import itertools
+import pytest
 
 sys.path.append('./Cell_Fabric_FinFET__Mock')
 
@@ -10,7 +11,8 @@ import gen_gds_json
 import fabric_NMOS
 import pattern_generator
 
-def test_fabric_Dp():
+@pytest.fixture
+def setup():
     fin_u = 12
     fin = 12
     x_cells = 4
@@ -22,42 +24,32 @@ def test_fabric_Dp():
     gu = gate + 2*gateDummy
      ##### Routing    
     SDG =(SA, GA, DA, SB, GB, DB) = pattern_generator.pattern.common_centroid(x_cells, gu, gate, gateDummy)
-    
+
     (S, D, G) = (SA+SB, DA+DB, GA+GB)
     CcM3 = (min(S)+max(S))//2
 
     uc = fabric_NMOS.UnitCell( fin_u, fin, finDummy, gate, gateDummy, '../PDK_Abstraction/FinFET14nm_Mock_PDK/FinFET_Mock_PDK_Abstraction.json')
-   
+
     for (x,y) in ( (x,y) for x in range(x_cells) for y in range(y_cells)):
         Routing = [('S', S, 1, CcM3), ('DA', DA if y%2==0 else DB, 2, CcM3-1), ('DB', DB if y%2==0 else DA, 3, CcM3+1), ('GA', GA if y%2==0 else GB, 4, CcM3-2), ('GB', GB if y%2==0 else GA, 5, CcM3+2)]
 
         uc.unit( x, y, x_cells, y_cells, fin_u, fin, finDummy, gate, gateDummy, SDG, Routing)
 
-    uc.computeBbox()
+    return uc
 
-    data = { 'bbox' : uc.bbox.toList(), 'globalRoutes' : [], 'globalRouteGrid' : [], 'terminals' : uc.terminals}
+def test_fabric_Dp(setup):
+
+    uc = setup
 
     fn = "tests/__json_dp_nmos"
 
     with open( fn + "_cand", "wt") as fp:
-        fp.write( json.dumps( data, indent=2) + '\n')
-
-    
-    #with open( args.block_name + ".json", "rt") as fp0, \
-    #     open( args.block_name + ".gds.json", 'wt') as fp1:
-    #    gen_gds_json.translate(args.block_name, '', fp0, fp1, datetime.now())
+        data = uc.writeJSON( fp)
 
     with open( fn + "_gold", "rt") as fp:
         data_golden = json.load( fp)
         assert data['bbox'] == data_golden['bbox']
-#        assert data == data_golden
-        for (x,y) in zip( data['terminals'], data_golden['terminals']):
-            x['netName'] = '_'
-            y['netName'] = '_'
-            x['pin'] = '_'
-            y['pin'] = '_'
-            assert x == y
-
+        assert data == data_golden
 
 def test_fabric_gds_json():
     
