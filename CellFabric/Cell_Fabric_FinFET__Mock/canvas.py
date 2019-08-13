6ef8e4cc741c89a72ca4be294b7ecd4f3852e67a
Parijat Mukherjee <parijat.mukherjee@intel.com>
2019-08-13 14:38:36 -0700

    [MockPDK] Remove redundant class definitions

--- CellFabric/Cell_Fabric_FinFET__Mock/canvas.py
+++ CellFabric/Cell_Fabric_FinFET__Mock/canvas.py
@@ -132,15 +132,3 @@ class FinFET_Mock_PDK_Canvas(DefaultCanvas):
         if x == x_cells -1 and y == y_cells -1:      
             self.addRegion( self.pselect, 'ps', None, (0, -1), 0, ((1+x)*self.gatesPerUnitCell, -1), (y+1)* self.finsPerUnitCell)
             self.addRegion( self.nwell, 'nw', None, (0, -1), 0, ((1+x)*self.gatesPerUnitCell, -1), (y+1)* self.finsPerUnitCell)    
-
-class NMOS(FinFET_Mock_PDK_Canvas):
-
-    def unit( self, x, y, x_cells, y_cells, fin_u, fin, finDummy, gate, gateDummy, SDG, Routing):
-        
-        super().genNMOS(x, y, x_cells, y_cells, fin_u, fin, finDummy, gate, gateDummy, SDG, Routing)
-
-class PMOS(FinFET_Mock_PDK_Canvas):
-
-    def unit( self, x, y, x_cells, y_cells, fin_u, fin, finDummy, gate, gateDummy, SDG, Routing):
-
-        super().genPMOS(x, y, x_cells, y_cells, fin_u, fin, finDummy, gate, gateDummy, SDG, Routing)
