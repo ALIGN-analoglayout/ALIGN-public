942a1e17bb557ef2c128c2d30ea7ed7c97e49ba0
Steven Burns <smburns47@gmail.com>
2019-08-22 14:43:59 -0700

    Add test report as well

--- PlaceRouteHierFlow/run_small_in_docker.sh
+++ PlaceRouteHierFlow/run_small_in_docker.sh
@@ -2,6 +2,6 @@
  
 (cd testcase_example; tar cvf - .) | docker run --rm -i --mount source=placerInputVol,target=/PlaceRouteHierFlow/INPUT ubuntu /bin/bash -c "cd /PlaceRouteHierFlow/INPUT; tar xvf -"
 
-docker run --name PnR --mount source=placerInputVol,target=/PlaceRouteHierFlow/INPUT --mount source=placerOutputVol,target=/PlaceRouteHierFlow/OUTPUT placeroute_coverage_image /bin/bash -c "cd /PlaceRouteHierFlow && lcov -z ; ./pnr_compiler ./testcase_example switched_capacitor_filter.lef switched_capacitor_filter.v switched_capacitor_filter.map FinFET_Mock_PDK_Abstraction.json switched_capacitor_filter 2 0 > PnR.log && PnRDB/unit_tests && lcov --capture --directory . --output-file coverage.info ; genhtml coverage.info --output-directory /PlaceRouteHierFlow/coverage.out" 
+docker run --name PnR --mount source=placerInputVol,target=/PlaceRouteHierFlow/INPUT --mount source=placerOutputVol,target=/PlaceRouteHierFlow/OUTPUT placeroute_coverage_image /bin/bash -c "cd /PlaceRouteHierFlow && lcov -z ; ./pnr_compiler ./testcase_example switched_capacitor_filter.lef switched_capacitor_filter.v switched_capacitor_filter.map FinFET_Mock_PDK_Abstraction.json switched_capacitor_filter 2 0 > PnR.log && PnRDB/unit_tests --gtest_output=xml:junit.xml && lcov --capture --directory . --output-file coverage.info ; genhtml coverage.info --output-directory coverage.out"
 
 
