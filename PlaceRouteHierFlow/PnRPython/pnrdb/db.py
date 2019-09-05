d6d9caba5275b9c03fc9e8a375ec16de8e6883c2
Steven Burns <smburns47@gmail.com>
2019-09-05 08:19:27 -0700

    Trying generic routine to generate the classes

--- PlaceRouteHierFlow/PnRPython/pnrdb/db.py
+++ PlaceRouteHierFlow/PnRPython/pnrdb/db.py
@@ -1,5 +1,185 @@
 import json
 
+abstract_structs = [
+           ("point",[("x",None),
+                   ("y",None)
+           ]),
+           ("bbox",[("polygon",(list, "point")),
+                  ("LL","point"),
+                  ("LR","point"),
+                  ("UL","point"),
+                  ("UR","point")
+           ]),
+           ("contact",[("metal",None),
+                     ("originBox","bbox"),
+                     ("placedBox","bbox"),
+                     ("originCenter","point"),
+                     ("placedCenter","point")
+           ]),
+           ("terminal",[("name",None),
+                      ("type",None),
+                      ("netIter",None),
+                      ("termContacts",(list,"contact"))
+           ]),
+           ("connectNode",[("type",None),
+                         ("iter",None),
+                         ("iter2",None)
+           ]),
+           ("globalContact",[("conTact","contact"),
+                           ("MetalIdx",None)
+           ]),
+           ("net",[("name",None),
+                 ("shielding",None),
+                 ("sink2Terminal",None),
+                 ("degree",None),
+                 ("symCounterpart",None),
+                 ("iter2SNetLsit",None),
+                 ("connected",(list, "connectNode")),
+                 ("priority",None),
+                 ("segments",(list,"contact")),
+                 ("interVias",(list,"contact")),
+                 ("path_metal",(list,"Metal")),
+                 ("path_via",(list,"Via")),
+                 ("connectedContact",(list,"globalContact")),
+                 ("axis_dir",None),
+                 ("axis_coor",None)
+           ]),
+           ("SymmNet",[("net1","net"),
+                     ("net2","net"),
+                     ("iter1",None),
+                     ("iter2",None)
+           ]),
+           ("SymmPairBlock",[("sympair",(list,None)),
+                           ("selfsym",(list,None))
+           ]),
+           ("Preplace",[("blockid1",None),
+                      ("blockid2",None),
+                      ("conner",None),
+                      ("distance",None),
+                      ("horizon",None)
+           ]),
+           ("Alignment",[("blockid1",None),
+                       ("blockid2",None),
+                       ("distance",None),
+                       ("horizon",None)
+           ]),
+           ("Abument",[("blockid1",None),
+                     ("blockid2",None),
+                     ("distance",None),
+                     ("horizon",None)
+           ]),
+           ("MatchBlock",[("blockid1",None),
+                        ("blockid2",None)
+           ]),
+           ("AlignBlock",[("blocks",(list,None)),
+                        ("horizon",None)
+           ]),
+           ("PortPos",[("tid",None),
+                     ("pos",None)
+           ]),
+           ("CCCap",[("size",(list,None)),
+                   ("CCCap_name",None),
+                   ("Unit_capacitor",None),
+                   ("cap_ratio",None),
+                   ("cap_r",None),
+                   ("cap_s",None)
+           ]),
+           ("Metal",[("MetalIdx",None),
+                   ("LinePoint",(list,"point")),
+                   ("width",None),
+                   ("MetalRect","contact")
+           ]),
+           ("Via",[("model_index",None),
+                 ("originpos","point"),
+                 ("placedpos","point"),
+                 ("UpperMetalRect","contact"),
+                 ("LowerMetalRect","contact"),
+                 ("ViaRect","contact")
+           ]),
+           ("pin",[("name",None),
+                 ("type",None),
+                 ("use",None),
+                 ("netIter",None),
+                 ("pinContacts",(list,"contact")),
+                 ("pinVias",(list,"Via")),
+           ]),
+
+
+           ("block",[("name",None),
+                   ("master",None),
+                   ("lefmaster",None),
+                   ("type",None),
+                   ("width",None),
+                   ("height",None),
+                   ("isLeaf",None),
+                   ("originBox","bbox"),
+                   ("originCenter","point"),
+                   ("gdsFile",None),
+                   ("orient",None),
+                   ("placedBox","bbox"),
+                   ("placedCenter","point"),
+                   ("blockPins",(list,"pin")),
+                   ("interMetals",(list,"contact")),
+                   ("interVias",(list,"Via")),
+                   ("dummy_power_pin",(list,"pin")),
+           ]),
+           ("blockComplex",[("instance",(list, "block")),
+                          ("selectedInstance",None),
+                          ("child",None),
+                          ("instNum",None)
+           ]),
+           ("PowerGrid",[("metals",(list,"Metal")),
+                       ("vias",(list,"Via")),
+                       ("power",None)
+           ]),
+           ("PowerNet",[("name",None),
+                      ("power",None),
+                      ("Pins",(list,"pin")),
+                      ("connected",(list,"connectNode")),
+                      ("dummy_connected",(list,"connectNode")),
+                      ("path_metal",(list,"Metal")),
+                      ("path_via",(list,"Via")),
+           ]),
+           ("layoutAS",[("width",None),
+                      ("height",None),
+                      ("gdsFile",None),
+                      ("Blocks",(list,"blockComplex")),
+                      ("Nets",(list,"net")),
+                      ("Terminals",(list,"terminal"))
+           ]),
+
+          ("hierNode",[("isCompleted",None),
+                      ("isTop",None),
+                      ("width",None),
+                      ("height",None),
+                      ("name",None),
+                      ("gdsFile",None),
+                      ("parent",(list,None)),
+                      ("Blocks", (list, "blockComplex")),
+                      ("Nets", (list, "net")),
+                      ("Terminals", (list, "terminal")),
+                      ("Vdd", "PowerGrid"),
+                      ("Gnd", "PowerGrid"),
+                      ("PowerNets", (list,"PowerNet")),
+                      ("blockPins", (list,"pin")),
+                      ("interMetals", (list,"contact")),
+                      ("interVias", (list,"Via")),
+                      ("PnRAS", (list,"layoutAS")),
+                      ("SNets", (list,"SymmNet")),
+                      ("SPBlocks", (list,"SymmPairBlock")),
+                      ("Preplace_blocks", (list,"Preplace")),
+                      ("Alignment_blocks", (list,"Alignment")),
+                      ("Align_blocks", (list,"AlignBlock")),
+                      ("Abument_blocks", (list,"Abument")),
+                      ("Match_blocks", (list,"MatchBlock")),
+                      ("CC_Caps", (list,"CCCap")),
+                      ("Port_Location", (list,"PortPos")),
+                      ("bias_Hgraph",None),
+                      ("bias_Vgraph",None)
+           ])
+]
+
+
 class FallbackJSON:
     def __init__(self, d):    
         self.__dict__['_d'] = d
@@ -9,6 +189,28 @@ class FallbackJSON:
 
     def __setattr__(self, nm, v):
         self._d[nm] = v
+
+for (k,v) in abstract_structs:
+    attrs_dict = {}
+    for (nm,vv) in v:
+        def init_fn(self, d):
+            super().__init__(d)
+            if isinstance( vv, tuple):
+                assert vv[0] is list
+                if vv[1] is None:
+                    self.__dict__[nm] = [ x for x in d[nm]]
+                else:
+                    klass = globals()[vv[1]]
+                    self__dict__[nm] = [ klass(x) for x in d[nm]]
+            else:
+                if vv is None:
+                    self.__dict__[nm] = d[nm]
+                else:
+                    klass = globals()[vv]
+                    self__dict__[nm] = klass(x)
+        attrs_dict["__init__"] = init_fn
+    type( k, (FallbackJSON,), attrs_dict)
+
 
 class point(FallbackJSON):
     def __init__(self, d):    
@@ -177,180 +379,17 @@ class hierNode(FallbackJSON):
         self.__dict__['CC_Caps'] = [ CCCap(x) for x in d["CC_Caps"]]
         self.__dict__['Port_Location'] = [ PortPos(x) for x in d["Port_Location"]]
 
-structs = [(hierNode,[("isCompleted",None),
-                      ("isTop",None),
-                      ("width",None),
-                      ("height",None),
-                      ("name",None),
-                      ("gdsFile",None),
-                      ("parent",(list,None)),
-                      ("Blocks", (list, blockComplex)),
-                      ("Nets", (list, net)),
-                      ("Terminals", (list, terminal)),
-                      ("Vdd", PowerGrid),
-                      ("Gnd", PowerGrid),
-                      ("PowerNets", (list,PowerNet)),
-                      ("blockPins", (list,pin)),
-                      ("interMetals", (list,contact)),
-                      ("interVias", (list,Via)),
-                      ("PnRAS", (list,layoutAS)),
-                      ("SNets", (list,SymmNet)),
-                      ("SPBlocks", (list,SymmPairBlock)),
-                      ("Preplace_blocks", (list,Preplace)),
-                      ("Alignment_blocks", (list,Alignment)),
-                      ("Align_blocks", (list,AlignBlock)),
-                      ("Abument_blocks", (list,Abument)),
-                      ("Match_blocks", (list,MatchBlock)),
-                      ("CC_Caps", (list,CCCap)),
-                      ("Port_Location", (list,PortPos)),
-                      ("bias_Hgraph",None),
-                      ("bias_Vgraph",None)
-           ]),
-           (blockComplex,[("instance",(list, block)),
-                          ("selectedInstance",None),
-                          ("child",None),
-                          ("instNum",None)
-           ]),
-           (block,[("name",None),
-                   ("master",None),
-                   ("lefmaster",None),
-                   ("type",None),
-                   ("width",None),
-                   ("height",None),
-                   ("isLeaf",None),
-                   ("originBox",bbox),
-                   ("originCenter",point),
-                   ("gdsFile",None),
-                   ("orient",None),
-                   ("placedBox",bbox),
-                   ("placedCenter",point),
-                   ("blockPins",(list,pin)),
-                   ("interMetals",(list,contact)),
-                   ("interVias",(list,Via)),
-                   ("dummy_power_pin",(list,pin)),
-           ]),
-           (terminal,[("name",None),
-                      ("type",None),
-                      ("netIter",None),
-                      ("termContacts",(list,contact))
-           ]),
-           (bbox,[("polygon",(list, point)),
-                  ("LL",point),
-                  ("LR",point),
-                  ("UL",point),
-                  ("UR",point)
-           ]),
-           (point,[("x",None),
-                   ("y",None)
-           ]),
-           (contact,[("metal",None),
-                     ("originBox",bbox),
-                     ("placedBox",bbox),
-                     ("originCenter",point),
-                     ("placedCenter",point)
-           ]),
-           (connectNode,[("type",None),
-                         ("iter",None),
-                         ("iter2",None)
-           ]),
-           (globalContact,[("conTact",contact),
-                           ("MetalIdx",None)
-           ]),
-           (net,[("name",None),
-                 ("shielding",None),
-                 ("sink2Terminal",None),
-                 ("degree",None),
-                 ("symCounterpart",None),
-                 ("iter2SNetLsit",None),
-                 ("connected",(list, connectNode)),
-                 ("priority",None),
-                 ("segments",(list,contact)),
-                 ("interVias",(list,contact)),
-                 ("path_metal",(list,Metal)),
-                 ("path_via",(list,Via)),
-                 ("connectedContact",(list,globalContact)),
-                 ("axis_dir",None),
-                 ("axis_coor",None)
-           ]),
-           (Metal,[("MetalIdx",None),
-                   ("LinePoint",(list,point)),
-                   ("width",None),
-                   ("MetalRect",contact)
-           ]),
-           (Via,[("model_index",None),
-                 ("originpos",point),
-                 ("placedpos",point),
-                 ("UpperMetalRect",contact),
-                 ("LowerMetalRect",contact),
-                 ("ViaRect",contact)
-           ]),
-           (pin,[("name",None),
-                 ("type",None),
-                 ("use",None),
-                 ("netIter",None),
-                 ("pinContacts",(list,contact)),
-                 ("pinVias",(list,Via)),
-           ]),
-           (PowerGrid,[("metals",(list,Metal)),
-                       ("vias",(list,Via)),
-                       ("power",None)
-           ]),
-           (PowerNet,[("name",None),
-                      ("power",None),
-                      ("Pins",(list,pin)),
-                      ("connected",(list,connectNode)),
-                      ("dummy_connected",(list,connectNode)),
-                      ("path_metal",(list,Metal)),
-                      ("path_via",(list,Via)),
-           ]),
-           (layoutAS,[("width",None),
-                      ("height",None),
-                      ("gdsFile",None),
-                      ("Blocks",(list,blockComplex)),
-                      ("Nets",(list,net)),
-                      ("Terminals",(list,terminal))
-           ]),
-           (SymmNet,[("net1",net),
-                     ("net2",net),
-                     ("iter1",None),
-                     ("iter2",None)
-           ]),
-           (SymmPairBlock,[("sympair",(list,None)),
-                           ("selfsym",(list,None))
-           ]),
-           (Preplace,[("blockid1",None),
-                      ("blockid2",None),
-                      ("conner",None),
-                      ("distance",None),
-                      ("horizon",None)
-           ]),
-           (Alignment,[("blockid1",None),
-                       ("blockid2",None),
-                       ("distance",None),
-                       ("horizon",None)
-           ]),
-           (Abument,[("blockid1",None),
-                     ("blockid2",None),
-                     ("distance",None),
-                     ("horizon",None)
-           ]),
-           (MatchBlock,[("blockid1",None),
-                        ("blockid2",None)
-           ]),
-           (AlignBlock,[("blocks",(list,None)),
-                        ("horizon",None)
-           ]),
-           (PortPos,[("tid",None),
-                     ("pos",None)
-           ]),
-           (CCCap,[("size",(list,None)),
-                   ("CCCap_name",None),
-                   ("Unit_capacitor",None),
-                   ("cap_ratio",None),
-                   ("cap_r",None),
-                   ("cap_s",None)
-           ]),
-]
+
+def ff( x):
+    def f(x):
+        return None if x is None else globals()[x]
+    if isinstance( x, tuple):
+        assert x[0] is list
+        return (list, f(x[1]))
+    else:
+        return f(x)
+
+structs = [ (globals()[k], [(nm,ff(vv)) for (nm,vv) in v]) for (k,v) in abstract_structs]
 
 class PnRDBEncoder(json.JSONEncoder):
     def default(self, obj):
