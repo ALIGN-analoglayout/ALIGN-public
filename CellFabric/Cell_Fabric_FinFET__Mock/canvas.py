70cbd2f5d09cc2ef8c2773ff1ee687d9654051d5
Parijat Mukherjee <parijat.mukherjee@intel.com>
2019-09-13 14:00:28 -0700

    [canvas] Reduce LOC

--- CellFabric/Cell_Fabric_FinFET__Mock/canvas.py
+++ CellFabric/Cell_Fabric_FinFET__Mock/canvas.py
@@ -44,18 +44,17 @@ class FinFET_Mock_PDK_Canvas(DefaultCanvas):
         self.active = self.addGen( Wire( 'active', 'active', 'h',
                                          clg=UncoloredCenterLineGrid( pitch=activePitch, width=activeWidth, offset=activeOffset),
                                          spg=EnclosureGrid( pitch=unitCellLength, offset=0, stoppoint=stoppoint, check=True)))
-        
-        
+
         self.RVT = self.addGen( Wire( 'RVT', 'polycon', 'h',
                                       clg=UncoloredCenterLineGrid( pitch=activePitch, width=RVTWidth, offset=activeOffset),
                                       spg=EnclosureGrid( pitch=unitCellLength, offset=0, stoppoint=stoppoint, check=True)))
-        
+
         stoppoint = activeOffset-activeWidth//2
         self.LISD = self.addGen( Wire( 'LISD', 'LISD', 'v',
                                          clg=UncoloredCenterLineGrid( pitch=p['M1']['Pitch'], width=p['Feol']['LISDWidth'], offset=p['M1']['Offset']),
                                          spg=EnclosureGrid( pitch=self.unitCellHeight, offset=0, stoppoint=stoppoint, check=True)))
- 
-        stoppoint = gateDummy*p['Poly']['Pitch']+p['Poly']['Offset']-p['Feol']['PcExt']-p['Poly']['Width']//2      
+
+        stoppoint = gateDummy*p['Poly']['Pitch']+p['Poly']['Offset']-p['Feol']['PcExt']-p['Poly']['Width']//2
         self.pc = self.addGen( Wire( 'pc', 'pc', 'h',
                                          clg=UncoloredCenterLineGrid( pitch=activePitch, width=p['Feol']['PcWidth'], offset=p['M2']['Pitch']),
                                          spg=EnclosureGrid( pitch=unitCellLength, offset=0, stoppoint=stoppoint, check=True)))
@@ -85,51 +84,45 @@ class FinFET_Mock_PDK_Canvas(DefaultCanvas):
 
     def _gen_abstract_MOS( self, x, y, x_cells, y_cells, fin_u, fin, finDummy, gate, gateDummy, SDG, Routing):
 
-        (SA, GA, DA, SB, GB, DB) = SDG
-        (S, D, G) = (SA+SB, DA+DB, GA+GB)
-        #####   This part generats locations of S/D/G   #####
-        gu = self.gatesPerUnitCell
-        h = self.m2PerUnitCell
-                       
-        self.addWire( self.active, None, None, y, (x,1), (x+1,-1)) 
+        self.addWire( self.active, None, None, y, (x,1), (x+1,-1))
         self.addWire( self.RVT,    None,    None, y, (x, 1), (x+1, -1))
         self.addWire( self.pc, None, None, y, (x,1), (x+1,-1))
- 
- 
+
         for i in range(1,  self.finsPerUnitCell):
             self.addWire( self.fin, None, None,  self.finsPerUnitCell*y+i, x, x+1)
 
-        #####   Gate Placement   #####                       
-        for i in range(gu):        
-            self.addWire( self.pl, None, None, gu*x+i,   (y,0), (y,1))
+        #####   Gate Placement   #####
+        for i in range(self.gatesPerUnitCell):
+            self.addWire( self.pl, None, None, self.gatesPerUnitCell*x+i,   (y,0), (y,1))
+
+        #####   This part generats locations of S/D/G   #####
+        (SA, GA, DA, SB, GB, DB) = SDG
+        (S, D, G) = (SA+SB, DA+DB, GA+GB)
 
         if x_cells-1==x:
-            grid_y0 = y*h + finDummy//2-1
+            grid_y0 = y*self.m2PerUnitCell + finDummy//2-1
             grid_y1 = grid_y0+(fin+2)//2
             for i in G:
                 self.addWire( self.m1, None, None, i, (grid_y0, -1), (grid_y1, 1))
-                self.addVia( self.va, None, None, i, (y*h//2, 1))
+                self.addVia( self.va, None, None, i, (y*self.m2PerUnitCell//2, 1))
             for i in S+D:
                 self.addWire( self.m1, None, None, i, (grid_y0, -1), (grid_y1, 1))
-                self.addWire( self.LISD, None, None, i, (y, 1), (y+1, -1)) 
+                self.addWire( self.LISD, None, None, i, (y, 1), (y+1, -1))
                 for j in range((((fin-fin_u)//2 +finDummy+3)//2),self.v0.h_clg.n):
                     self.addVia( self.v0, None, None, i, (y, j))
 
-            #pin = 'VDD' if y%2==0 else 'GND'    
-            #self.addWire( self.m2, pin, pin, h*(y+1), (0, 1), (x_cells*gu, -1))
-            #self.addWire( self.m2, 'GND', 'GND', 0, (0, 1), (x_cells*gu, -1))                   
             for (pin, contact, track, m3route) in Routing:
-                self.addWire( self.m2, pin, pin, y*h+track, (min(contact), -1), (max(contact), 1))
+                self.addWire( self.m2, pin, pin, y*self.m2PerUnitCell+track, (min(contact), -1), (max(contact), 1))
                 if y_cells > 1:
-                   self.addWire( self.m3, pin, pin, m3route, (track, -1), (y*h+track, 1))
+                   self.addWire( self.m3, pin, pin, m3route, (track, -1), (y*self.m2PerUnitCell+track, 1))
                    self.addVia( self.v2, None, None, m3route, track)
-                   self.addVia( self.v2, None, None, m3route, y*h+track)
+                   self.addVia( self.v2, None, None, m3route, y*self.m2PerUnitCell+track)
 
                 for i in contact:
-                    self.addVia( self.v1, None, None, i, y*h+track) 
+                    self.addVia( self.v1, None, None, i, y*self.m2PerUnitCell+track)
 
     def genNMOS( self, x, y, x_cells, y_cells, fin_u, fin, finDummy, gate, gateDummy, SDG, Routing):
-        
+
         self._gen_abstract_MOS(x, y, x_cells, y_cells, fin_u, fin, finDummy, gate, gateDummy, SDG, Routing)
 
         #####   Nselect Placement   #####
@@ -141,6 +134,6 @@ class FinFET_Mock_PDK_Canvas(DefaultCanvas):
         self._gen_abstract_MOS(x, y, x_cells, y_cells, fin_u, fin, finDummy, gate, gateDummy, SDG, Routing)
 
         #####   Pselect and Nwell Placement   #####
-        if x == x_cells -1 and y == y_cells -1:      
+        if x == x_cells -1 and y == y_cells -1:
             self.addRegion( self.pselect, None, None, (0, -1), 0, ((1+x)*self.gatesPerUnitCell, -1), (y+1)* self.finsPerUnitCell)
             self.addRegion( self.nwell, None, None, (0, -1), 0, ((1+x)*self.gatesPerUnitCell, -1), (y+1)* self.finsPerUnitCell)    
