0acb1449a00916a7b973fd17586576659547f390
Steven Burns <smburns47@gmail.com>
2019-10-04 14:45:54 -0700

    Added scripts to do DRC of simple OTA circuits

--- PnRPython/run_cascode_current_mirror_ota.sh
+++ PnRPython/run_cascode_current_mirror_ota.sh
@@ -6,6 +6,4 @@ for blk in CASCODED_CMC_NMOS CASCODED_CMC_PMOS $DESIGN; do
 ./gen_viewer_json.py -b $blk -d $ALIGN_WORK_DIR/$DESIGN/pnr_output/Results -l INFO  --json_dir $ALIGN_WORK_DIR/$DESIGN/pnr_output/inputs/ --check
 done
 
-./gen_viewer_json.py -b $DESIGN -o ../Viewer/INPUT -d $ALIGN_WORK_DIR/$DESIGN/pnr_output/Results -l INFO  --json_dir $ALIGN_WORK_DIR/$DESIGN/pnr_output/inputs/ --check
-
 ./gen_viewer_json.py -b $DESIGN -o ../Viewer/INPUT -d $ALIGN_WORK_DIR/$DESIGN/pnr_output/Results -l INFO  --json_dir $ALIGN_WORK_DIR/$DESIGN/pnr_output/inputs/ --draw_grid
