ef6e6796610a252ba9a2b27d8e7d6c9bbbc9a55b
Steven Burns <smburns47@gmail.com>
2019-10-04 14:34:23 -0700

    Use ENV var for work dir

--- PnRPython/run_cascode_current_mirror.sh
+++ PnRPython/run_cascode_current_mirror.sh
@@ -3,7 +3,9 @@
 export DESIGN=cascode_current_mirror_ota
 
 for blk in CASCODED_CMC_NMOS CASCODED_CMC_PMOS $DESIGN; do
-./gen_viewer_json.py -b $blk -d ../compose/tmp/$DESIGN/pnr_output/Results -l INFO  --json_dir ../compose/tmp/$DESIGN/pnr_output/inputs/ --check
+./gen_viewer_json.py -b $blk -d $ALIGN_WORK_DIR/$DESIGN/pnr_output/Results -l INFO  --json_dir $ALIGN_WORK_DIR/$DESIGN/pnr_output/inputs/ --check
 done
 
-./gen_viewer_json.py -b $DESIGN -o ../Viewer/INPUT -d ../compose/tmp/$DESIGN/pnr_output/Results -l INFO  --json_dir ../compose/tmp/$DESIGN/pnr_output/inputs/ --check
+./gen_viewer_json.py -b $DESIGN -o ../Viewer/INPUT -d $ALIGN_WORK_DIR/$DESIGN/pnr_output/Results -l INFO  --json_dir $ALIGN_WORK_DIR/$DESIGN/pnr_output/inputs/ --check
+
+./gen_viewer_json.py -b $DESIGN -o ../Viewer/INPUT -d $ALIGN_WORK_DIR/$DESIGN/pnr_output/Results -l INFO  --json_dir $ALIGN_WORK_DIR/$DESIGN/pnr_output/inputs/ --draw_grid
