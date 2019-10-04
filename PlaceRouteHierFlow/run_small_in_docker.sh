ec039cc5924880d61f0d68d1520db1655a360140
desmond <desmond.a.kirkpatrick@intel.com>
2019-10-04 23:44:00 +0000

    BUG found in run_small that blocked coverage.

--- PlaceRouteHierFlow/run_small_in_docker.sh
+++ PlaceRouteHierFlow/run_small_in_docker.sh
@@ -15,7 +15,7 @@ lcov -z ; \
 (cd cap_placer && ./unit_tests --gtest_output=xml:junit.xml) && \
 (cd placer && ./unit_tests --gtest_output=xml:junit.xml) && \
 (cd router && ./unit_tests --gtest_output=xml:junit.xml) && \
-./pnr_compiler ./testcase_example switched_capacitor_filter.lef switched_capacitor_filter.v switched_capacitor_filter.map layers.json switched_capacitor_filter 2 0 > PnR.log && \
+./pnr_compiler ./testcase_example switched_capacitor_filter.lef switched_capacitor_filter.v switched_capacitor_filter.map layers.json switched_capacitor_filter 2 0 > PnR.log ; \
 lcov --capture --directory . --output-file coverage.info ; \
 genhtml coverage.info --output-directory coverage.out"
 
