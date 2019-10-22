e47ebf6c442d7129b913d1ac5b51aae5ff715601
Parijat Mukherjee <parijat.mukherjee@intel.com>
2019-10-22 14:06:29 -0700

    [Circuit] Start on parser

--- Circuit/circuit/parser.py
+++ Circuit/circuit/parser.py
@@ -33,36 +33,15 @@ class SpiceParser:
             if tok.type not in ['WS', 'COMMENT', 'CONTINUE', 'EMPTY']:
                 yield tok
 
-    def _nexttoken(self):
-        'Advance one token ahead'
-        return next(self._tokens, None)
-
     def parse(self, text):
-        self._tokens = self._generate_tokens(text)
-        tok = self._nexttoken()
-        if tok.type == 'PLUS':
-            self._cache_statement()
-        elif tok.type == 'DECL':
-            self._dispatch()
-            self._process_declaration(tok.value)
-        else:
-            self._dispatch()
-            self._process_instance(tok.value)
+        cache = []
+        tokens = self._generate_tokens(text)
+        for tok in tokens:
+            if tok.type == 'NEWL':
+                self._dispatch(cache)
+                cache = []
+            else:
+                cache.append(tok)
 
-    def _cache():
-        if self._instance_cache is not None:
-            cache = self._instance_cache
-        elif self._declaration_cache is not None:
-            cache = self._declaration_cache
-        else:
-            raise AssertionError
-        tok = self._nexttoken()
-        while tok is not None:
-            cache.append(tok)
-            tok = self._nexttoken()
-
-    def _dispatch():
-        if self._instance_cache is not None:
-            pass
-        if self._declaration_cache is not None:
-            pass
+    def _dispatch(self, cache):
+        pass
