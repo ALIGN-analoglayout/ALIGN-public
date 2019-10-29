0334c012abeea4642d58c25f8c9447d8772c421a
Parijat Mukherjee <parijat.mukherjee@intel.com>
2019-10-29 11:05:08 -0700

    [CellFabric] Refactor pex net names [CellFabric] Fix pex nfins

--- CellFabric/cell_fabric/pex.py
+++ CellFabric/cell_fabric/pex.py
@@ -82,7 +82,7 @@ class ParasiticExtraction():
             node1 = self._gen_netcell_node_name(net, self.canvas.pdk[layer]['Stack'][0], x, y)
             node2 = self._gen_netcell_node_name(net, self.canvas.pdk[layer]['Stack'][1], x, y)
         elif self.canvas.pdk[layer]['Stack'][0] is None:
-            node1 = f'{terminal[0].replace("/", "_")}_{terminal[1]}'
+            node1 = f'net_{terminal[0].replace("/", "_")}_{terminal[1]}'
             node2 = self._gen_netcell_node_name(net, self.canvas.pdk[layer]['Stack'][1], x, y)
         else:
             raise NotImplementedError
@@ -173,5 +173,5 @@ class ParasiticExtraction():
 
         for inst, v in self.canvas.rd.subinsts.items():
             inst = inst.replace("/", "_")
-            fp.write( f"{inst}_0 {inst}_D {inst}_G {inst}_diff {v.parameters['model']} w={v.parameters['width']} l={v.parameters['length']} nfin={v.parameters['nfin']*2}\n")
-            fp.write( f"{inst}_1 {inst}_diff {inst}_G {inst}_S {v.parameters['model']} w={v.parameters['width']} l={v.parameters['length']} nfin={v.parameters['nfin']*2}\n")
+            fp.write( f"{inst}_0 net_{inst}_D net_{inst}_G net_{inst}_diff {v.parameters['model']} w={v.parameters['width']} l={v.parameters['length']} nfin={v.parameters['nfin']}\n")
+            fp.write( f"{inst}_1 net_{inst}_diff net_{inst}_G net_{inst}_S {v.parameters['model']} w={v.parameters['width']} l={v.parameters['length']} nfin={v.parameters['nfin']}\n")
