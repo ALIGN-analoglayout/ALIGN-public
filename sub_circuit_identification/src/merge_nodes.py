fae9c4dd80db6cc9b241a970b93de8848be2e6c0
Parijat Mukherjee <parijat.mukherjee@intel.com>
2019-11-15 16:49:52 -0800

    [sub_circuit_identification] Check and pass model

--- sub_circuit_identification/src/merge_nodes.py
+++ sub_circuit_identification/src/merge_nodes.py
@@ -52,6 +52,8 @@ def merge_nodes(G, hier_type, argv, matched_ports):
                 ports[ele] = G[node][ele]["weight"]
 
         #G.add_edge(new_node,ele,weight=wt)
+    max_value['model'] = G.nodes[node]["real_inst_type"]
+    assert all(G.nodes[node]["real_inst_type"] == max_value["model"] for node in argv)
     new_node = new_node[1:]
     G.add_node(new_node,
                inst_type=hier_type,
