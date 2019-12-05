0c513a908fd1fc4af830fde3e13d9bd4ef788693
arvuce22 <aksharma@umn.edu>
2019-12-05 16:11:38 -0600

    removed body pin from Caps

--- PDK_Abstraction/FinFET14nm_Mock_PDK/gen_lef.py
+++ PDK_Abstraction/FinFET14nm_Mock_PDK/gen_lef.py
@@ -45,7 +45,11 @@ def json_lef(input_json,out_lef,cell_pin):
 
     fp.write( "  SIZE %s BY %s ;\n" % ( s(j['bbox'][2]), s(j['bbox'][3])))
     cell_pin = list(cell_pin)
-    cell_pin.append('B') ### add body contact to the pin list
+    if cell_pin[0] != "PLUS":
+      cell_pin.append('B') ### add body contact to the pin list of transistors
+    else:
+      pass 
+
     for i in cell_pin:
       fp.write( "  PIN %s\n" % i)
         #fp.write( "    DIRECTION %s ;\n" % obj['ported'])
