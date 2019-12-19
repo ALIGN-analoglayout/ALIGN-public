8916f272066b0bb4bb8a21f47a4d328b14a29d07
Yaguang <liyg@tamu.edu>
2019-12-19 15:32:38 -0700

    updates

--- PlaceRouteHierFlow/router/GcellGlobalRouter.cpp
+++ PlaceRouteHierFlow/router/GcellGlobalRouter.cpp
@@ -237,7 +237,7 @@ void GcellGlobalRouter::PlaceTerminal(){
   }
 
   std::cout<<"terminal test 1"<<std::endl;
-  int times = 4;
+  int times = 5;
   Determine_Terminal_Center(horizontal_index, vertical_index, times);
   std::cout<<"terminal test 1.1"<<std::endl;
   std::cout<<"terminal test 2"<<std::endl;
