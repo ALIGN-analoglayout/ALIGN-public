78d2a073ff16c50d69cc16b10a5e655c02b1a672
Parijat Mukherjee <parijat.mukherjee@intel.com>
2019-12-24 12:09:17 -0800

    [setup] Single source version number

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
+import os
+
 from setuptools import setup, find_packages
 
+def get_version(pkg_path):
+    with open(os.path.join(pkg_path, '__init__.py'), 'r') as fp:
+        for line in fp:
+            if line.startswith('__version__'):
+                return line.split('"' if '"' in line else "'")[1]
+
 setup(name='align',
-      version='0.1',
+      version=get_version(
+            os.path.join(
+                  os.path.abspath(os.path.dirname(__file__)),
+                  'align')),
       description='Analog Layout Synthesis Package',
       url='ALIGN-analoglayout/ALIGN-public.git',
       author='Parijat Mukherjee',
       author_email='parijat.mukherjee@intel.com',
-      license='MIT',
-      packages=find_packages(),
-      setup_requires=["pytest-runner"],
-      tests_require=["pytest"],
+      license='BSD-3-Clause',
+      packages=find_packages(include=['align', 'align.*']),
+      install_requires=['networkx>=2.4'],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+      python_requires='~=3.6',
       zip_safe=False)
