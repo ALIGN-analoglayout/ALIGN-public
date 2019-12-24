c35fce09912dfaefb7d08105fc7ebd2412e174ea
Soner Yaldiz <soner.yaldiz@intel.com>
2019-12-24 20:12:07 +0000

    Minor changes

--- Cktgen/runit-comparator.sh
+++ Cktgen/runit-comparator.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 
-(cd Intel; python generate_comparator_v2.py)
+(cd Intel; python3 generate_comparator_v2.py)
 
 docker run --mount source=comparatorInputVol,target=/INPUT --name sam ubuntu
 docker cp Intel/__json sam:INPUT
 docker cp Intel/__json_grs sam:INPUT
 docker rm sam
 
-docker build -t cktgen .
+docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy -t cktgen .
 
 ./flow.sh $* -sgr -p 8086 -iv comparatorInputVol -ov comparatorOutputVol -s "-m cktgen.cktgen_physical_from_json" -src comparator -td Intel/DR_COLLATERAL_Generator/22nm --placer_json INPUT/__json --gr_json INPUT/__json_grs --no_interface
