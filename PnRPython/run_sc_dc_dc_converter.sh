9cb763ae182958d1293f9fbe2b60622877bbaf3c
Parijat Mukherjee <parijat.mukherjee@intel.com>
2019-12-27 14:06:51 -0800

    [align] Remove CellFabric, GDSConv, sci, PnRPython top-level directories

--- PnRPython/run_sc_dc_dc_converter.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export DESIGN=sc_dc_dc_converter
-
-./gen_viewer_json.py -b $DESIGN -o ../Viewer/INPUT -d $ALIGN_WORK_DIR/$DESIGN/pnr_output/Results -l INFO  --json_dir $ALIGN_WORK_DIR/$DESIGN/pnr_output/inputs/ --draw_grid
