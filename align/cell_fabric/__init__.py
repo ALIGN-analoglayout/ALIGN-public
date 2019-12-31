f9736a424d9d4d03909c5679f3c071b70efd5cea
Parijat Mukherjee <parijat.mukherjee@intel.com>
2019-12-31 13:31:46 -0800

    [cell_fabric] Move shared primitive generation code from pdks to cell_fabric

--- align/cell_fabric/__init__.py
+++ align/cell_fabric/__init__.py
@@ -2,6 +2,7 @@ from .grid import *
 from .generators import *
 from .canvas import Canvas
 from .default import DefaultCanvas
+from .primitive import DefaultPrimitiveGenerator
 from .pdk import Pdk
 
 from .primitive import generate_primitive
