2506d62fc6d0dab99584de735d2967b7cc8d52cd
Parijat Mukherjee <parijat.mukherjee@intel.com>
2020-01-02 15:33:26 -0800

    [primitive] Add capacitor generator

--- align/cell_fabric/primitive.py
+++ align/cell_fabric/primitive.py
@@ -3,6 +3,7 @@ import sys
 import pathlib
 import logging
 import collections
+import math
 
 logger = logging.getLogger(__name__)
 
@@ -167,6 +168,76 @@ class DefaultPrimitiveGenerator():
         self.addRegion( self.pselect, None, None, (0, -1), 0, (x_cells*self.gatesPerUnitCell, -1), y_cells* self.finsPerUnitCell+self.lFin)
         self.addRegion( self.nwell, None, None, (0, -1), 0, (x_cells*self.gatesPerUnitCell, -1), y_cells* self.finsPerUnitCell+self.lFin)
 
+    def addCap( self, unit_cap):
+
+        x_length = float((math.sqrt(unit_cap/2))*1000)
+        y_length = float((math.sqrt(unit_cap/2))*1000)  
+
+        c_m1_p = self.pdk['Cap']['m1Pitch']
+        c_m1_w = self.pdk['Cap']['m1Width']
+        c_m2_p = self.pdk['Cap']['m2Pitch']
+        c_m2_w = self.pdk['Cap']['m2Width']
+        m1_p = self.pdk['M1']['Pitch']
+        m2_p = self.pdk['M2']['Pitch']
+
+        print( f"Pitches {c_m1_p} {c_m2_p} {m1_p} {m2_p}")
+
+        def compute( l, p, w):
+            # this is nonsense but if l is a multiple of 2p say 2kp, then 2kp+p-w/(2p) is always k
+            return int( 2*round(  (l+p-w)/(2.0*p) ))
+
+        x_number = compute( x_length, c_m1_p, c_m1_w)
+        y_number = compute( y_length, c_m2_p, c_m2_w)
+
+        print( f"Number of wires {x_number} {y_number}")
+
+        def roundup( x, p):
+            return (x+p-1)//p
+
+        last_y1_track = roundup( (y_number-1)*c_m2_p, m2_p)
+        last_x1_track = roundup( (x_number-1)*c_m1_p, m1_p)
+
+        grid_y0 = 0
+        grid_y1 = grid_y0 + last_y1_track
+
+        for i in range(x_number-1):
+            grid_x = i
+            net = 'PLUS' if i%2 == 1 else 'MINUS'
+            self.addWire( self.m1n, net, None, grid_x, (grid_y0, -1), (grid_y1, 1))
+            self.addWire( self.m3n, net, None, grid_x, (grid_y0, -1), (grid_y1, 1))
+
+            grid_y = ((i+1)%2)*grid_y1
+
+            self.addVia( self.v1_nx, net, None, grid_x, grid_y)
+            self.addVia( self.v2_nx, net, None, grid_x, grid_y)
+
+        pin = 'PLUS'
+        # Don't port m1 per Yaguang instructions
+        self.addWire( self.m1, 'PLUS', None, last_x1_track, (grid_y0, -1), (grid_y1, 1))
+        # don't port m3 (or port one or the other)
+        self.addWire( self.m3, 'PLUS', None, last_x1_track, (grid_y0, -1), (grid_y1, 1))
+
+        grid_x0 = 0
+        grid_x1 = grid_x0 + last_x1_track
+
+        for i in range(y_number-1):
+            grid_x = ((i+1)%2)*grid_x1
+            net = 'PLUS' if i%2 == 0 else 'MINUS'
+            self.addVia( self.v1_xn, net, None, grid_x, i)
+            self.addVia( self.v2_xn, net, None, grid_x, i)
+            pin = 'PLUS' if i == 0 else None
+            self.addWire( self.m2n, net, pin, i, (grid_x0, -1), (grid_x1, 1))
+
+        pin = 'MINUS'
+        self.addWire( self.m2, 'MINUS', pin, last_y1_track, (grid_x0, -1), (grid_x1, 1))
+
+        self.addRegion( self.boundary, 'Boundary', None,
+                        0, 0,
+                        last_x1_track,
+                        last_y1_track)
+
+        print( "Computed Boundary:", self.terminals[-1], self.terminals[-1]['rect'][2], self.terminals[-1]['rect'][2]%80)
+
 def get_xcells_pattern( primitive, pattern, x_cells):
     if any(primitive.startswith(f'{x}_') for x in ["CM", "CMFB"]):
         # Dual transistor (current mirror) primitives
@@ -187,25 +258,11 @@ def get_parameters(primitive, parameters, nfin):
     parameters['nfin'] = nfin
     return parameters
 
-# WARNING: Bad code. Changing these default values breaks functionality.
-def generate_primitive(block_name, primitive=None, pattern=1, height=12, nfin=12, x_cells=1, y_cells=1, parameters=None, pinswitch=0, pdkdir=pathlib.Path.cwd(), outputdir=pathlib.Path.cwd()):
-
-    assert pdkdir.exists() and pdkdir.is_dir(), "PDK directory does not exist"
-    sys.path.insert(0, str(pdkdir))
-    import gen_gds_json
-    import gen_lef
-    from primitive import PrimitiveGenerator
-    sys.path.pop(0)
-
-    fin = height
-    gateDummy = 3 ### Total Dummy gates per unit cell: 2*gateDummy
-    finDummy = 4  ### Total Dummy fins per unit cell: 2*finDummy
-    gate = 2
+# TODO: Pass cell_pin and pattern to this function to begin with
+def generate_MOS_primitive(uc, block_name, primitive, height, nfin, x_cells, y_cells, pattern, parameters, pinswitch):
 
     x_cells, pattern = get_xcells_pattern(primitive, pattern, x_cells)
     parameters = get_parameters(primitive, parameters, nfin)
-
-    uc = PrimitiveGenerator( fin, finDummy, gate, gateDummy)
 
     def gen( pattern, routing):
         if 'NMOS' in primitive:
@@ -285,7 +342,40 @@ def generate_primitive(block_name, primitive=None, pattern=1, height=12, nfin=12
                                  'DB': [('M2', 'D'), ('M1', 'G')]}) 
 
     else:
-        assert False, f"Unrecognized primitive {primitive}"
+        raise NotImplementedError(f"Unrecognized primitive {primitive}")
+
+    return uc, cell_pin
+
+def generate_Cap(uc, block_name, unit_cap):
+    uc.addCap(unit_cap)
+
+    return uc, ['PLUS', 'MINUS']
+
+# WARNING: Bad code. Changing these default values breaks functionality.
+def generate_primitive(block_name, primitive, height=12, x_cells=1, y_cells=1, pattern=1, value=12, parameters=None, pinswitch=0, pdkdir=pathlib.Path.cwd(), outputdir=pathlib.Path.cwd()):
+
+    assert pdkdir.exists() and pdkdir.is_dir(), "PDK directory does not exist"
+    sys.path.insert(0, str(pdkdir))
+    import gen_gds_json
+    import gen_lef
+    from primitive import PrimitiveGenerator
+    sys.path.pop(0)
+
+    # TODO: THIS SHOULD NOT BE NEEDED !!!
+    fin = height
+    gateDummy = 3 ### Total Dummy gates per unit cell: 2*gateDummy
+    finDummy = 4  ### Total Dummy fins per unit cell: 2*finDummy
+    gate = 2
+
+    uc = PrimitiveGenerator( fin, finDummy, gate, gateDummy)
+
+    if 'MOS' in primitive:
+        uc, cell_pin = generate_MOS_primitive(uc, block_name, primitive, height, value, x_cells, y_cells, pattern, parameters, pinswitch)
+    elif 'Cap' in primitive:
+        uc, cell_pin = generate_Cap(uc, block_name, value)
+        uc.setBboxFromBoundary()
+    else:
+        raise NotImplementedError(f"Unrecognized primitive {primitive}")
 
     with open(outputdir / (block_name + '.json'), "wt") as fp:
         uc.writeJSON( fp)
