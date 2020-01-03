85894cae504a31433186957d996485c7fe783ba7
Parijat Mukherjee <parijat.mukherjee@intel.com>
2020-01-02 22:59:41 -0800

    [pnr] More graceful error handling

--- align/pnr/main.py
+++ align/pnr/main.py
@@ -1,6 +1,8 @@
 import subprocess
 import pathlib
 import os
+import logging
+logger = logging.getLogger(__name__)
 
 def generate_pnr(input_dir, lef_file, verilog_file, map_file, pdk_file, design_name, unknownarg1=1, unknownarg2=0):
     assert 'ALIGN_HOME' in os.environ
@@ -8,4 +10,9 @@ def generate_pnr(input_dir, lef_file, verilog_file, map_file, pdk_file, design_n
     assert compiler_path.is_file()
     cmd = [str(x) for x in (compiler_path, input_dir, lef_file, verilog_file, map_file, pdk_file, design_name, unknownarg1, unknownarg2)]
     print(' '.join(cmd))
-    subprocess.run(cmd)
+    try:
+        subprocess.run(cmd, stderr=subprocess.PIPE, check=True)
+    except subprocess.CalledProcessError as e:
+        logger.critical(f"\nCall to '{' '.join(cmd)}' failed with error message:\n\n{e.stderr.decode('utf-8')}")
+        print(f"\nCall to '{' '.join(cmd)}' failed with error message:\n\n{e.stderr.decode('utf-8')}")
+        return None
