21946f4b37f0b436068c282563db39d7c99f8aed
Parijat Mukherjee <parijat.mukherjee@intel.com>
2020-01-03 00:17:49 -0800

    [pnr] Better error message

--- align/pnr/main.py
+++ align/pnr/main.py
@@ -5,11 +5,10 @@ import logging
 logger = logging.getLogger(__name__)
 
 def generate_pnr(input_dir, lef_file, verilog_file, map_file, pdk_file, design_name, unknownarg1=1, unknownarg2=0):
-    assert 'ALIGN_HOME' in os.environ
+    assert 'ALIGN_HOME' in os.environ, f"ALIGN_HOME not in environment"
     compiler_path = pathlib.Path(os.environ['ALIGN_HOME']).resolve() / 'PlaceRouteHierFlow' / 'pnr_compiler'
-    assert compiler_path.is_file()
+    assert compiler_path.is_file(), f"{compiler_path} not found"
     cmd = [str(x) for x in (compiler_path, input_dir, lef_file, verilog_file, map_file, pdk_file, design_name, unknownarg1, unknownarg2)]
-    print(' '.join(cmd))
     try:
         subprocess.run(cmd, stderr=subprocess.PIPE, check=True)
     except subprocess.CalledProcessError as e:
