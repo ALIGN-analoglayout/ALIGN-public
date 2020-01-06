ec38160a450b896cc69a8c6f06634e3614a8cab7
Steven Burns <smburns47@gmail.com>
2020-01-06 12:55:13 -0800

    Comment out short and open visualization

--- align/pnr/checkers.py
+++ align/pnr/checkers.py
@@ -293,24 +293,28 @@ def gen_viewer_json( hN, *, pdk=pathlib.Path(__file__).resolve().parent.parent.p
         d['bbox'] = cnv.bbox.toList()
         d['terminals'] = cnv.terminals
 
-        for (idx,sh) in enumerate(cnv.rd.shorts):
-            if isinstance( sh, tuple) and len(sh) == 2:
-                p0, p1 = sh
-                logger.info( f"SH: {p0} {p1}")
-                term = { "layer": "M0", "netName": f"SH{idx}_{p0.netName}", "rect": p0.rect}
-                d['terminals'].append( term)
-                term = { "layer": "M0", "netName": f"SH{idx}_{p1.netName}", "rect": p1.rect}
-                d['terminals'].append( term)
-            else:
-                logger.error( f"Unknown short type: {sh}")
+        #
+        # Experiment for visualizing shorts and opens
+        # 
+
+        # for (idx,sh) in enumerate(cnv.rd.shorts):
+        #     if isinstance( sh, tuple) and len(sh) == 2:
+        #         p0, p1 = sh
+        #         logger.info( f"SH: {p0} {p1}")
+        #         term = { "layer": "M0", "netName": f"SH{idx}_{p0.netName}", "rect": p0.rect}
+        #         d['terminals'].append( term)
+        #         term = { "layer": "M0", "netName": f"SH{idx}_{p1.netName}", "rect": p1.rect}
+        #         d['terminals'].append( term)
+        #     else:
+        #         logger.error( f"Unknown short type: {sh}")
 
 
-        for (nm,lst) in cnv.rd.opens:
-            logger.info( f"OP: {nm} {lst}")            
-            for (jdx,l) in enumerate(lst):
-                for (ly,r) in l:
-                    term = { "layer": ly, "netName": f"OP_{nm}_{jdx}", "rect": r}
-                    d['terminals'].append( term)
+        # for (nm,lst) in cnv.rd.opens:
+        #     logger.info( f"OP: {nm} {lst}")            
+        #     for (jdx,l) in enumerate(lst):
+        #         for (ly,r) in l:
+        #             term = { "layer": ly, "netName": f"OP_{nm}_{jdx}", "rect": r}
+        #             d['terminals'].append( term)
 
         # multiply by ten make it be in JSON file units (angstroms) This is a mess!
         rational_scaling( d, mul=10)
