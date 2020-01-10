ea45ac5c06ea78001b0f774272f3a567a74b9801
Parijat Mukherjee <parijat.mukherjee@intel.com>
2020-01-10 01:36:50 -0700

    [test_all] Disable bulk nightly

--- tests/integration/test_all.py
+++ tests/integration/test_all.py
@@ -3,31 +3,28 @@ import align
 import os
 import pathlib
 
-pdks= []
-for pdk in (pathlib.Path(__file__).parent.parent.parent / 'pdks').iterdir():
-    if pdk.is_dir() and (pdk / 'layers.json').exists():
-        pdks.append(pdk)
+ALIGN_HOME = pathlib.Path(__file__).parent.parent.parent
+
+pdks= [pdk for pdk in (ALIGN_HOME / 'pdks').iterdir() \
+           if pdk.is_dir() and (pdk / 'layers.json').exists() and 'Bulk' not in pdk.name]
 
 run_flat=['linear_equalizer']
 
-ALIGN_HOME = pathlib.Path(__file__).parent.parent.parent
 examples_dir =  ALIGN_HOME / 'examples'
-examples = []
-examples += [p for p in examples_dir.iterdir() if p.is_dir() and p.name != 'modified_USC_UW_UT_testcases']
-examples += [p in (examples_dir / 'modified_USC_UW_UT_testcases').iterdir() if p.is_dir()]
+
+examples = [p for p in examples_dir.iterdir() if p.is_dir() and p.name != 'modified_USC_UW_UT_testcases']
+# examples += [p for p in (examples_dir / 'modified_USC_UW_UT_testcases').iterdir() if p.is_dir()]
 
 @pytest.mark.nightly
 @pytest.mark.parametrize( "design_dir", examples, ids=lambda x: x.name)
 @pytest.mark.parametrize( "pdk_dir", pdks, ids=lambda x: x.name)
 def test_A( pdk_dir, design_dir):
     nm = design_dir.name
-    run_dir = pathlib.Path( os.environ['ALIGN_WORK_DIR']).resolve() / nm
-
-    run_dir.mkdir( exist_ok=True)
+    run_dir = pathlib.Path( os.environ['ALIGN_WORK_DIR']).resolve() / pdk_dir.name / nm
+    run_dir.mkdir(parents=True, exist_ok=True)
     os.chdir(run_dir)
 
     args = [str(design_dir), '-f', str(design_dir / f"{nm}.sp"), '-s', nm, '-p', str(pdk_dir), '-flat',  str(1 if nm in run_flat else 0), '--check']
-
     results = align.CmdlineParser().parse_args(args)
 
     for result in results:
