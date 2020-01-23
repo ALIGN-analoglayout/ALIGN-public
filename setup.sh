0bb04198c742402b87890e8a57d5853a0323dc2a
Kishor Kunal <kunal001@bose.ece.umn.edu>
2020-01-22 18:27:12 -0600

    checking sanitized

--- setup.sh
+++ setup.sh
@@ -20,15 +20,18 @@ setenv BOOST_LP $ALIGN_HOME/boost
 setenv LD_LIBRARY_PATH $ALIGN_HOME/lpsolve/lp_solve_5.5.2.5_dev_ux64/
 setenv JSON $ALIGN_HOME/json
 setenv GTEST_DIR $ALIGN_HOME/googletest/googletest/
+setenv VENV $ALIGN_HOME/general
 python3.6 -m venv general
 source general/bin/activate.csh
+pip install --upgrade pip
+pip install wheel pytest general networkx pygraphviz coverage pytest-cov protobuf matplotlib pyyaml python-gdsii
+pip install -e .
+deactivate
 cd $ALIGN_HOME/PlaceRouteHierFlow/
 make
 cd ..
-pip install -e .
 mkdir work
-pip install --upgrade pip
-pip install wheel pytest general networkx pygraphviz coverage pytest-cov protobuf matplotlib pyyaml python-gdsii
 cd $ALIGN_WORK_DIR
 ln -s $ALIGN_HOME/build/Makefile .
+
 make VENV=$VENV
