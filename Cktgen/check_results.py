fb5f66800a9399798ee7fc07c1d8054bddb06bce
Steven Burns <smburns47@gmail.com>
2020-01-23 14:10:23 -0800

    First attempt

---
+++ Cktgen/check_results.py
@@ -0,0 +1,54 @@
+
+import json
+import pathlib
+import re
+
+from align.cell_fabric.gen_gds_json import translate_data
+from align.cell_fabric import pdk
+
+if __name__ == "__main__":
+    with open("comparator.json", "rt") as fp:
+        d = json.load(fp)
+
+    skip_layers = set( ["boundary", "diearea", "cellarea"])
+
+    layer_tbl = { "poly": "Poly",
+                  "diffcon": "Diffcon",
+                  "polycon": "Polycon",
+                  "nwell": "Nwell",
+                  "pdiff": "Pdiff",
+                  "ndiff": "Ndiff",
+                  "metal1": "M1",
+                  "metal2": "M2",
+                  "metal3": "M3",
+                  "via0": "V0",
+                  "via1": "V1",
+                  "via2": "V2"}
+
+
+    p = re.compile( "^(.*)_gr$")
+
+    terminals = []
+    for term in d['terminals']:
+        ly = term['layer']
+        if ly in skip_layers: continue
+        nm = term['netName'] if 'netName' in term else term['net_name']
+        if p.match(nm): continue
+        term['layer'] = layer_tbl.get( ly, ly)
+        terminals.append( term)
+    d['terminals'] = terminals
+
+    pdkfile = pathlib.Path('Intel/Intel_P1222p2_PDK/layers.json')
+    p = pdk.Pdk().load(pdkfile)
+
+    cnv = generate canvas from p
+
+    cnv.bbox = transformation.Rect( *d['bbox'])
+    cnv.terminals = d['terminals']
+
+    cnv.gen_data(run_pex=False)
+    
+    assert len(cnv.rd.different_widths) == 0, pformat(cnv.rd.different_widths)
+    assert len(cnv.rd.shorts) == 0, pformat(cnv.rd.shorts)
+    assert len(cnv.rd.opens) == 0, pformat(cnv.rd.opens)
+    assert len(cnv.drc.errors) == 0, pformat(cnv.drc.errors)
