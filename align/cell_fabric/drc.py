f212f80300efeca5b156a0177e4929f078b62b96
Steven Burns <smburns47@gmail.com>
2020-01-24 06:40:11 -0800

    First attempt and enclosure drc; will fail in general

--- align/cell_fabric/drc.py
+++ align/cell_fabric/drc.py
@@ -24,6 +24,7 @@ class DesignRuleCheck():
                 continue
             if self.canvas.rd.layers[layer] == '*':
                 self._check_via_rules(layer, vv)
+                self._check_via_enclosure_rules(layer, vv)
             else:
                 self._check_metal_rules(layer, vv)
         for error in self.errors:
@@ -34,6 +35,38 @@ class DesignRuleCheck():
         '''TODO : Add via pattern checking rules '''
         space = self.canvas.pdk[layer]['SpaceX']
         return space
+
+    def _check_via_enclosure_rules(self, layer, vv):
+        '''Check via enclosures.'''
+        v = self.canvas.pdk[layer]
+        [ly_l,ly_u] = v['Stack']
+        [ml,mu] = [self.canvas.pdk[ly] for ly in [ly_l,ly_u]]
+        assert ml['Direction'].upper() in ['V','H']
+        assert mu['Direction'].upper() in ['V','H']
+
+        for c2, sl in vv.items():
+            for r in sl.rects:
+                cx2 = r.rect[0]+r.rect[2]
+                cy2 = r.rect[1]+r.rect[3]
+                
+                idx,o = (cx2,1) if ml['Direction'].upper() == 'V' else (cy2,0)
+                # find rect covering via
+                l_sl = self.canvas.rd.store_scan_lines[ly_l][idx]
+                assert len(l_sl.rects) == 1
+                metal_r = l_sl.rects[0].rect
+                # check enclosure rules
+                if metal_r[o+0] > r.rect[o+0] - v['VencA_L'] or metal_r[o+2] < r.rect[o+2] + v['VencA_L']:
+                    self.errors.append( f"Enclosure error: {metal_r} does not sufficiently surround {r.rect}")
+
+                idx,o = (cx2,1) if mu['Direction'].upper() == 'V' else (cy2,0)
+                # find rect covering via
+                u_sl = self.canvas.rd.store_scan_lines[ly_u][idx]
+                assert len(u_sl.rects) == 1
+                metal_r = u_sl.rects[0].rect
+                # check enclosure rules
+                if metal_r[o+0] > r.rect[o+0] - v['VencA_H'] or metal_r[o+2] < r.rect[o+2] + v['VencA_H']:
+                    self.errors.append( f"Enclosure error: {v} {metal_r} does not sufficiently surround {r.rect}")
+
 
     def _check_metal_rules(self, layer, vv):
         '''Check metal min-length / min-spacing rules'''
