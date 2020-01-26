71e2898515719314071fd7e57450759db0d11c2d
kunal <kkunal1408@gmail.com>
2020-01-25 20:50:58 -0600

    doc updates

--- docs/sphinx_docs/source/conf.py
+++ docs/sphinx_docs/source/conf.py
@@ -18,8 +18,8 @@ sys.path.insert(0, os.path.abspath('../../'))
 # -- Project information -----------------------------------------------------
 
 project = 'ALIGN'
-copyright = '2020, Kunal and ALIGN team'
-author = 'Kunal and ALIGN team'
+copyright = '2020, ALIGN team'
+author = 'ALIGN team'
 
 # The full version, including alpha/beta/rc tags
 release = '0.9'
