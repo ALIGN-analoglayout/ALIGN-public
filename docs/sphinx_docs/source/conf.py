350ce21fb36ca3a2748ce941c281e9d19acabdf6
kunal <kkunal1408@gmail.com>
2020-01-27 00:28:43 -0600

    doc updates

--- docs/sphinx_docs/source/conf.py
+++ docs/sphinx_docs/source/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 # -- Project information -----------------------------------------------------
 
 project = 'ALIGN'
-copyright = '2020, ALIGN team'
+copyright = '2020, ALIGN team' # pylint: disable=redefined-builtin
 author = 'ALIGN team'
 
 # The full version, including alpha/beta/rc tags
