56e0124637b6b118afadc2b5e0c754d07d8bfce3
Steve Burns <steven.m.burns@intel.com>
2020-02-13 22:42:37 +0000

    Removed debug message

--- Cktgen/cktgen/cktgen/cktgen_physical_from_json.py
+++ Cktgen/cktgen/cktgen/cktgen_physical_from_json.py
@@ -396,7 +396,6 @@ def main(args, tech):
   if 'preroutes' in placer_results:
     preroutes = placer_results['preroutes']
     for preroute in preroutes:
-      print( "PREROUTE", preroute['layer'])
       adnetl.addPreroute( preroute)
 
   adnetl.genNetlist( netl)
