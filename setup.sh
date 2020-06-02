b9b33c0ef3d7a030d9ff2c9497e1c150c0e99a73
Yaguang Li <liyg@tamu.edu>
2020-06-01 19:18:23 -0500

    add path for superlu

--- setup.sh
+++ setup.sh
@@ -11,6 +11,7 @@ export LP_DIR=$ALIGN_HOME/lpsolve
 export JSON=$ALIGN_HOME/json
 export GTEST_DIR=$ALIGN_HOME/googletest/googletest/
 export VENV=$ALIGN_HOME/general
+export SuperLu_DIR=$ALIGN_HOME/superlu
 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH:}$ALIGN_HOME/lpsolve/lp_solve_5.5.2.5_dev_ux64/
 
