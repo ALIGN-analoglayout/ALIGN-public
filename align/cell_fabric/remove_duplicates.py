b634c7a0dce79f7e3ed00c6ac812442d2e1ded70
Steven Burns <smburns47@gmail.com>
2020-07-09 09:36:15 -0700

    Close to working

--- align/cell_fabric/remove_duplicates.py
+++ align/cell_fabric/remove_duplicates.py
@@ -38,30 +38,32 @@ class ScanlineRect(UnionFind):
         return str( (self.rect, self.netName))
 
 class Scanline:
-    def __init__(self, proto, indices, dIndex):
-        self.proto = proto
+    def __init__(self, indices, dIndex):
         self.indices = indices
         self.dIndex = dIndex
         self.rects = []
         self.dad = None
 
-
     def isEmpty(self):
         return len(self.rects) == 0
 
-
-    def new_slr(self, rect, netName, *, isPorted=False):
+    def new_slr_no_add(self, rect, netName, *, isPorted=False):
         slr = ScanlineRect()
-        slr.rect = self.proto[:]
-        slr.rect[self.dIndex] = rect[self.dIndex]
-        slr.rect[self.dIndex+2] = rect[self.dIndex+2]
+        slr.rect = rect[:]
         if netName is not None and ':' in netName:
             slr.terminal = tuple(netName.split(':'))
             assert len(slr.terminal) == 2
         else:
             slr.netName = netName
         slr.isPorted = isPorted
+        return slr
+
+    def add_slr(self, slr):
         self.rects.append(slr)
+        return slr
+
+    def new_slr(self, rect, netName, *, isPorted=False):
+        self.add_slr( self.new_slr_no_add( rect, netName, isPorted=isPorted))
         return slr
 
     def merge_slr(self, base_slr, new_slr):
@@ -220,18 +222,23 @@ class RemoveDuplicates():
                             logger.warning( f"{tup}")
                             self.different_widths.append( tup)
 
-                sl = self.store_scan_lines[layer][twice_center] = Scanline(v[0][0], indices, dIndex)
+                sl = self.store_scan_lines[layer][twice_center] = Scanline( indices, dIndex)
 
                 current_slr = None
                 for (rect, netName, isPorted) in sorted(v, key=lambda p: p[0][dIndex]):
-                    if not sl.isEmpty() and rect[dIndex] <= current_slr.rect[dIndex+2] \
-                            and all(rect[i] == current_slr.rect[i] for i in indices):  # continuation
-                        if self.connectPair(layer,current_slr, sl.new_slr(rect, netName, isPorted=isPorted)):
-                            sl.merge_slr(current_slr, sl.rects.pop())
+                    potential_slr = sl.new_slr_no_add(rect, netName, isPorted=isPorted)
+                    if not sl.isEmpty() and \
+                       rect[dIndex] <= current_slr.rect[dIndex+2] and \
+                       all(rect[i] == current_slr.rect[i] for i in indices):  # continuation
+                        if self.connectPair(layer,current_slr, potential_slr):
+                            sl.merge_slr(current_slr, potential_slr)
                         else:
-                            current_slr = sl.rects[-1]
+                            current_slr = sl.add_slr( potential_slr)
                     else:  # empty or gap or different width
-                        current_slr = sl.new_slr(rect, netName, isPorted=isPorted)
+                        current_slr = sl.add_slr( potential_slr)
+
+                    # invariant (can probably remove current_slr)
+                    assert current_slr == sl.rects[-1]
 
                 if different_widths_in_bin:
                     logger.warning( f"Different widths: {layer} {sl}")
@@ -285,6 +292,8 @@ class RemoveDuplicates():
 
     def generate_rectangles( self):
 
+        logger.debug( f"BEFORE: generate_rectangles {self.canvas.terminals}")
+
         terminals = []
 #
 # Write out regions
@@ -304,6 +313,9 @@ class RemoveDuplicates():
                         terminals[-1]['pin'] = root.netName
                     if slr.terminal is not None:
                         terminals[-1]['terminal'] = slr.terminal
+
+        logger.debug( f"AFTER: generate_rectangles {terminals}")
+
         return terminals
 
     def remove_duplicates( self):
--- tests/cell_fabric/test_canvas_drc_merge_different_widths.py
+++ tests/cell_fabric/test_canvas_drc_merge_different_widths.py
@@ -20,27 +20,59 @@ def setup():
     dy0 = m2['Width']//2
     dy1 = dy0 + 4
 
+    return c,dy0,dy1
+
+
+def test_merged_same(setup):
+    c,dy0,dy1 = setup
+
+    c.terminals = [
+        {'layer': 'M2', 'netName': 'x', 'rect': [    0, -dy0,  200, dy0]},
+        {'layer': 'M2', 'netName': 'x', 'rect': [  200, -dy0,  400, dy0]},
+        {'layer': 'M2', 'netName': 'y', 'rect': [ 1000, -dy0, 1200, dy0]}
+    ]
+
+    assert len(c.terminals) == 3
+
+    data = c.gen_data()
+
+    assert c.drc.num_errors == 0
+
+    gold = [
+        {'layer': 'M2', 'netName': 'x', 'rect': [    0, -dy0,  400, dy0]},
+        {'layer': 'M2', 'netName': 'y', 'rect': [ 1000, -dy0, 1200, dy0]}
+    ]
+
+    assert len(data['terminals']) == 2
+
+def test_merged_diff_after(setup):
+    c,dy0,dy1 = setup
+
     c.terminals = [
         {'layer': 'M2', 'netName': 'x', 'rect': [    0, -dy0,  200, dy0]},
         {'layer': 'M2', 'netName': 'x', 'rect': [  200, -dy0,  400, dy0]},
         {'layer': 'M2', 'netName': 'y', 'rect': [ 1000, -dy1, 1200, dy1]}
     ]
 
-    return c
+    assert len(c.terminals) == 3
 
-@pytest.fixture
-def setup2():
-    p = Pdk().load(pdkfile)
-    c = Canvas(p)
-    c.addGen( Wire( nm='m2', layer='M2', direction='h', clg=None, spg=None))
+    data = c.gen_data()
 
-    m2 = p['M2']
+    assert c.drc.num_errors == 0
 
-    assert 'Width' in m2
-    assert m2['Width'] % 2 == 0
+    gold = [
+        {'layer': 'M2', 'netName': 'x', 'rect': [    0, -dy0,  400, dy0]},
+        {'layer': 'M2', 'netName': 'y', 'rect': [ 1000, -dy1, 1200, dy1]}
+    ]
 
-    dy0 = m2['Width']//2
-    dy1 = dy0 + 4
+    assert len(data['terminals']) == 2
+
+    assert data['terminals'] == gold
+
+
+
+def test_merged_diff_before(setup):
+    c,dy0,dy1 = setup
 
     c.terminals = [
         {'layer': 'M2', 'netName': 'y', 'rect': [ -1000, -dy1, -800, dy1]},
@@ -48,26 +80,16 @@ def setup2():
         {'layer': 'M2', 'netName': 'x', 'rect': [   200, -dy0,  400, dy0]}
     ]
 
-    return c
-
-def test_merged_ok(setup):
-    c = setup
-
     assert len(c.terminals) == 3
 
-    c.gen_data()
+    data = c.gen_data()
 
     assert c.drc.num_errors == 0
-    assert len(c.terminals) == 2
 
-def test_merged_ok2(setup2):
-    c = setup2
+    gold = [
+        {'layer': 'M2', 'netName': 'y', 'rect': [ -1000, -dy1, -800, dy1]},
+        {'layer': 'M2', 'netName': 'x', 'rect': [     0, -dy0,  400, dy0]}
+    ]
 
-    assert len(c.terminals) == 3
-
-    c.gen_data()
-
-    assert c.drc.num_errors == 0
-    assert len(c.terminals) == 2
-
-
+    assert len(data['terminals']) == 2
+    assert data['terminals'] == gold
