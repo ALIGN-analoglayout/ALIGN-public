938f4c8cd55b1089b5f2fbf16b68bcce22d9ae08
Philipp Gühring <pg@futureware.at>
2020-08-18 09:04:48 +0200

    Added VENV support again

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ export LP_DIR=$ALIGN_HOME/lpsolve
 #export BOOST_LP=$ALIGN_HOME/boost
 export JSON=$ALIGN_HOME/json
 export GTEST_DIR=$ALIGN_HOME/googletest/googletest/
-#export VENV=$ALIGN_HOME/align_venv # VENV is optional
+export VENV=$ALIGN_HOME/align_venv # VENV is optional
 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH:}$ALIGN_HOME/lpsolve/lp_solve_5.5.2.5_dev_ux64/
 
