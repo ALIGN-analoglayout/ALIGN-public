d7dfcb8d84ffb5aa2befd0ff48a680383445564b
Yaguang Li <liyg@tamu.edu>
2020-08-19 17:31:18 -0500

    update for comparator_1_1

--- PlaceRouteHierFlow/comparator_1_1/comparator_1_1.v
+++ PlaceRouteHierFlow/comparator_1_1/comparator_1_1.v
@@ -5,15 +5,15 @@
 module comparator_1_1 ( vop, vcc, vin, clk, von, vss, vip ); 
 input vop, vcc, vin, clk, von, vss, vip;
 
-Switch_NMOS_n12_X3_Y3 mn1 ( .D(net1), .G(vip), .S(vss) ); 
-Switch_NMOS_n12_X3_Y3 mn2 ( .D(net2), .G(vin), .S(vss) ); 
-Switch_NMOS_n12_X4_Y2 mn3 ( .D(net3), .G(von), .S(net1) ); 
-Switch_NMOS_n12_X4_Y2 mn4 ( .D(net4), .G(vop), .S(net2) ); 
-Switch_NMOS_n12_X7_Y2 mn5 ( .D(vop), .G(clk), .S(net3) ); 
-Switch_NMOS_n12_X7_Y2 mn6 ( .D(von), .G(clk), .S(net4) ); 
-Switch_PMOS_n12_X5_Y1 mp0 ( .D(vop), .G(von), .S(vcc) ); 
-Switch_PMOS_n12_X5_Y1 mp1 ( .D(von), .G(vop), .S(vcc) ); 
-Switch_PMOS_n12_X5_Y2 mp2 ( .D(vop), .G(clk), .S(vcc) ); 
-Switch_PMOS_n12_X5_Y2 mp3 ( .D(von), .G(clk), .S(vcc) ); 
+Switch_NMOS_n12_X3_Y3 mn1 ( .D(net1), .G(vip), .S(vss), .Bg(vss) ); 
+Switch_NMOS_n12_X3_Y3 mn2 ( .D(net2), .G(vin), .S(vss), .Bg(vss) ); 
+Switch_NMOS_n12_X4_Y2 mn3 ( .D(net3), .G(von), .S(net1), .Bg(vss) ); 
+Switch_NMOS_n12_X4_Y2 mn4 ( .D(net4), .G(vop), .S(net2), .Bg(vss) ); 
+Switch_NMOS_n12_X7_Y2 mn5 ( .D(vop), .G(clk), .S(net3), .Bg(vss) ); 
+Switch_NMOS_n12_X7_Y2 mn6 ( .D(von), .G(clk), .S(net4), .Bg(vss) ); 
+Switch_PMOS_n12_X5_Y1 mp0 ( .D(vop), .G(von), .S(vcc), .Bg(vcc) ); 
+Switch_PMOS_n12_X5_Y1 mp1 ( .D(von), .G(vop), .S(vcc), .Bg(vcc) ); 
+Switch_PMOS_n12_X5_Y2 mp2 ( .D(vop), .G(clk), .S(vcc), .Bg(vcc) ); 
+Switch_PMOS_n12_X5_Y2 mp3 ( .D(von), .G(clk), .S(vcc), .Bg(vcc) ); 
 
 endmodule
