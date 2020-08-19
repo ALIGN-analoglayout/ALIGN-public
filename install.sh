0bd3621f9f32df5486a4f09baf91686e44562e97
Steven Burns <smburns47@gmail.com>
2020-08-19 11:15:46 -0700

    Reverting these to original

--- install.sh
+++ install.sh
@@ -20,41 +20,23 @@ fi
 #-----------------------
 
 #### Install Packages
-$SUDO apt-get update && $SUDO apt-get install -yq libboost-container-dev
-$SUDO apt-get install -yq xfce4
-$SUDO apt-get install -yq \
+$SUDO apt-get update && $SUDO apt-get install -yq \
     python3 \
     python3-pip \
     python3-venv \
     g++\
     cmake \
-    libboost-all-dev \
+    libboost-container-dev \
     graphviz \
     gnuplot \
     curl \
     xvfb \
-    libx11-dev \
-    xorg-dev \
-    libglu1-mesa-dev \
-    freeglut3-dev \
-    libglew1.5 \
-    libglew1.5-dev \
-    libglu1-mesa \
-    libglu1-mesa-dev \
-    libgl1-mesa-glx \
-    libgl1-mesa-dev \
-    lp-solve \
-    liblpsolve55-dev \
-    libgtest-dev \
 &&  $SUDO apt-get clean
 
 #### Install klayout 
 curl -o ./klayout_0.26.3-1_amd64.deb https://www.klayout.org/downloads/Ubuntu-18/klayout_0.26.3-1_amd64.deb
-curl -o ./klayout_0.27.7-1_amd64.deb https://www.klayout.org/downloads/Ubuntu-20/klayout_0.26.7-1_amd64.deb
 $SUDO apt-get install -yq ./klayout_0.26.3-1_amd64.deb
-$SUDO apt-get install -yq ./klayout_0.27.7-1_amd64.deb
 rm ./klayout_0.26.3-1_amd64.deb
-rm ./klayout_0.27.7-1_amd64.deb
 #** WSL users would need to install Xming for the display to work
 
 #### Install lpsolve
@@ -83,15 +65,13 @@ export LP_DIR=$ALIGN_HOME/lpsolve
 #export BOOST_LP=$ALIGN_HOME/boost
 export JSON=$ALIGN_HOME/json
 export GTEST_DIR=$ALIGN_HOME/googletest/googletest/
-export VENV=$ALIGN_HOME/align_venv
+export VENV=$ALIGN_HOME/general
 
 ## Install ALIGN
 #---------------
 
 # Install ALIGN python packages
 cd $ALIGN_HOME
-pip3 install --upgrade pip
-pip3 install -e .
 python3 -m venv $VENV
 source $VENV/bin/activate
 pip install --upgrade pip
@@ -103,13 +83,10 @@ export LD_LIBRARY_PATH=$ALIGN_HOME/lpsolve/lp_solve_5.5.2.5_dev_ux64/
 cd $ALIGN_HOME/PlaceRouteHierFlow/ && make
 cd $ALIGN_HOME
 
-# Setup environment variable script for next time:
-mkdir $ALIGN_WORK_DIR
-cd $ALGIN_WORK_DIR
-ln -s $ALIGN_HOME/build/Makefile .
-
 ## Run first example
 #---------------------
+#mkdir $ALIGN_WORK_DIR
 #cd $ALIGN_WORK_DIR
+#ln -s $ALIGN_HOME/build/Makefile .
 ### for umn: module load gcc/8.2.0
 #make VENV=$VENV
--- setup.sh
+++ setup.sh
@@ -10,19 +10,17 @@ export LP_DIR=$ALIGN_HOME/lpsolve
 #export BOOST_LP=$ALIGN_HOME/boost
 export JSON=$ALIGN_HOME/json
 export GTEST_DIR=$ALIGN_HOME/googletest/googletest/
-export VENV=$ALIGN_HOME/align_venv # VENV is optional
+export VENV=$ALIGN_HOME/general
 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH:}$ALIGN_HOME/lpsolve/lp_solve_5.5.2.5_dev_ux64/
 
-#source $VENV/bin/activate
+source $VENV/bin/activate
 
 echo "ALIGN-public environment is now set up."
 echo "To run a first example, issue the following:"
-#echo "   mkdir -p \$ALIGN_WORK_DIR/telescopic_ota"
-echo "   cd \$ALIGN_WORK_DIR"
-echo "   make DESIGN=telescopic_ota"
-# echo "   cd \$ALIGN_WORK_DIR/telescopic_ota"
-# echo "   schematic2layout.py \$ALIGN_HOME/examples/telescopic_ota -c -p ../../pdks/OSU180_PDK/"
+echo "   mkdir -p \$ALIGN_WORK_DIR/telescopic_ota"
+echo "   cd \$ALIGN_WORK_DIR/telescopic_ota"
+echo "   schematic2layout.py \$ALIGN_HOME/examples/telescopic_ota -c"
 
 
 
