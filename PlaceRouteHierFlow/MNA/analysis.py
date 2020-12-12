f02167fbe28c137aff372dc9cd822063c9e0a500
lastdayends <liyg@tamu.edu>
2020-12-12 11:43:51 -0800

    add script for result analysis

--- PlaceRouteHierFlow/MNA/analysis.py
+++ PlaceRouteHierFlow/MNA/analysis.py
@@ -1,4 +1,3 @@
-#from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 import matplotlib
 from matplotlib import cm
@@ -16,9 +15,9 @@ def readin(filename):
         break
         pass
       lines = [float(i) for i in lines.split()]
-      data.append(lines)  # 添加新读取的数据
+      data.append(lines) 
       pass
-    data = np.array(data) # 将数据从list类型转换为array类型。
+    data = np.array(data) 
 
   return data
 
@@ -31,12 +30,12 @@ def readincurrent(filename):
         break
         pass
       lines = [float(i) for i in lines.split()]
-      data.append(lines)  # 添加新读取的数据
+      data.append(lines)
       pass
   data1 = data[0:2]
   data3 = data1[0]+data1[1]
   data2 = data[2:]
-  data2 = np.array(data2) # 将数据从list类型转换为array类型。
+  data2 = np.array(data2) 
   data3 = np.array(data3)
   return data2,data3
 
@@ -44,7 +43,7 @@ def transferdata(data):
   metal = 2
   #metal2 = 6
   power = 0.8
-  print(data.shape)
+  #print(data.shape)
   X = []
   Y = []
   Z = []
@@ -69,8 +68,8 @@ def transferdata(data):
   line_y  = np.array(y)
 
   Xmesh, Ymesh = np.meshgrid(line_x, line_y)
-  print(Xmesh)
-  print(Ymesh)
+  #print(Xmesh)
+  #print(Ymesh)
   z = np.zeros(Xmesh.shape)
 
   for k in(range(X.shape[0])):
@@ -95,9 +94,9 @@ def transferdata(data):
   for i in(range(line_x.shape[0])):
     for j in(range(line_y.shape[0])):
       if z[j][i]<0.5:
-        print('j= %d i= %d' %(j,i))
+        #print('j= %d i= %d' %(j,i))
         z[j][i]=0.5*(z[j][i-1]+z[j][i+1])
-    print('finish print index %d' %(i))
+    #print('finish print index %d' %(i))
 
   return Xmesh,Ymesh,z,line_x,line_y
 
@@ -105,7 +104,7 @@ def transfercurrent(data,line_x,line_y):
   metal = 2
   #metal2 = 6
   power = 0.8
-  print(data.shape)
+  #print(data.shape)
   X = []
   Y = []
   Z = []
@@ -124,8 +123,8 @@ def transfercurrent(data,line_x,line_y):
   Z = np.array(Z)    
 
   Xmesh, Ymesh = np.meshgrid(line_x, line_y)
-  print(Xmesh)
-  print(Ymesh)
+  #print(Xmesh)
+  #print(Ymesh)
   z = np.zeros(Xmesh.shape)
 
   for k in(range(X.shape[0])):
@@ -170,7 +169,7 @@ def plotresult(Xmesh,Ymesh,z):
   cset = ax1.contour(Xmesh, Ymesh, z, zdir='z', offset=0.7, cmap=cm.rainbow)
   #cset = ax.contour(X, Y, Z, zdir='x', offset=-40, cmap=cm.coolwarm)
   #cset = ax.contour(X, Y, Z, zdir='y', offset=40, cmap=cm.coolwarm)
-
+  '''
   font1 = {'family' : 'Times New Roman','weight' : 'normal','size'   : 20}
   plt.tick_params(labelsize=20)
   x = range(0,120000,30000)
@@ -184,10 +183,9 @@ def plotresult(Xmesh,Ymesh,z):
   #ax1.set_zlabel('   mV',font1)
   ax1.set_zlim(0.7, 0.8)
   #fig.savefig('demo.png', bbox_inches='tight')
-
+  '''
   #plt.figure()
   #fig.savefig('demo.png', bbox_inches='tight')
-
   plt.show()
 
 # coding=gbk
@@ -197,34 +195,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def ImageToMatrix(filename):
-    # 读取图片
+
     im = Image.open(filename)
-    # 显示图片
-#     im.show()  
+
     width,height = im.size
     im = im.convert("L") 
     data = im.getdata()
     data = np.matrix(data,dtype='float')/255.0
     new_data = np.reshape(data,(width,height))
     return new_data
-#     new_im = Image.fromarray(new_data)
-#     # 显示图片
-#     new_im.show()
+
 def MatrixToImage(data):
     data = data*255
     new_im = Image.fromarray(data.astype(np.uint8))
     return new_im
     
-    
-'''
-filename = 'lena.jpg'
-data = ImageToMatrix(filename)
-print data 
-new_im = MatrixToImage(data)
-plt.imshow(data, cmap=plt.cm.gray, interpolation='nearest')
-new_im.show()
-new_im.save('lena_1.bmp')
-'''
 def resizeresult(z):
   d = 32
   Z=np.zeros((d,d))
@@ -241,32 +226,6 @@ def resizeresult(z):
   return Z
 
 if __name__ == '__main__':
-  Output=[]
-  Input_current=[]
-  Input_powergrid=[]
-  for i in(range(1)):
-    filename = str(i)+'_2.txt' 
-
-    data=readin(filename)
-    print(data)
-  
-    Xmesh,Ymesh,z,line_x,line_y=transferdata(data)
-
-    plotresult(Xmesh,Ymesh,z)
-    Z = resizeresult(z)
-    Output.append(Z)
-
-    filename1 = str(i)+'_1.txt'
-    data2,data3=readincurrent(filename1)
-    print(data3)
-    Input_powergrid.append(data3)
-    Xmesh1,Ymesh1,z1=transfercurrent(data2,line_x,line_y)
-    Z1 = resizeresult(z1)
-    Input_current.append(Z1)
-    
-    fig = plt.figure()
-    plt.imshow(Z1, interpolation='nearest')
-    plt.savefig(r'test.jpg', dpi=300)
-    plt.show()
-    
-  np.savez('data.npz',Output = Output, Input_current = Input_current, Input_powergrid = Input_powergrid)
+  data=readin('../IR_drop.txt')
+  Xmesh,Ymesh,z,line_x,line_y=transferdata(data)
+  plotresult(Xmesh,Ymesh,z)
