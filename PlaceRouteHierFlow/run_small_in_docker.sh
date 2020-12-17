49cb6c2e06735dc888dc65bca79774a687a89b5a
Steven Burns <smburns47@gmail.com>
2020-12-17 14:31:49 -0800

    Added two testcases to the PnR CI (with coverage)

--- PlaceRouteHierFlow/run_small_in_docker.sh
+++ PlaceRouteHierFlow/run_small_in_docker.sh
@@ -16,6 +16,8 @@ lcov -z ; \
 (cd placer && ./unit_tests --gtest_output=xml:junit.xml) && \
 (cd router && ./unit_tests --gtest_output=xml:junit.xml) && \
 ./pnr_compiler ./testcase_example switched_capacitor_filter.lef switched_capacitor_filter.v switched_capacitor_filter.map layers.json switched_capacitor_filter 2 0 > PnR.log && \
+./pnr_compiler ./testcase_guardring switched_capacitor_filter.lef switched_capacitor_filter.v switched_capacitor_filter.map layers.json switched_capacitor_filter 1 0 > PnR-guardring.log && \
+./pnr_compiler ./testcase_guardringpowernet high_speed_comparator.lef high_speed_comparator.v high_speed_comparator.map layers.json high_speed_comparator 1 0 > PnR-guardringpowernet.log && \
 lcov --capture --directory . --output-file coverage.info && \
 genhtml coverage.info --output-directory coverage.out"
 
