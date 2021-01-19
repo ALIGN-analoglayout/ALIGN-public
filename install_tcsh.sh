8d417ff626288cf9315a676229b42d0c6a64edf4
soneryaldiz <56893713+soneryaldiz@users.noreply.github.com>
2021-01-19 11:11:22 -0800

    Update install_tcsh.sh

--- install_tcsh.sh
+++ install_tcsh.sh
@@ -50,6 +50,12 @@ make
 mkdir googletest/mybuild
 cp -r lib googletest/mybuild/.
 
+#### Install logger
+cd $ALIGN_HOME
+git clone https://github.com/gabime/spdlog.git
+cd spdlog && mkdir build && cd build
+cmake .. && make -j
+
 ### Install superLU
 cd $ALIGN_HOME
 mkdir superlu
@@ -69,8 +75,9 @@ setenv LP_DIR $ALIGN_HOME/lpsolve
 #setenv BOOST_LP $ALIGN_HOME/boost
 setenv JSON $ALIGN_HOME/json
 setenv GTEST_DIR $ALIGN_HOME/googletest/googletest/
-setenv VENV $ALIGN_HOME/general
+export SPDLOG_DIR=$ALIGN_HOME/spdlog
 setenv SuperLu_DIR $ALIGN_HOME/superlu
+setenv VENV $ALIGN_HOME/general
 
 ## install align 
 #---------------
