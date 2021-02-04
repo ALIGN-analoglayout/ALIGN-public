53cb9e742bd72c3ef24ad17db4db631e7cd4b754
Parijat Mukherjee <parijat.mukherjee@intel.com>
2021-02-04 00:30:40 -0800

    [Dockerfile] Remove orphan apt-get

--- build/Dockerfile.using-install.sh
+++ build/Dockerfile.using-install.sh
@@ -9,11 +9,6 @@ FROM ubuntu:18.04 as align_base_using_install
 ENV http_proxy=$http_proxy
 ENV https_proxy=$https_proxy
 
-# Update packages
-RUN apt-get -qq update && DEBIAN_FRONTEND=noninterative apt-get -qq install \
-    git \
-&&    apt-get -qq clean
-
 ENV ALIGN_HOME=/ALIGN-public
 WORKDIR $ALIGN_HOME
 ENV USER=root
