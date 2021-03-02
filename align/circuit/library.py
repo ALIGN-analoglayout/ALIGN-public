054cdf189f6a6cbcaf718a55a11486e3f0fed2d8
Parijat Mukherjee <parijat.mukherjee@intel.com>
2021-03-01 19:40:46 -0800

    [library] Randomize library names if none

--- align/circuit/library.py
+++ align/circuit/library.py
@@ -1,4 +1,6 @@
 import inspect
+import random
+import string
 
 from . import device
 
@@ -6,9 +8,15 @@ libraries = {}
 
 class Library(dict):
 
-    def __init__(self, name='default', pdk=None):
-        if name in libraries:
-            self.update(libraries[name])
+    def __init__(self, name=None, pdk=None):
+        if name:
+            assert name not in libraries
+        else:
+            name = self._gen_lib_name()
         libraries.update({name: self})
+
+    def _gen_lib_name(self, nchar=8):
+        name = 'LIB' + ''.join(random.choices(string.ascii_uppercase + string.digits, k=nchar))
+        return self._gen_lib_name(nchar) if name in libraries else name
 
 default = Library('default')
