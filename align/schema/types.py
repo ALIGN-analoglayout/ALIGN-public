1a319dbb531dce2b1a62700ad70aa4477d325a66
Parijat Mukherjee <parijat.mukherjee@intel.com>
2021-03-10 18:14:39 -0800

    [schema] Make types more general

--- align/schema/types.py
+++ align/schema/types.py
@@ -1,7 +1,7 @@
 import typing
 import pydantic
 
-from typing import TypeVar, Optional, Generic, Union, NamedTuple, Literal, Dict, ClassVar, List
+from typing import TypeVar, Optional, Generic, Union, NamedTuple, Literal, ClassVar, Sequence, Mapping
 from pydantic.generics import GenericModel
 from pydantic import PrivateAttr
 
@@ -9,7 +9,7 @@ KeyT = TypeVar('KeyT')
 DataT = TypeVar('DataT')
 
 class List(GenericModel, Generic[DataT]):
-    __root__: typing.List[DataT]
+    __root__: Sequence[DataT]
 
     class Config:
         copy_on_model_validation = False
@@ -38,13 +38,16 @@ class List(GenericModel, Generic[DataT]):
         return self.__root__ == other
 
 class Dict(GenericModel, Generic[KeyT,DataT]):
-    __root__: typing.Dict[KeyT, DataT]
+    __root__: Mapping[KeyT, DataT]
 
     class Config:
         copy_on_model_validation = False
 
     def items(self):
         return self.__root__.items()
+
+    def keys(self):
+        return self.__root__.keys()
 
     def values(self):
         return self.__root__.values()
