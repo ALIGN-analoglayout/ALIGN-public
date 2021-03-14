893ca9ce3bcbe76af8a7444ab5995fc79818bc0c
Parijat Mukherjee <parijat.mukherjee@intel.com>
2021-03-14 00:45:06 -0800

    [install] Prune pip install list

--- install.sh
+++ install.sh
@@ -113,7 +113,9 @@ then
     python3 -m venv $VENV
     source $VENV/bin/activate
     python -m pip install --upgrade pip
-    python -m pip install pytest pytest-cov pytest-timeout coverage-badge
+    python -m pip install \
+        pytest pytest-timeout \
+        coverage coverage-badge
     python setup.py egg_info
     pip install -r *.egg-info/requires.txt
     rm -fr *.egg-info
