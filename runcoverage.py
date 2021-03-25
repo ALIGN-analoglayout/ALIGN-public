fff1eea725fc22b28a8ffe7d497b50b77a584489
Parijat Mukherjee <parijat.mukherjee@intel.com>
2021-03-25 11:34:45 -0700

    [coverage] Get coverage working again

--- runcoverage.py
+++ runcoverage.py
@@ -1,74 +1,79 @@
 #!/usr/bin/env python
 
+import os
 import sys
 import pathlib
 import subprocess
 import shutil
 import itertools
 
-from coverage.cmdline import main
-
-def main_wrapper():
+def main():
     assert pathlib.Path.cwd().resolve() == pathlib.Path(__file__).parent.resolve(), \
         f"Please run {__file__} from ALIGN_HOME."
     argv = sys.argv[1:]
 
-    output_dir = 'coverage-reports'
+    output_dir = pathlib.Path('coverage-reports').resolve()
+    c_coverage_file = pathlib.Path('coverage.info').resolve()
 
-    c_coverage_file = 'coverage.info'
-    build_dir = pathlib.Path.cwd() / 'build' / 'temp.linux-x86_64-3.8'
+    try:
+        from tests._cmake import CMAKE_BINARY_DIR, CMAKE_SOURCE_DIR
+    except:
+        CMAKE_BINARY_DIR=None
+        CMAKE_SOURCE_DIR=None
 
     # Detect whether to run LCOV
     GCOV_ENABLED = False
     if shutil.which('lcov') is None:
         print("WARNING: `lcov` not found. Generating coverage for python components only.")
-    elif next(pathlib.Path.cwd().glob('**/build/temp.linux-x86_64-3.8/**/*.gcno'), None) is None:
+    elif not CMAKE_BINARY_DIR or not CMAKE_SOURCE_DIR:
+        print("WARNING: CPP Source / Binary information not found. Generating coverage for python components only.")
+        print("         Run `pip install -e .[test] --no-build-isolation --install-option='-DCODE_COVERAGE=ON'` to instrument cpp code.")
+    elif next(pathlib.Path(CMAKE_BINARY_DIR).glob('**/*.gcno'), None) is None:
         print("WARNING: Could not find any .gcno files. Generating coverage for python components only.")
-        print("         Run pip with --editable & --install-option='--coverage' to view cpp coverage as well.")
+        print("         Run `pip install -e .[test] --no-build-isolation --install-option='-DCODE_COVERAGE=ON'` to instrument cpp code.")
     else:
+        print("INFO: Code coverage for cpp extension has been enabled. Please see coverage-reports/cpp.")
         GCOV_ENABLED = True
+
+    # Clean existing report (if any)
+    if output_dir.is_dir():
+        shutil.rmtree(str(output_dir))
+    # Number of parallel jobs
+    if 'MAX_JOBS' in os.environ:
+        MAX_JOBS = os.environ['MAX_JOBS']
+    else:
+        MAX_JOBS = 'auto'
 
     # LCOV init
     if GCOV_ENABLED:
         subprocess.run(' '.join([
-            'lcov', '--directory', '.', '--zerocounters']),
+            'lcov', '--directory', CMAKE_BINARY_DIR, '--zerocounters']),
             shell=True)
+
 
     # Actual command is run here
     subprocess.run(' '.join([
-        'coverage', 'run', *argv]),
+        'pytest', '-vv', # Call pytest in verbose mode
+        '-n', MAX_JOBS, # pytest-xdist options
+        '--cov-report', f'html:{output_dir}/python', '--cov=align',  # pytest-cov options
+        *argv
+        ]),
         shell=True)
-    # status = main(['run'] + argv)
 
-    # Finish capture
     if GCOV_ENABLED:
+        # Finish capture
         ret = subprocess.run(' '.join([
-            'lcov', '--capture',
+            'lcov', '--capture', '--no-external',
+            '--exclude', '*/_deps/*',
             '--directory', '.',
-            '--output-file', c_coverage_file]),
+            '--output-file', f'{c_coverage_file}']),
             shell=True)
-
-    # Clean existing report (if any)
-    shutil.rmtree(output_dir)
-
-    # Generate report
-    status = main(['html', '-d', f'{output_dir}/python'])
-    if GCOV_ENABLED:
-        subprocess.run(
-            ' '.join([
-                'lcov', '--remove', c_coverage_file,
-                '/usr/include/*',
-                '*/include/pybind11/*',
-                '*/include/spdlog/*',
-                '*/include/nlohmann/*',
-                '*/include/gtest/*',
-                '--output-file', c_coverage_file]),
-                shell=True)
+        # Generate report
         subprocess.run(' '.join([
-            'genhtml', c_coverage_file,
+            'genhtml', f'{c_coverage_file}',
             '--output-directory',  f'{output_dir}/cpp',
             '--no-branch-coverage',
             '--title', '"CPP lcov report"']), shell=True)
 
 if __name__ == '__main__':
-    main_wrapper()
+    main()
