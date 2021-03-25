812e66c40bc254a68ff883b99b2bc6418c40ee31
Parijat Mukherjee <parijat.mukherjee@intel.com>
2021-03-24 19:50:12 -0700

    [tests] Avoid race conditions in tests.gdsconv

--- tests/gdsconv/test_gds_txt.py
+++ tests/gdsconv/test_gds_txt.py
@@ -11,12 +11,12 @@ def binary_dir():
     return os.path.dirname(sys.executable)
 
 def test_gds_txt_roundtrip (binary_dir):
-    os.system (f"{binary_dir}/gds2txt {mydir}/file.gds > {mydir}/fromgds.txt")      #nosec
-    os.system (f"{binary_dir}/txt2gds {mydir}/fromgds.txt -o {mydir}/fromtxt.gds")  #nosec
-    assert (filecmp.cmp (f"{mydir}/file.gds", f"{mydir}/fromtxt.gds"))
+    os.system (f"{binary_dir}/gds2txt {mydir}/file.gds > {mydir}/fromgds5.txt")      #nosec
+    os.system (f"{binary_dir}/txt2gds {mydir}/fromgds5.txt -o {mydir}/fromtxt5.gds")  #nosec
+    assert (filecmp.cmp (f"{mydir}/file.gds", f"{mydir}/fromtxt5.gds"))
 
 def test_txt_gds_roundtrip (binary_dir):
-    os.system (f"{binary_dir}/txt2gds {mydir}/file.txt -o {mydir}/fromtxt2.gds")    #nosec
-    os.system (f"{binary_dir}/gds2txt {mydir}/fromtxt2.gds > {mydir}/fromgds2.txt") #nosec
-    assert (filecmp.cmp (f"{mydir}/file.txt", f"{mydir}/fromgds2.txt"))
+    os.system (f"{binary_dir}/txt2gds {mydir}/file.txt -o {mydir}/fromtxt6.gds")    #nosec
+    os.system (f"{binary_dir}/gds2txt {mydir}/fromtxt6.gds > {mydir}/fromgds6.txt") #nosec
+    assert (filecmp.cmp (f"{mydir}/file.txt", f"{mydir}/fromgds6.txt"))
 
