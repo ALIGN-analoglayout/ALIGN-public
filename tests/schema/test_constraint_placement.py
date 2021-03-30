880808f341e77f4cd91fcb5888e73a39f42d5b97
soneryal <soner.yaldiz@intel.com>
2021-03-30 14:30:43 -0700

    exceptions for dockerignore

--- tests/schema/test_constraint_placement.py
+++ tests/schema/test_constraint_placement.py
@@ -8,14 +8,14 @@ my_dir = pathlib.Path(__file__).resolve().parent
 
 
 def test_one():
-    with open(my_dir/'constraint_placement_1.json', "rt") as fp:
+    with open(my_dir/'constraint_placement_1_gold.json', "rt") as fp:
         const = json.load(fp)
         placement_constraints = parse_obj_as(ConstraintsPlacement, const)
         print(placement_constraints.json())
 
 
 def test_two():
-    with open(my_dir/'constraint_placement_2.json', "rt") as fp:
+    with open(my_dir/'constraint_placement_2_gold.json', "rt") as fp:
         const = json.load(fp)
         with pytest.raises(Exception):
             placement_constraints = parse_obj_as(ConstraintsPlacement, const)
