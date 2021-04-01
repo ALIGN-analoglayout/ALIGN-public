f8162afb3a8eaed5e9475c58871ce4e460fa4a2a
soneryaldiz <56893713+soneryaldiz@users.noreply.github.com>
2021-04-01 09:44:32 -0700

    Ready: Placement Constraints (#587)
    * alignment;orientation;boundary;generator
    
    * alignment abut
    
    * exceptions for dockerignore
    
    * [test] Rename constraint_placement files
    
    Co-authored-by: Parijat Mukherjee <parijat.mukherjee@intel.com>

---
+++ tests/schema/test_constraint_placement.py
@@ -0,0 +1,22 @@
+import json
+import pytest
+import pathlib
+from align.schema import ConstraintsPlacement
+from pydantic import parse_obj_as
+
+my_dir = pathlib.Path(__file__).resolve().parent
+
+
+def test_one():
+    with open(my_dir/'test_constraint_placement_1-freeze.json', "rt") as fp:
+        const = json.load(fp)
+        placement_constraints = parse_obj_as(ConstraintsPlacement, const)
+        print(placement_constraints.json())
+
+
+def test_two():
+    with open(my_dir/'test_constraint_placement_2-freeze.json', "rt") as fp:
+        const = json.load(fp)
+        with pytest.raises(Exception):
+            placement_constraints = parse_obj_as(ConstraintsPlacement, const)
+            print(placement_constraints.json())
