4706bf05fc1c48cd174589d4eeb24c0ff082754b
Steven Burns <smburns47@gmail.com>
2021-04-01 10:22:35 -0700

    Standalone test; no JSON files needed

--- tests/schema/test_constraint_placement.py
+++ tests/schema/test_constraint_placement.py
@@ -3,19 +3,65 @@ import pytest
 import pathlib
 from align.schema import ConstraintsPlacement
 from pydantic import parse_obj_as
+import io
 
 my_dir = pathlib.Path(__file__).resolve().parent
 
 
 def test_one():
-    with open(my_dir/'test_constraint_placement_1-freeze.json', "rt") as fp:
+    json_str = """\
+{
+    "constraints": [
+      {
+        "constraint_name": "alignment",
+        "instances": ["i0"],
+        "direction": "horizontal",
+        "edge": "bottom"
+      },
+      {
+        "constraint_name": "generator",
+        "instances": ["i1", "i2"],
+        "style": "cc"
+      },
+      {
+        "constraint_name": "orientation",
+        "instances": ["i2"],
+        "flip_y": true
+      },
+      {
+        "constraint_name": "boundary",
+        "subcircuits": ["subcircuit_a"],
+        "aspect_ratio_min": 0.3,
+        "aspect_ratio_max": 0.6
+      }
+    ]
+  }
+"""   
+    with io.StringIO( json_str) as fp:
         const = json.load(fp)
         placement_constraints = parse_obj_as(ConstraintsPlacement, const)
         print(placement_constraints.json())
 
 
 def test_two():
-    with open(my_dir/'test_constraint_placement_2-freeze.json', "rt") as fp:
+    json_str = """\
+{
+    "constraints": [
+      {
+        "constraint_name": "alignment",
+        "instances": ["i0, i1"],
+        "direction": "horizontal",
+        "edge": "nowhere"
+      },
+      {
+        "constraint_name": "orientation",
+        "instances": ["i1"],
+        "flip_y": true
+      }
+    ]
+  }
+"""
+    with io.StringIO( json_str) as fp:
         const = json.load(fp)
         with pytest.raises(Exception):
             placement_constraints = parse_obj_as(ConstraintsPlacement, const)
