532de568eb1f925d586738a5f7047465fe9963be
soneryal <soner.yaldiz@intel.com>
2021-04-05 21:05:37 -0700

    constraint_database

---
+++ align/schema/constraint_database.py
@@ -0,0 +1,93 @@
+import abc
+import random
+import string
+import collections
+import more_itertools as itertools
+
+from . import types
+from .types import Union, Optional, Literal, List, ClassVar
+
+import logging
+logger = logging.getLogger(__name__)
+
+try:
+    import z3
+except:
+    z3 = None
+    logger.warning("Could not import z3. ConstraintDB will not look for spec inconsistency.")
+
+
+from .constraint_placement import *
+ConstraintType = Union[Align, AlignVertical, AlignHorizontal]
+
+class ConstraintDB(types.BaseModel):
+
+    __root__ : List[ConstraintType]
+
+    @types.validate_arguments
+    def append(self, constraint: ConstraintType):
+        self.__root__.append(constraint)
+        if self._validation:
+            self._solver.append(*constraint.check())
+            assert self._solver.check() == z3.sat
+
+    def __init__(self, validation=None):
+        super().__init__(__root__=[])
+        self._commits = collections.OrderedDict()
+        if z3 is not None:
+            self._solver = z3.Solver()
+            if validation is not None:
+                self._validation = validation
+        else:
+            assert not validation, "Cannot validate without z3"
+
+    #
+    # Private attribute affecting class behavior
+    #
+    _solver = types.PrivateAttr()
+    _commits = types.PrivateAttr()
+    _validation = types.PrivateAttr(default_factory=lambda: z3 is not None)
+
+    def __iter__(self):
+        return iter(self.__root__)
+
+    def __getitem__(self, item):
+        return self.__root__[item]
+
+    def __len__(self):
+        return len(self.__root__)
+
+    def _gen_commit_id(self, nchar=8):
+        id_ = ''.join(random.choices(string.ascii_uppercase + string.digits, k=nchar))
+        return self._gen_commit_id(nchar) if id_ in self._commits else id_
+
+    def checkpoint(self):
+        self._commits[self._gen_commit_id()] = len(self.__root__)
+        if self._validation:
+            self._solver.push()
+        return next(reversed(self._commits))
+
+    def _revert(self):
+        if self._validation:
+            self._solver.pop()
+        _, length = self._commits.popitem()
+        del self.__root__[length:]
+
+    def revert(self, name=None):
+        assert len(self._commits) > 0, 'Top of scope. Nothing to revert'
+        if name is None or name == next(reversed(self._commits)):
+            self._revert()
+        else:
+            assert name in self._commits
+            self._revert()
+            self.revert(name)
+
+    @classmethod
+    def GenerateVar(cls, name, **fields):
+        if fields:
+            return collections.namedtuple(
+                name,
+                fields.keys(),
+            )(*z3.Ints(' '.join(fields.values())))
+        else:
+            return z3.Int(name)
