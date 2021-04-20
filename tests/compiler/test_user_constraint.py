75ec417f6295ffdd538ce82cf918e02ea2bb2f3a
Parijat Mukherjee <parijat.mukherjee@intel.com>
2021-04-20 16:38:41 -0700

    [tests] Skip constraint checking if no Z3

--- tests/compiler/test_user_constraint.py
+++ tests/compiler/test_user_constraint.py
@@ -3,6 +3,7 @@ import pytest
 import json
 
 from align.compiler.compiler import compiler, compiler_output
+from align.schema.checker import Z3Checker
 
 @pytest.fixture
 def test_compiler_hsc(dir_name):
@@ -43,7 +44,7 @@ def test_group_block_hsc(test_compiler_hsc):
         gold_const.sort(key=lambda item: item.get("const_name"))
     assert gold_const == gen_const
 
-
+@pytest.mark.skipif(not Z3Checker.enabled, reason="Couldn't import Z3")
 @pytest.mark.parametrize('dir_name', ['high_speed_comparator_broken'])
 def test_constraint_checking(dir_name):
     circuit_name = 'high_speed_comparator'
