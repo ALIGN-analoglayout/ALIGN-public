298fb23be768213f711334abf8b29e51b5638673
Steven Burns <smburns47@gmail.com>
2021-04-21 11:43:49 -0700

    Fix multi-placement rendering

--- align/pnr/render_placement.py
+++ align/pnr/render_placement.py
@@ -60,7 +60,8 @@ def gen_placement_verilog(hN, DB, verilog_d):
             b = inst.originBox
             new_r = b.LL.x, b.LL.y, b.UR.x, b.UR.y
             if child_idx >= 0:
-                aux(DB.hierTree[child_idx], new_r, new_prefix_path)
+                new_hN = DB.CheckoutHierNode(child_idx, blk.selectedInstance)
+                aux(new_hN, new_r, new_prefix_path)
             else:
                 leaf_bboxes[inst.master].append( new_r)
 
@@ -109,58 +110,6 @@ def gen_placement_verilog(hN, DB, verilog_d):
     print( json.dumps( d, indent=2))
 
     return d
-
-def dump_blocks(hN, DB, leaves_only=False):
-    logger.info(f'hN.parent={hN.parent}')
-
-    fig = go.Figure()
-
-    def gen_trace_xy(inst, prefix_path, child_idx, tr):
-
-        if leaves_only and child_idx >= 0:
-            return
-
-        # tr converts local coordinates into global coordinates
-
-        b = inst.originBox
-        r = b.LL.x, b.LL.y, b.UR.x, b.UR.y
-        [x0, y0, x1, y1] = tr.hitRect(
-            transformation.Rect(*r)).canonical().toList()
-        x = [x0, x1, x1, x0, x0]
-        y = [y0, y0, y1, y1, y0]
-
-        hovertext = f'{"/".join(prefix_path)}<br>{inst.master} ({child_idx})<br>{tr}<br>Global {x0} {y0} {x1} {y1}<br>Local {r[0]} {r[1]} {r[2]} {r[3]}'
-
-        fig.add_trace(go.Scatter(x=x, y=y, mode='lines',
-                      name=hovertext, fill="toself", showlegend=False))
-
-
-    def aux(hN, prefix_path, tr):
-
-        for blk in hN.Blocks:
-            child_idx = blk.child
-            inst = blk.instance[blk.selectedInstance]
-
-            new_prefix_path = prefix_path + [inst.name]
-
-            # tr converts hN coordinates to global coordinates
-            # tr2 = gen_transformation(inst) converts local coordinates to hN coordinates
-            # new_tr should be global = tr(tr2(local)
-
-            new_tr = tr.postMult(gen_transformation(inst))
-
-            gen_trace_xy(inst, new_prefix_path, child_idx, new_tr)
-
-            if child_idx >= 0:
-                new_hN = DB.hierTree[child_idx]
-                aux(new_hN, new_prefix_path, new_tr)
-
-    aux(hN, [], transformation.Transformation())
-
-
-    fig.update_yaxes(scaleanchor="x", scaleratio=1)
-    fig.update_layout(title=dict(text=f'{hN.name}_{hN.n_copy}'))
-    fig.show()
 
 def dump_blocks2( placement_verilog_d, top_cell, sel, leaves_only=False):
     logger.info(f'Drawing {top_cell}_{sel}...')
