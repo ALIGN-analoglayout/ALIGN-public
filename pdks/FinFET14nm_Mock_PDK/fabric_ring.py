367c6646bbde30351773540a226a126fa9bfc5fc
Arvind <aksharma@umn.edu>
2021-04-23 00:36:43 -0500

    Setting default values for X and Y to 1

--- pdks/FinFET14nm_Mock_PDK/fabric_ring.py
+++ pdks/FinFET14nm_Mock_PDK/fabric_ring.py
@@ -6,8 +6,8 @@ from align.primitive import generate_primitive
 def gen_parser():
     parser = argparse.ArgumentParser( description="Inputs for Cell Generation")
     parser.add_argument( "-b", "--block_name", type=str, required=True)
-    parser.add_argument( "-X", "--Xcells", type=int, required=True)
-    parser.add_argument( "-Y", "--Ycells", type=int, required=True)
+    parser.add_argument( "-X", "--Xcells", type=int, required=False, default=1)
+    parser.add_argument( "-Y", "--Ycells", type=int, required=False, default=1)
     parser.add_argument( "-q", "--pinSwitch", type=int, required=False, default=0)
     parser.add_argument( "-d", "--pdkdir", type=pathlib.Path, required=False, default=pathlib.Path(__file__).resolve().parent)
     parser.add_argument( "-o", "--outputdir", type=pathlib.Path, required=False, default=pathlib.Path(__file__).resolve().parent)
