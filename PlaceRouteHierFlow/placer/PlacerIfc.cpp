1d048e8799247478703762eb059e4b8ae755d52d
Steven Burns <smburns47@gmail.com>
2021-04-24 03:36:36 -0700

    Allow placer hyperparameters to be set in Python

--- PlaceRouteHierFlow/placer/PlacerIfc.cpp
+++ PlaceRouteHierFlow/placer/PlacerIfc.cpp
@@ -11,6 +11,6 @@ double ConstGraph::PHI=1500;
 double ConstGraph::PI=1500;
 double ConstGraph::PII=1500;
 
-PlacerIfc::PlacerIfc(PnRDB::hierNode& currentNode, int numLayout, string opath, int effort, PnRDB::Drc_info& drcInfo) : _nodeVec( numLayout, currentNode) {
-  Placer curr_plc(_nodeVec,opath,effort,drcInfo);
+PlacerIfc::PlacerIfc(PnRDB::hierNode& currentNode, int numLayout, string opath, int effort, PnRDB::Drc_info& drcInfo, const PlacerHyperparameters& hyper) : _nodeVec( numLayout, currentNode) {
+  Placer curr_plc(_nodeVec,opath,effort,drcInfo,hyper);
 }
