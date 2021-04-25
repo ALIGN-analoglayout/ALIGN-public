dfff956e74e68bd9a337b14aa43c76548c68e0ea
Steven Burns <smburns47@gmail.com>
2021-04-25 09:51:43 -0700

    [skip CI more fixes]

--- regression_summaries/merge.py
+++ regression_summaries/merge.py
@@ -5,4 +5,4 @@ import pandas as pd
 df_23 = pd.read_csv( "summary_2021_04_23.csv")
 df_25 = pd.read_csv( "summary_2021_04_25.csv")
 
-df_23.merge(df_25, how='outer', on='name').to_csv( 'merged.csv')
+df_23.merge(df_25, how='outer', on='name').to_csv( 'merged.csv', index=False)
