c55d68add46bae71dcba6041d29b8ed9ed2825fc
Steven Burns <smburns47@gmail.com>
2021-04-25 11:11:07 -0700

    [skip CI] More general merge script

--- regression_summaries/merge.py
+++ regression_summaries/merge.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 
+import argparse
 import pandas as pd
 
-df_23 = pd.read_csv( "summary_2021_04_23.csv")
-df_25 = pd.read_csv( "summary_2021_04_25.csv")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser( description='Analyze Regression Results and Build CSV Table')
+    parser.add_argument( '-0', '--csv_input_file0', type=str, help='CSV input file 0 (reference)')
+    parser.add_argument( '-1', '--csv_input_file1', type=str, help='CSV input file 1 (current)')
+    parser.add_argument( '-o', '--csv_output_file', type=str, default='merged.csv', help='CSV output file')
 
-df_23.merge(df_25, how='outer', on='name').to_csv( 'merged.csv', index=False)
+    args = parser.parse_args()
+
+    df0 = pd.read_csv( args.csv_input_file0)
+    df1 = pd.read_csv( args.csv_input_file1)
+
+    df0.merge(df1, how='outer', on='name').to_csv( args.csv_output_file, index=False)
