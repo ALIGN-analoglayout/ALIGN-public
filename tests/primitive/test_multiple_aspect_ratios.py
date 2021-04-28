879165b5159cb228d9b43aae51c54363f8f06d6d
Steven Burns <smburns47@gmail.com>
2021-04-28 04:15:11 -0700

    Experiments to determine placeholder hierarchy to maintain the folded model

--- tests/primitive/test_multiple_aspect_ratios.py
+++ tests/primitive/test_multiple_aspect_ratios.py
@@ -2,6 +2,10 @@ import pytest
 import align
 import os
 import pathlib
+import json
+import re
+import logging
+from collections import defaultdict
 
 ALIGN_HOME = pathlib.Path(__file__).resolve().parent.parent.parent
 
@@ -13,11 +17,52 @@ else:
 def test_primitive_multiple_aspect_ratios():
     design_dir = ALIGN_HOME / 'examples' / 'current_mirror_ota'
 
+    current = pathlib.Path(__file__).resolve().parent
+    run_dir = current / 'current_mirror_ota'
+
+    with (run_dir / '1_topology' / 'current_mirror_ota.verilog.json').open('rt') as fp:
+        verilog_d = json.load(fp)
+
+    with (run_dir / '1_topology' / 'primitives.json').open('rt') as fp:
+        primitives_d = json.load(fp)
+
+    map_d = defaultdict(list)
+
+    p = re.compile( r'^(\S+)_nfin(\d+)_n(\d+)_X(\d+)_Y(\d+)_(\S+)$')
+    for k,v in primitives_d.items():
+        m = p.match(k)
+        if m:
+            nfin,n,X,Y = tuple(int(x) for x in m.groups()[1:5])
+            abstract_name = f'{m.groups()[0]}_nfin{nfin}_{m.groups()[5]}'
+            map_d[abstract_name].append( k)
+        else:
+            print( f'No match for primitive {k}')
+
+    concrete2abstract = { vv:k for k,v in map_d.items() for vv in v}
+
+    placeholders_d = defaultdict(list)
+    for module in verilog_d['modules']:
+        parent_name = module['name']
+        for instance in module['instances']:
+            abstract_name = concrete2abstract[instance['template_name']]
+            instance_name = instance['instance_name']
+            full_name = f'{abstract_name}:{parent_name}/{instance_name}'
+            placeholders_d[abstract_name].append(full_name)
+            instance['template_name'] = full_name
+
+    print(json.dumps(verilog_d, indent=2))
+
+    map2_d = []
+    for k,v in map_d.items():
+        map2_d.append( {'abstract': k, 'concrete': v, 'placeholders': placeholders_d[k]})
+    print(json.dumps( map2_d, indent=2))
+
+
     args = [str(design_dir), '-c', '--flow_start', '2_primitives', '--flow_stop', '2_primitives']
 
-    current = pathlib.Path(__file__).resolve().parent
 
-    os.chdir( current / 'current_mirror_ota')
+
+    os.chdir( run_dir)
 
     results = align.CmdlineParser().parse_args(args)
 
