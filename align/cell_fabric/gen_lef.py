4c69264c91dd3beeb931fdb91bdcc157f90d4240
Arvind <aksharma@umn.edu>
2021-05-01 06:50:38 -0500

    updating test_gds_gds_gold for gen_gds_json.py unit test

--- align/cell_fabric/gen_lef.py
+++ align/cell_fabric/gen_lef.py
@@ -2,6 +2,56 @@ import sys
 import json
 import collections
 
+def gen_lef_data(data, fp, macro_name, cell_pin, bodyswitch):
+    def s(x):
+        return "%.4f" % (x/10000.0)
+
+    fp.write("MACRO %s\n" % macro_name)
+    fp.write("  ORIGIN 0 0 ;\n")
+    fp.write("  FOREIGN %s 0 0 ;\n" % macro_name)
+
+    fp.write("  SIZE %s BY %s ;\n" % (s(data['bbox'][2]), s(data['bbox'][3])))
+
+    exclude_layers = {"via0", "via1", "via2", "poly", "LISD", "SDT", "RVT",
+                      "M0", "fin", "polycon", "GCUT", "active", "nselect", "pselect", "nwell"}
+
+# O(npins * nsegments) algorithm. Could be O(npins + nsegments) FIX!
+
+    for i in cell_pin:
+        if i == 'B' and bodyswitch==0:continue
+        fp.write("  PIN %s\n" % i)
+        #fp.write( "    DIRECTION %s ;\n" % obj['ported'])
+        fp.write("    DIRECTION INOUT ;\n")
+        fp.write("    USE SIGNAL ;\n")
+        fp.write("  PORT\n")
+        for obj in data['terminals']:
+            if 'pin' in obj:
+                if obj['pin'] == i:
+                    fp.write("      LAYER %s ;\n" % obj['layer'])
+                    fp.write("        RECT %s %s %s %s ;\n" %
+                             tuple([s(x) for x in obj['rect']]))
+        fp.write("    END\n")
+        fp.write("  END %s\n" % i)
+
+    fp.write("  OBS\n")
+    for obj in data['terminals']:
+        if ('pin' not in obj or obj['pin'] not in cell_pin) and obj['layer'] not in exclude_layers:
+            fp.write("    LAYER %s ;\n" % obj['layer'])
+            fp.write("      RECT %s %s %s %s ;\n" %
+                     tuple([s(x) for x in obj['rect']]))
+    fp.write("  END\n")
+
+    fp.write("END %s\n" % macro_name)
+
+
+def gen_lef_json_fp(json_fp, lef_fp, macro_name, cell_pin, bodyswitch):
+    gen_lef_data(json.load(json_fp), lef_fp, macro_name, cell_pin, bodyswitch)
+
+
+def gen_lef_json(json_fn, lef_fn, macro_name, cell_pin, bodyswitch):
+    with open(json_fn, "rt") as json_fp, open(lef_fn, "wt") as lef_fp:
+        gen_lef_json_fp(json_fp, lef_fp, macro_name, cell_pin)
+
 def json_lef(input_json, out_lef, cell_pin, bodyswitch, blockM, p):
 
     exclude_layers = p.get_lef_exclude()
