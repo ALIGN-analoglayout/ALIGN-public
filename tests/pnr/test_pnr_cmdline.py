3f9311709582982cb9bf2f0c62364f73328862c5
Steven Burns <smburns47@gmail.com>
2021-05-16 10:30:51 -0700

    Resurrect standalone pnr_compiler.py test (still fails for verilog input)

--- tests/pnr/test_pnr_cmdline.py
+++ tests/pnr/test_pnr_cmdline.py
@@ -3,8 +3,10 @@ import pathlib
 import io
 import pytest
 import os
+import shutil
 
 from align.pnr.cmdline import cmdline
+import align
 
 ALIGN_HOME = pathlib.Path(__file__).resolve().parent.parent.parent
 
@@ -20,16 +22,51 @@ else:
 
 mydir = pathlib.Path(__file__).resolve().parent
 
-@pytest.mark.skip
+@pytest.mark.skip # Broken because of incorrect primitive names
 def test_verilog():
     nm = 'current_mirror_ota'
-    d = mydir / "current_mirror_ota_inputs"
-    argv = [ 'pnr_compiler.py', str(d), f'{nm}.lef', f'{nm}.v', f'{nm}.map', 'layers.json', nm, '1', '0']
-    cmdline( argv, results_dir=str(pathlib.Path(ALIGN_WORK_DIR) / 'test_verilog_Results'))
 
-@pytest.mark.skip
+    run_dir = ALIGN_WORK_DIR / f'{nm}_pnr_cmdline'
+
+    if run_dir.exists():
+        assert run_dir.is_dir()
+        shutil.rmtree(run_dir)
+
+    run_dir.mkdir(parents=True, exist_ok=False)
+    os.chdir(run_dir)
+
+    nm = 'current_mirror_ota'
+
+    design_dir = ALIGN_HOME / 'examples' / nm
+    args = [str(design_dir), '--flow_stop', '3_pnr:prep']
+    results = align.CmdlineParser().parse_args(args)
+
+    (run_dir / '3_pnr' / 'inputs' / f'{nm}.v').write_text((run_dir / '1_topology' / f'{nm}.v').read_text())    
+
+    os.chdir(run_dir / "3_pnr")
+
+    d = "inputs"
+    argv = [ 'pnr_compiler.py', "inputs", f'{nm}.lef', f'{nm}.v', f'{nm}.map', 'layers.json', nm, '1', '0']
+    cmdline( argv, results_dir='Results')
+
 def test_verilog_json():
     nm = 'current_mirror_ota'
-    d = mydir / "current_mirror_ota_inputs"
-    argv = [ 'pnr_compiler.py', str(d), f'{nm}.lef', f'{nm}.verilog.json', f'{nm}.map', 'layers.json', nm, '1', '0']
-    cmdline( argv, results_dir=str(pathlib.Path(ALIGN_WORK_DIR) / 'test_verilog_json_Results'))
+
+    run_dir = ALIGN_WORK_DIR / f'{nm}_pnr_cmdline'
+
+    if run_dir.exists():
+        assert run_dir.is_dir()
+        shutil.rmtree(run_dir)
+
+    run_dir.mkdir(parents=True, exist_ok=False)
+    os.chdir(run_dir)
+
+    design_dir = ALIGN_HOME / 'examples' / nm
+    args = [str(design_dir), '--flow_stop', '3_pnr:prep']
+    results = align.CmdlineParser().parse_args(args)
+
+    os.chdir(run_dir / "3_pnr")
+
+    d = "inputs"
+    argv = [ 'pnr_compiler.py', "inputs", f'{nm}.lef', f'{nm}.verilog.json', f'{nm}.map', 'layers.json', nm, '1', '0']
+    cmdline( argv, results_dir='Results')
