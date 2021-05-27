9c37d48d653fb03fb1c31cd05d50cce117352817
soneryaldiz <56893713+soneryaldiz@users.noreply.github.com>
2021-05-27 13:52:28 -0700

    Ready: Define and Harden Boundary (#677)
    * "define and harden boundary"
    
    * revision
    
    * scaleFactor
    
    * temporary commit to reproduce
    
    * use scalefactor
    
    * undo scalefactor
    
    * updated scaling and tests
    
    Co-authored-by: Yishuang Lin <854768750@qq.com>

--- align/pnr/toplevel.py
+++ align/pnr/toplevel.py
@@ -426,7 +426,7 @@ def place_and_route( *, DB, opath, fpath, numLayout, effort, adr_mode, PDN_mode,
                 (pathlib.Path(opath) / f'{concrete_name}.placement_verilog.json').write_text(scaled_placement_verilog_d.json(indent=2,sort_keys=True))
 
                 standalone_overlap_checker( scaled_placement_verilog_d, concrete_name)
-                check_placement( scaled_placement_verilog_d)
+                check_placement( scaled_placement_verilog_d, scale_factor)
 
                 if gui:
 
