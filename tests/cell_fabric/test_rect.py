3b477ccecf9af4e97b621bbecefea2eb85b1664a
Steve Burns <steven.m.burns@intel.com>
2021-07-06 15:51:14 -0700

    Update

--- tests/cell_fabric/test_rect.py
+++ tests/cell_fabric/test_rect.py
@@ -1,15 +1,14 @@
 from align.cell_fabric.transformation import Rect
 
-def test_toList():
-    r = Rect( 0, 0, 1, 1)
-    assert r.toList() == [0, 0, 1, 1]
-
 def test_canonical():
+    r = Rect( 0, 0, 1, 1)
+    assert r.canonical().toList() == [0, 0, 1, 1]
     r = Rect( 1, 1, 0, 0)
     assert r.canonical().toList() == [0, 0, 1, 1]
 
-def test_repr():
+def test_representations():
     r = Rect( 0, 0, 1, 1)    
     assert r.__repr__() == "[0, 0, 1, 1]"
     assert repr(r) == "[0, 0, 1, 1]"
-    assert str(r) == "[0, 0, 1, 1]"
+    assert r.toColonSepStr() == "0:0:1:1"
+    assert r.toList() == [0, 0, 1, 1]
