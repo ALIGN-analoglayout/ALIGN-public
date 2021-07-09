46ceab7da0b5fe61d618c31ecafbb514c6278e2e
Steven Burns <smburns47@gmail.com>
2021-07-09 10:37:47 -0700

    More autopep8

--- tests/adr/test_adr_cktgen.py
+++ tests/adr/test_adr_cktgen.py
@@ -1,4 +1,3 @@
-import json
 import os
 import pathlib
 
@@ -11,22 +10,22 @@ if 'ALIGN_HOME' in os.environ:
 else:
     os.environ['ALIGN_HOME'] = str(ALIGN_HOME)
 
-def test_adr_cktgen():
-    
-    metal_layer_map = { f'M{i}' : f'metal{i}' for i in range(0,7) }
-    via_layer_map = { f'V{i}' : f'via{i}' for i in range(0,6) }
-    layer_map = dict(list(metal_layer_map.items()) + list(via_layer_map.items()))
 
+def test_adr_cktgen():
+
+    metal_layer_map = {f'M{i}': f'metal{i}' for i in range(0, 7)}
+    via_layer_map = {f'V{i}': f'via{i}' for i in range(0, 6)}
+    layer_map = dict(list(metal_layer_map.items()) + list(via_layer_map.items()))
 
     bn = "foo"
 
     placer_results = {
-        "bbox": [0,0,200,100],
+        "bbox": [0, 0, 200, 100],
         "instances": [
             {
                 "template_name": "a",
                 "instance_name": "u0",
-                "transformation": { "oX": 0, "oY": 0, "sX": 1, "sY": 1},
+                "transformation": {"oX": 0, "oY": 0, "sX": 1, "sY": 1},
                 "formal_actual_map": {
                     "inp": "a"
                 }
@@ -34,7 +33,7 @@ def test_adr_cktgen():
             {
                 "template_name": "a",
                 "instance_name": "u1",
-                "transformation": { "oX": 100, "oY": 0, "sX": 1, "sY": 1},
+                "transformation": {"oX": 100, "oY": 0, "sX": 1, "sY": 1},
                 "formal_actual_map": {
                     "inp": "a"
                 }
@@ -43,31 +42,30 @@ def test_adr_cktgen():
         "leaves": [
             {
                 "template_name": "a",
-                "bbox": [0,0,100,100],
+                "bbox": [0, 0, 100, 100],
                 "terminals": [
                     {
                         "layer": "M1",
                         "netName": "inp",
-                        "rect": [1,1,3,3]
+                        "rect": [1, 1, 3, 3]
                     }
                 ]
             }
         ]
     }
 
-    netl = ADNetlist.fromPlacerResults( bn, layer_map, placer_results)
+    netl = ADNetlist.fromPlacerResults(bn, layer_map, placer_results)
 
     assert netl.nm == bn
-    assert netl.bbox.toList() == [0,0,200,100]
+    assert netl.bbox.toList() == [0, 0, 200, 100]
     assert netl.gidIndex == 2
     assert len(netl.wire_cache) == 2
 
-    assert ('a', (  1,1,  3,3), 'metal1') in netl.wire_cache
-    assert ('a', (101,1,103,3), 'metal1') in netl.wire_cache
+    assert ('a', (1, 1, 3, 3), 'metal1') in netl.wire_cache
+    assert ('a', (101, 1, 103, 3), 'metal1') in netl.wire_cache
 
-    assert [  0,0,100,100] == netl.instances['u0'].toList()
-    assert [100,0,200,100] == netl.instances['u1'].toList()
+    assert [0, 0, 100, 100] == netl.instances['u0'].toList()
+    assert [100, 0, 200, 100] == netl.instances['u1'].toList()
 
     assert len(netl.nets) == 1
     print(netl.nets)
-
