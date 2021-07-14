ec7bbfc1e887eed4494a0e7c2e5c7820327e918e
Soner Yaldiz <soner.yaldiz@intel.com>
2021-07-14 14:35:15 -0700

    test order with abut

--- tests/constraints/test_constraint.py
+++ tests/constraints/test_constraint.py
@@ -1,3 +1,4 @@
+import textwrap
 try:
     from .utils import get_test_id, build_example, run_example
     from . import circuits
@@ -47,5 +48,30 @@ def test_do_not_identify():
     netlist = circuits.ota_five(name)
     setup = ""
     constraints = [{"constraint": "AlignInOrder", "line": "left", "instances": ["mp1", "mn1"]}]
+    example = build_example(name, netlist, setup, constraints)
+    run_example(example)
+
+
+def test_order_abut():
+    name = f'ckt_{get_test_id()}'
+    netlist = circuits.comparator(name)
+    setup = textwrap.dedent("""\
+        POWER = vccx
+        GND = vssx
+        """)
+    constraints = [
+        {"constraint": "GroupBlocks", "instances": ["mn1", "mn2"], "name": "dp"},
+        {"constraint": "GroupBlocks", "instances": ["mn3", "mn4"], "name": "ccn"},
+        {"constraint": "GroupBlocks", "instances": ["mp5", "mp6"], "name": "ccp"},
+        {"constraint": "GroupBlocks", "instances": ["mn11", "mp13"], "name": "invp"},
+        {"constraint": "GroupBlocks", "instances": ["mn12", "mp14"], "name": "invn"},
+        {"constraint": "SymmetricBlocks", "direction": "V",
+            "pairs": [["ccp"], ["ccn"], ["dp"], ["mn0"], ["invn", "invp"], ["mp7", "mp8"], ["mp9", "mp10"]]},
+        {"constraint": "Order", "direction": "top_to_bottom", "instances": ["invn", "ccp", "ccn", "dp", "mn0"]},
+        {"constraint": "Order", "direction": "top_to_bottom", "instances": ["invn", "mp9", "mp7", "mn0"]},
+        {"constraint": "MultiConnection", "nets": ["vcom"], "multiplier": 6},
+        {"constraint": "AspectRatio", "subcircuit": "comparator", "ratio_low": 0.5, "ratio_high": 1.5},
+        {"constraint": "Order", "abut": True, "direction": "left_to_right", "instances": ["invn", "invp"]}
+    ]
     example = build_example(name, netlist, setup, constraints)
     run_example(example)
