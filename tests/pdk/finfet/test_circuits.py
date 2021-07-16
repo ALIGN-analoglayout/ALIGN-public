7d5916c8a78203664c701067da5d0026dd3d17a3
Soner Yaldiz <soner.yaldiz@intel.com>
2021-07-16 10:27:20 -0700

    test_cmp_good width and height

--- tests/pdk/finfet/test_circuits.py
+++ tests/pdk/finfet/test_circuits.py
@@ -1,3 +1,5 @@
+import json
+import shutil
 import pytest
 import textwrap
 try:
@@ -30,7 +32,8 @@ def test_cmp_noconst():
 
 
 @pytest.mark.nightly
-def test_cmp_1():
+def test_cmp_duo():
+    """ Two lines of symmetry """
     name = f'ckt_{get_test_id()}'
     netlist = circuits.comparator(name)
     setup = textwrap.dedent("""\
@@ -52,11 +55,12 @@ def test_cmp_1():
         {"constraint": "AspectRatio", "subcircuit": "comparator", "ratio_low": 0.7, "ratio_high": 1.3}
     ]
     example = build_example(name, netlist, setup, constraints)
-    run_example(example, cleanup=False)
+    run_example(example)
 
 
 @pytest.mark.nightly
-def test_cmp_2():
+def test_cmp_uno():
+    """ Single line of symmetry """
     name = f'ckt_{get_test_id()}'
     netlist = circuits.comparator(name)
     setup = textwrap.dedent("""\
@@ -77,7 +81,7 @@ def test_cmp_2():
         {"constraint": "AspectRatio", "subcircuit": "comparator", "ratio_low": 0.5, "ratio_high": 1.5}
     ]
     example = build_example(name, netlist, setup, constraints)
-    run_example(example, cleanup=False)
+    run_example(example)
 
 
 @pytest.mark.nightly
@@ -104,32 +108,7 @@ def test_cmp_clk():
 
 
 @pytest.mark.nightly
-def test_cmp_1():
-    """ Single line of symmetry """
-    name = f'ckt_{get_test_id()}'
-    netlist = circuits.comparator(name)
-    setup = textwrap.dedent("""\
-        POWER = vccx
-        GND = vssx
-        """)
-    constraints = [
-        {"constraint": "GroupBlocks", "instances": ["mn1", "mn2"], "name": "dp"},
-        {"constraint": "GroupBlocks", "instances": ["mn3", "mn4"], "name": "ccn"},
-        {"constraint": "GroupBlocks", "instances": ["mp5", "mp6"], "name": "ccp"},
-        {"constraint": "GroupBlocks", "instances": ["mn11", "mp13"], "name": "invp"},
-        {"constraint": "GroupBlocks", "instances": ["mn12", "mp14"], "name": "invn"},
-        {"constraint": "SymmetricBlocks", "direction": "V", 
-            "pairs": [["ccp"], ["ccn"], ["dp"], ["mn0"], ["invn", "invp"], ["mp7", "mp8"], ["mp9", "mp10"]]},
-        {"constraint": "Order", "direction": "top_to_bottom", "instances": ["invn", "ccp", "ccn", "dp", "mn0"]},
-        {"constraint": "MultiConnection", "nets": ["vcom"], "multiplier": 6},
-        {"constraint": "AspectRatio", "subcircuit": "comparator", "ratio_low": 0.5, "ratio_high": 2}
-    ]
-    example = build_example(name, netlist, setup, constraints)
-    run_example(example)
-
-
-@pytest.mark.nightly
-def test_cmp_2():
+def test_cmp_uno_2():
     """ Two lines of symmetry """
     name = f'ckt_{get_test_id()}'
     netlist = circuits.comparator(name)
@@ -208,7 +187,22 @@ def test_cmp_good():
         {"constraint": "AspectRatio", "subcircuit": "comparator", "ratio_low": 0.5, "ratio_high": 1.5}
     ]
     example = build_example(name, netlist, setup, constraints)
-    run_example(example, cleanup=False)
+    ckt_dir, run_dir = run_example(example, cleanup=False)
+
+    with (run_dir / '3_pnr' / 'Results' / f'{name}_0.placement_verilog.json').open('rt') as fp:
+        placement = json.load(fp)
+        assert 'modules' in placement, 'modules not in placement'
+        for mod in placement['modules']:
+            if mod['abstract_name'] == name:
+                break
+        w = mod['bbox'][2]-mod['bbox'][0]
+        h = mod['bbox'][3]-mod['bbox'][1]
+        w_target = 7*(8*1080) + 1080  # 7 power grid blocks with margin for grid
+        h_target = 15*(7*900) + 900  # 15 power grid blocks with margin for grid
+        assert w <= w_target, f'Layout width {w} is larger than {w_target}'
+        assert h <= h_target, f'Layout height {h} is larger than {h_target}'
+    shutil.rmtree(ckt_dir)
+    shutil.rmtree(run_dir)
 
 
 @pytest.mark.nightly
@@ -233,7 +227,7 @@ def test_cmp_bad():
         {"constraint": "AspectRatio", "subcircuit": "comparator", "ratio_low": 0.45, "ratio_high": 1.5}
     ]
     example = build_example(name, netlist, setup, constraints)
-    run_example(example, cleanup=False)
+    run_example(example)
 
 
 @pytest.mark.nightly
