19f4b1813c1e052ee782e7235dfe9ac6efceedab
kkunal1408 <kkunal1408@gmail.com>
2021-08-03 06:17:04 -0700

    model path fix

--- tests/compiler/test_all_sp.py
+++ tests/compiler/test_all_sp.py
@@ -21,19 +21,14 @@ assert examples_dir.is_dir()
 examples =  [p for p in examples_dir.rglob('*.sp') \
                 if all(x not in skip_dirs for x in p.relative_to(examples_dir).parts)]
 
-for root, dirs, files in os.walk(os.environ['ALIGN_HOME']):
-    for file in files:
-        if file.endswith("model.txt"):
-             model_path = os.path.join(root, file)
+
 
 @pytest.fixture
 def get_parser():
     parser = SpiceParser()
     assert ALIGN_HOME.is_dir()
     ah = ALIGN_HOME / 'align' # There is no align dir in ALIGN HOME in CI
-
-    model_statemenets = ALIGN_HOME/ 'align' / 'config' / 'model.txt'
-    assert model_statemenets == model_path
+    model_statemenets = ALIGN_HOME/ 'files' / 'test_circuits' / 'model.txt'
     assert model_statemenets.is_file()
     with open(model_statemenets) as f:
         lines = f.read()
@@ -42,7 +37,6 @@ def get_parser():
 
 @pytest.mark.parametrize( "design", examples)
 def test_all_examples(get_parser,design):
-    print(design.name)
     with open(design) as f:
         lines = f.read()
     get_parser.parse(lines)
