a91626aa235eec03e4a03806774988b61d27f38f
Yishuang Lin <854768750@qq.com>
2021-08-25 13:16:30 -0500

    resume ahpha to 0.995

--- PlaceRouteHierFlow/placer/PlacerHyperparameters.h
+++ PlaceRouteHierFlow/placer/PlacerHyperparameters.h
@@ -5,7 +5,7 @@ class PlacerHyperparameters {
 public:  
   double T_INT = 1e6;
   double T_MIN = 1e-6;
-  double ALPHA = 0.9;
+  double ALPHA = 0.995;
   int COUNT_LIMIT = 200;
 
   // this needs to be connected to both the log-based cost funciton and the ILP formulation
