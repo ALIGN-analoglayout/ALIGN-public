5da509b36f5e0e858542400d532fad578ce1b44f
kkunal1408 <kkunal1408@gmail.com>
2021-09-23 12:21:25 -0700

    turn off ILP testcase

--- tests/test_cmdline.py
+++ tests/test_cmdline.py
@@ -10,7 +10,7 @@ examples = [('inverter_v1',1,False,False),
             # ('adder',1,False,False),
             #Test select_in_ILP with something small
             #No longer small enough
-            ('five_transistor_ota',1,False,True),
+            # ('five_transistor_ota',1,False,True),
             #Hierarchical block fail with num_placements > 1
             ('cascode_current_mirror_ota',2,False,False),
             #Test PDN_mode
