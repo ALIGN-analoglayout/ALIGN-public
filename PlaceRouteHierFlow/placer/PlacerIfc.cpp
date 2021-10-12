6c94b7abfe388f8e5850efb8fcfd875e4d6a3887
Steven Burns <smburns47@gmail.com>
2021-10-12 08:52:49 -0700

    Closer but not working

--- PlaceRouteHierFlow/placer/PlacerIfc.cpp
+++ PlaceRouteHierFlow/placer/PlacerIfc.cpp
@@ -14,51 +14,47 @@ double ConstGraph::PII=1;
 
 PlacerIfc::PlacerIfc(PnRDB::hierNode& currentNode, int numLayout, string opath, int effort, PnRDB::Drc_info& drcInfo, const PlacerHyperparameters& hyper, bool select_in_ILP = false) : _nodeVec( numLayout, currentNode) {
   ConstGraph::LAMBDA = hyper.LAMBDA;
+
   if (hyper.use_analytical_placer) {
 
 /*
  * From PR text
  * I don't know what these values should be:
  *      dummy_init_weight, dummy_init_rate, placement_id
- *
-    clock_t start, end;
-    start = clock();
-    // EA placer
-    if(disable_io)std::cout.setstate(std::ios_base::failbit);
+ */
     Placement EA_placer;
-    EA_placer.set_dummy_net_weight(dummy_init_weight,dummy_init_rate,dummy_init_weight);
-    EA_placer.place(current_node);
-    //EA_placer.place_ut(current_node);
-    if(disable_io)std::cout.clear();
-    current_node.placement_id = placement_id;
-    PlacerIfc curr_plc1(current_node, numLayout, opath, effort, const_cast<PnRDB::Drc_info&>(drcInfo));  // do placement and update data in current node
-    current_node = curr_plc1.getNode(0);
-    // AFTER FIRST ILP, RUN THE FOLLOWING LINE
-    if(disable_io)std::cout.setstate(std::ios_base::failbit);
-    EA_placer.restore_MS(current_node);
-    // Do the ILP again
-    if(disable_io)std::cout.clear();
-    current_node.placement_id = placement_id;
-    PlacerIfc curr_plc(current_node, numLayout, opath, effort, const_cast<PnRDB::Drc_info&>(drcInfo)); // do placement and update data in current node
-    current_node = curr_plc.getNode(0);
-    if(disable_io)std::cout.setstate(std::ios_base::failbit);
-    EA_placer.break_merged_cc(current_node);
-    if(disable_io)std::cout.clear();
-    end = clock();
-    logger->info("Placement runtime: {0} s", (double)(end - start) / CLOCKS_PER_SEC);
-    // return 0;
-    // Placement
-    std::vector<PnRDB::hierNode> nodeVec={current_node};
-    logger->debug("Checkpoint: generated {0} placements", nodeVec.size());
-*/
-
-    Placement EA_placer;
-    EA_placer.place(currentNode);
     double dummy_init_weight = 1.0;
     double dummy_init_rate   = 0.01;
+    int placement_id = 0;
     EA_placer.set_dummy_net_weight(dummy_init_weight,dummy_init_rate,dummy_init_weight);
+    EA_placer.place(currentNode);
+
+    currentNode.placement_id = placement_id;
+    _nodeVec.clear();
+    _nodeVec.push_back( currentNode);
+
+    Placer curr_plc1(_nodeVec, opath, effort, drcInfo, hyper, select_in_ILP);
+
+    currentNode = getNode(0);
+
+    EA_placer.restore_MS(currentNode);
+
+    currentNode.placement_id = placement_id;
+    _nodeVec.clear();
+    _nodeVec.push_back( currentNode);
+
+    Placer curr_plc(_nodeVec, opath, effort, drcInfo, hyper, select_in_ILP);
+
+    currentNode = getNode(0);
+
+    EA_placer.break_merged_cc(currentNode);
+
+    currentNode.placement_id = placement_id;
+    _nodeVec.clear();
+    _nodeVec.push_back( currentNode);
 
   } else {
     Placer curr_plc(_nodeVec,opath,effort,drcInfo,hyper, select_in_ILP);
   }
+
 }
