841c39e06c9aa97e654848ccc9a2c80fc688d112
Yishuang Lin <854768750@qq.com>
2021-11-04 21:33:12 -0500

    fix some format

--- PlaceRouteHierFlow/placer/PlacerIfc.cpp
+++ PlaceRouteHierFlow/placer/PlacerIfc.cpp
@@ -1,23 +1,19 @@
 
-#include "PlacerIfc.h"
-
-#include "../EA_placer/placement.h"
 #include "../PnRDB/datatype.h"
+#include "PlacerIfc.h"
 #include "Placer.h"
+#include "../EA_placer/placement.h"
 
-double ConstGraph::LAMBDA = 1.;
-double ConstGraph::GAMAR = 30;
-double ConstGraph::BETA = 0.1;
-double ConstGraph::SIGMA = 1000;
-double ConstGraph::PHI = 0.05;
-double ConstGraph::PI = 0.05;
-double ConstGraph::PII = 1;
+double ConstGraph::LAMBDA=1.;
+double ConstGraph::GAMAR=30;
+double ConstGraph::BETA=0.1;
+double ConstGraph::SIGMA=1000;
+double ConstGraph::PHI=0.05;
+double ConstGraph::PI=0.05;
+double ConstGraph::PII=1;
 
-PlacerIfc::PlacerIfc(PnRDB::hierNode& currentNode, int numLayout, string opath, int effort, PnRDB::Drc_info& drcInfo, const PlacerHyperparameters& hyper,
-                     bool select_in_ILP = false)
-    : _nodeVec(numLayout, currentNode) {
+PlacerIfc::PlacerIfc(PnRDB::hierNode& currentNode, int numLayout, string opath, int effort, PnRDB::Drc_info& drcInfo, const PlacerHyperparameters& hyper, bool select_in_ILP = false) : _nodeVec( numLayout, currentNode) {
   ConstGraph::LAMBDA = hyper.LAMBDA;
-
   if (hyper.use_analytical_placer) {
     /*
      * From PR text
