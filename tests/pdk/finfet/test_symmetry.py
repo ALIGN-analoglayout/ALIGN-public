4111029e47e68f3ef98c5c4953976b4771e49abf
kkunal1408 <kkunal1408@gmail.com>
2021-11-15 16:03:04 -0800

    undo nightly move

--- tests/pdk/finfet/test_symmetry.py
+++ tests/pdk/finfet/test_symmetry.py
@@ -8,7 +8,6 @@ except BaseException:
     from utils import get_test_id, build_example, run_example
 
 
-@pytest.mark.nightly
 def test_mirror():
     name = f'ckt_{get_test_id()}'
     netlist = textwrap.dedent(f"""\
