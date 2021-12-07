d6b04c13645032fada0e139dd5090807c1b545e6
kkunal1408 <kkunal1408@gmail.com>
2021-12-07 05:05:33 -0800

    remove setup file

--- tests/pdk/finfet/test_pinless.py
+++ tests/pdk/finfet/test_pinless.py
@@ -9,7 +9,6 @@ cleanup = False
 
 def test_pinless():
     name = f'ckt_{get_test_id()}'
-    setup = textwrap.dedent("""""")
     netlist = textwrap.dedent(f"""\
         .subckt {name} vin vop vccx vssx
         xi0 vccx vop tfr_prim w=1e-6 l=1e-6 p1=pwr
@@ -18,9 +17,9 @@ def test_pinless():
     """)
     constraints = [
         {"constraint": "AutoConstraint", "isTrue": False},
-        {"constraint": "PowerPorts", "ports": ["vccx"]},
-        {"constraint": "GroundPorts", "ports": ["vssx"]},
-        {"constraint": "DoNotRoute", "nets": ["vccx", "vssx"]}
-        ]
-    example = build_example(name, netlist, setup, constraints)
+        {"constraint": "PowerPorts", "ports": ["VCCX"]},
+        {"constraint": "GroundPorts", "ports": ["VSSX"]},
+        {"constraint": "DoNotRoute", "nets": ["VCCX", "VSSX"]}
+    ]
+    example = build_example(name, netlist, constraints)
     run_example(example, cleanup=cleanup)
