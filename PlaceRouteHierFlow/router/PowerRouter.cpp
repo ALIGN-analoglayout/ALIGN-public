969117272cc62ed8d7d477a6ce2aaa49cb3e0ce6
lastdayends <liyg@tamu.edu>
2021-12-27 16:52:07 -0800

    pass the test

--- PlaceRouteHierFlow/router/PowerRouter.cpp
+++ PlaceRouteHierFlow/router/PowerRouter.cpp
@@ -575,7 +575,6 @@ void PowerRouter::CreatePowerGrid(PnRDB::hierNode& node, PnRDB::Drc_info& drc_in
   logger->debug("Create Power Grid Flag 1");
   GetData(node, drc_info, Lmetal, Hmetal);
   CreatePowerGridDrc_info(h_skip_factor, v_skip_factor);
-  this->drc_info = this->PowerGrid_Drc_info;
   this->cross_layer_drc_info = this->PowerGrid_Drc_info;
   UpdatePowerGridLLUR(Lmetal, Hmetal);
   logger->debug("Create Power Grid Flag 2");
@@ -597,6 +596,7 @@ void PowerRouter::CreatePowerGrid(PnRDB::hierNode& node, PnRDB::Drc_info& drc_in
   //this->drc_info = this->PowerGrid_Drc_info;
   std::set<RouterDB::SinkData, RouterDB::SinkDataComp> Set_x;
   InsertPlistToSet_x(Set_x, plist);
+  this->drc_info = this->PowerGrid_Drc_info;
   logger->debug("Create Power Grid Flag 10");
 
   // how to crate PowerGrid here????
