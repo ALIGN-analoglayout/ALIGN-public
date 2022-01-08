25ac2c9f0b612685b94854d1d1664bbecec3ab38
Arvind <aksharma@umn.edu>
2022-01-07 18:07:00 -0600

    adding ViaGenerator in __init__.py

--- pdks/FinFET14nm_Mock_PDK/__init__.py
+++ pdks/FinFET14nm_Mock_PDK/__init__.py
@@ -2,3 +2,4 @@ from .cap import CapGenerator
 from .res import ResGenerator
 from .mos import MOSGenerator
 from .guard_ring import RingGenerator
+from align.primitive.default.via import ViaArrayGenerator
