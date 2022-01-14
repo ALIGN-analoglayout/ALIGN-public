05691d3f300f1ce3d9c30f1685ba5dd8d3707857
kkunal1408 <kkunal1408@gmail.com>
2022-01-13 17:10:34 -0800

    add message for models.sp

--- align/compiler/read_library.py
+++ align/compiler/read_library.py
@@ -18,7 +18,7 @@ def read_models(pdk_dir: pathlib.Path, config_path=None):
     if not model_statements.exists():
         model_statements = config_path / "models.sp"
 
-    logger.info(f"Using model file from {config_path}")
+    logger.info(f"Using model file from {model_statements}")
     with open(model_statements, 'r') as f:
         lines = f.read()
     ckt_parser.parse(lines)
