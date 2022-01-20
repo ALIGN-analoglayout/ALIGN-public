17321edb2dd0ba1fab5b3753ba4d277d7c2684c1
Steven Burns <smburns47@gmail.com>
2022-01-19 16:47:45 -0800

    Reenabling check_placement

--- align/pnr/toplevel.py
+++ align/pnr/toplevel.py
@@ -396,7 +396,7 @@ def scale_and_check_placement(*, placement_verilog_d, concrete_name, scale_facto
     (pathlib.Path(opath) / f'{concrete_name}.scaled_placement_verilog.json').write_text(scaled_placement_verilog_d.json(indent=2,sort_keys=True))
     standalone_overlap_checker( scaled_placement_verilog_d, concrete_name)
     #Comment out the next two lines disable checking so you can use the GUI
-    #check_placement( scaled_placement_verilog_d, scale_factor)
+    check_placement( scaled_placement_verilog_d, scale_factor)
     if is_toplevel:
         check_place_on_grid(scaled_placement_verilog_d, concrete_name, opath)
     placement_verilog_alternatives[concrete_name] = scaled_placement_verilog_d
