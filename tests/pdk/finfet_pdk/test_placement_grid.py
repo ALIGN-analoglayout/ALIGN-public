82a8c59450c5f7ca31c1e4e5d1827a1825fbd10c
soneryal <soner.yaldiz@intel.com>
2022-01-26 16:27:26 -0800

    added a test

--- tests/pdk/finfet_pdk/test_placement_grid.py
+++ tests/pdk/finfet_pdk/test_placement_grid.py
@@ -1,5 +1,6 @@
 import os
 import json
+import shutil
 import textwrap
 from .utils import get_test_id, build_example, run_example
 from . import circuits
@@ -68,3 +69,28 @@ def test_hierarchy():
     ]
     example = build_example(name, netlist, constraints)
     run_example(example, cleanup=False)
+
+
+def test_check_constraints():
+    os.environ['PLACE_ON_GRID'] = 't'
+    name = f'ckt_{get_test_id()}'
+    netlist = textwrap.dedent(f"""\
+        .subckt {name} vi vo vbp vccx vssx
+        mp0 vo vbp vccx vccx p w=720e-9 nf=4 m=4
+        mn0 vo vi vssx vssx n w=720e-9 nf=4 m=4
+        .ends {name}
+    """)
+    constraints = []
+    example = build_example(name, netlist, constraints)
+    ckt_dir, run_dir = run_example(example, n=1, cleanup=False, additional_args=['--flow_stop', '2_primitives'])
+    name = name.upper()
+    primitives_folder = run_dir / '2_primitives'
+    for dev in ['NMOS', 'PMOS']:
+        mos = [f for f in primitives_folder.glob(f'{dev}*[0-9].json')][0]
+        with (mos).open('rt') as fp:
+            data = json.load(fp)
+            assert "metadata" in data
+            assert "constraints" in data["metadata"]
+            assert data["metadata"]["constraints"][0]["constraint"] == "place_on_grid"
+    shutil.rmtree(run_dir)
+    shutil.rmtree(ckt_dir)
