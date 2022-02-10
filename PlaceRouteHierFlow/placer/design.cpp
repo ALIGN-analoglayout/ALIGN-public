b55bf85ad9c5b2d38fea470ed06fea407016d4c5
soneryaldiz <56893713+soneryaldiz@users.noreply.github.com>
2022-02-10 10:12:25 -0800

    Placer Hangs in  PnR.placer.design.MergeNewBlockstoSymmetryGroup (#968)
    * failing test
    
    * do not add additional counterpart ordering constraints for H ordering
    
    * measure time
    
    Co-authored-by: Yishuang Lin <854768750@qq.com>

--- PlaceRouteHierFlow/placer/design.cpp
+++ PlaceRouteHierFlow/placer/design.cpp
@@ -282,7 +282,7 @@ design::design(PnRDB::hierNode& node, const int seed) {
     for (unsigned int i = 0; i < order.first.size() - 1; i++) {
       Ordering_Constraints.push_back(make_pair(make_pair(order.first[i], order.first[i + 1]), order.second == PnRDB::H ? placerDB::H : placerDB::V));
       if (Blocks[order.first[i]][0].counterpart != -1 && Blocks[order.first[i + 1]][0].counterpart != -1 &&
-          Blocks[order.first[i + 1]][0].counterpart != order.first[i])
+          Blocks[order.first[i + 1]][0].counterpart != order.first[i] && order.second == PnRDB::V)
         Ordering_Constraints.push_back(make_pair(make_pair(Blocks[order.first[i]][0].counterpart, Blocks[order.first[i + 1]][0].counterpart),
                                                  order.second == PnRDB::H ? placerDB::H : placerDB::V));
     }
