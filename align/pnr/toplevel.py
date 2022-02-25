8cb6fbb5e234fa12c1770c5e5eeae8a6fa861f92
Steven Burns <smburns47@gmail.com>
2022-02-25 14:42:39 -0800

    More refactoring

--- align/pnr/toplevel.py
+++ align/pnr/toplevel.py
@@ -89,10 +89,12 @@ def gen_abstract_verilog_d( verilog_d):
     return new_verilog_d
 
 
-def placer_driver(*, DB, opath, fpath, numLayout, effort, verilog_d,
+def placer_driver(*,
                   gui, lambda_coeff, scale_factor,
                   reference_placement_verilog_json, concrete_top_name, select_in_ILP, seed,
                   use_analytical_placer, ilp_solver, primitives, nroutings, toplevel_args, results_dir):
+
+    DB, verilog_d, fpath, opath, numLayout, effort = gen_DB_verilog_d(toplevel_args, results_dir)
 
     logger.debug(f'Using {ilp_solver} to solve ILP in placer')
 
@@ -123,7 +125,7 @@ def placer_driver(*, DB, opath, fpath, numLayout, effort, verilog_d,
     else:
         verilog_ds_to_run = [(f'{topname}_{i}', placement_verilog_alternatives[f'{topname}_{i}']) for i in placements_to_run]
 
-    return verilog_ds_to_run
+    return verilog_ds_to_run, fpath, opath, numLayout, effort
 
 
 def router_driver(*, opath, fpath, numLayout, effort, adr_mode, PDN_mode,
@@ -275,13 +277,13 @@ def toplevel(args, *, PDN_mode=False, adr_mode=False, results_dir=None, router_m
              nroutings=1, select_in_ILP=False,
              seed=0, use_analytical_placer=False, ilp_solver='symphony', primitives=None):
 
-    DB, verilog_d, fpath, opath, numLayout, effort = gen_DB_verilog_d(args, results_dir)
 
-    verilog_ds_to_run = placer_driver(DB=DB, opath=opath, fpath=fpath, numLayout=numLayout, effort=effort, verilog_d=verilog_d,
-                                      gui=gui, lambda_coeff=lambda_coeff, scale_factor=scale_factor,
-                                      reference_placement_verilog_json=reference_placement_verilog_json, concrete_top_name=concrete_top_name, select_in_ILP=select_in_ILP, seed=seed,
-                                      use_analytical_placer=use_analytical_placer, ilp_solver=ilp_solver, primitives=primitives, nroutings=nroutings,
-                                      toplevel_args=args, results_dir=results_dir)
+
+    verilog_ds_to_run, fpath, opath, numLayout, effort = \
+        placer_driver(gui=gui, lambda_coeff=lambda_coeff, scale_factor=scale_factor,
+                      reference_placement_verilog_json=reference_placement_verilog_json, concrete_top_name=concrete_top_name, select_in_ILP=select_in_ILP, seed=seed,
+                      use_analytical_placer=use_analytical_placer, ilp_solver=ilp_solver, primitives=primitives, nroutings=nroutings,
+                      toplevel_args=args, results_dir=results_dir)
 
     res_dict = router_driver(opath=opath, fpath=fpath, numLayout=numLayout, effort=effort, adr_mode=adr_mode, PDN_mode=PDN_mode,
                              router_mode=router_mode, skipGDS=skipGDS, lambda_coeff=lambda_coeff, scale_factor=scale_factor,
