c7a408ddb958565bb22d3841e9a7dd5177777f77
soneryal <soner.yaldiz@intel.com>
2022-02-28 12:40:54 -0800

    missing base model

--- tests/pdk/finfet_pdk/test_resistor.py
+++ tests/pdk/finfet_pdk/test_resistor.py
@@ -63,15 +63,45 @@ def test_res_one():
     netlist = textwrap.dedent(f"""\
         .subckt {name} a b
         xi0 a b tfr w=2e-6 l=1e-6
-        xi1 a b tfr_prim w=2e-6 l=1e-6
-        xi2 a b tfr_prim w=2e-6 l=2e-6
+        xi1 a b tfr w=2e-6 l=1e-6
+        xi2 a b tfr w=2e-6 l=2e-6
+        xi3 a b tfr_prim w=2e-6 l=1e-6
+        xi4 a b tfr_prim w=2e-6 l=1e-6
+        xi5 a b tfr_prim w=2e-6 l=2e-6
         .ends {name}
     """)
     constraints = [{"constraint": "AutoConstraint", "isTrue": False, "propagate": True}]
     example = build_example(name, netlist, constraints)
     _, run_dir = run_example(example, cleanup=False, n=1, additional_args=['--flow_stop', '2_primitives'])
+
+    name = name.upper()
     with (run_dir / '1_topology' / '__primitives_library__.json').open('rt') as f1:
         primitives = json.load(f1)
+        primitives = {p['name']: p for p in primitives}
+    with (run_dir / '1_topology' / f'{name}.verilog.json').open('rt') as f1:
+        modules = json.load(f1)
+        modules = {p['name']: p for p in modules['modules']}
+
+    instances = {inst['instance_name']: inst for inst in modules[name]['instances']}
+
+    def find_instance(instances, pattern):
+        inst = [key for key in instances.keys() if all([p in key for p in pattern])]
+        assert len(inst) > 0, f'{pattern} not found in {instances.keys()}'
+        assert len(inst) == 1, f'Collision: {inst}'
+        return instances[inst[0]]
+
+    atn = 'abstract_template_name'
+
+    i0 = find_instance(instances, ['XI0'])
+    i1 = find_instance(instances, ['XI1'])
+    i2 = find_instance(instances, ['XI2'])
+    i3 = find_instance(instances, ['XI3'])
+    i4 = find_instance(instances, ['XI4'])
+    i5 = find_instance(instances, ['XI5'])
+    assert i0[atn] == i1[atn] != i2[atn]
+    assert i3[atn] == i4[atn] != i5[atn]
+
+    assert primitives['TFR_PRIM']['base'] == 'TFR'
 
     shutil.rmtree(run_dir)
     shutil.rmtree(example)
