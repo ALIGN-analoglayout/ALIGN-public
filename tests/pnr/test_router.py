4e91e9fd8e33f1d7dde2371a234ea890aa9cdf25
soneryal <soner.yaldiz@intel.com>
2022-03-03 13:34:33 -0800

    failing test

--- tests/pnr/test_router.py
+++ tests/pnr/test_router.py
@@ -10,3 +10,15 @@ def test_ru_zero():
     cv.addWire(cv.m1, 'A', 4, (0, 1), (4, -1), netType='pin')
     cv.bbox = transformation.Rect(*[0, 0, 10*cv.pdk['M1']['Pitch'], 10*cv.pdk['M2']['Pitch']])
     run_postamble(name, cv, max_errors=0)
+
+
+def test_ru_1():
+    name = get_test_id()
+    cv = CanvasPDK()
+    cv.addWire(cv.m1, None, 1, (1, -1),  (19, 1))
+    cv.addWire(cv.m1, 'A',  2, (1, -1),  (6, 1), netType='pin')
+    cv.addWire(cv.m1, None, 3, (1, -1),  (6, 1))
+    cv.addWire(cv.m1, 'A',  3, (10, -1), (19, 1), netType='pin')
+    cv.addWire(cv.m1, None, 4, (1, -1),  (19, 1))
+    cv.bbox = transformation.Rect(*[0, 0, 6*cv.pdk['M1']['Pitch'], 20*cv.pdk['M2']['Pitch']])
+    run_postamble(name, cv, max_errors=0)
