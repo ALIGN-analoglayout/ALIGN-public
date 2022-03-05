0e440e835043fb4d765025072f1daea715f5e9b3
kkunal1408 <kkunal1408@gmail.com>
2022-03-05 11:25:55 -0800

    user-const tests

--- tests/compiler/test_user_constraint.py
+++ tests/compiler/test_user_constraint.py
@@ -2,10 +2,13 @@ import pathlib
 import pytest
 import json
 import shutil
-
+import textwrap
 from align.compiler.compiler import compiler_input, annotate_library
 from align.compiler.find_constraint import  constraint_generator
 from align.schema.checker import SolutionNotFoundError
+from align.schema import SubCircuit, constraint
+from utils import clean_data, build_example, get_test_id
+
 
 pdk_dir = (
     pathlib.Path(__file__).resolve().parent.parent.parent
@@ -110,3 +113,73 @@ def test_scf():
         gold_const = json.load(const_fp)
         gold_const.sort(key=lambda item: item.get("constraint"))
     assert gold_const == gen_const
+
+
+def test_merged_const():
+    name = f'ckt_{get_test_id()}'.upper()
+    netlist = textwrap.dedent(
+        f"""\
+        .subckt param_mos D G S B
+        mn1 D G S1 B n nfin=tf nf=n m=8
+        mn2 D1 G S B n nfin=tf nf=n m=8
+        .ends param_mos
+
+        .subckt {name} D G S B
+        xi1 D G S B param_mos
+        .ends {name}
+    """
+    )
+    constraints = []
+    example = build_example(name, netlist, constraints)
+    constraints = [
+        {'subcircuit': name,
+         'constraints': [{"constraint": "GroundPorts", "ports": ["S"]}]
+         },
+        {'subcircuit': 'PARAM_MOS',
+         'constraints': [{"constraint": "DoNotUseLib", "libraries": ["DP_NMOS_B"]}]
+         },
+    ]
+    with open(example.parent / f"const.json", "w") as fp:
+        fp.write(json.dumps(constraints, indent=2))
+    ckt_library, _ = compiler_input(example, name, pdk_dir, config_path)
+    all_modules = set([name, 'PARAM_MOS'])
+    available_modules = set(
+        [module.name for module in ckt_library if isinstance(module, SubCircuit)]
+    )
+    assert available_modules == all_modules, f"{available_modules}"
+    assert ckt_library.find(name).constraints.dict()['__root__'] == [{"constraint": "ground_ports", "ports": ["S"]}]
+    assert ckt_library.find('PARAM_MOS').constraints.dict()['__root__'] == [{"constraint": "do_not_use_lib", "libraries": ["DP_NMOS_B"], 'propagate': None},
+                                                                            {"constraint": "ground_ports", "ports": ["S"]}]
+    clean_data(name)
+
+
+def test_group_cap():
+    name = f'ckt_{get_test_id()}'.upper()
+    netlist = textwrap.dedent(
+        f"""\
+        .subckt {name} in1 out1 out2 vss
+        c2 in1 out1 30e-15
+        c1 in2 out2 30e-15
+        .ends {name}
+    """
+    )
+    constraints = [
+        {"constraint": "GroupCaps", "name": "cap_group1",
+         "instances": ["C1", "C2"],
+         "num_units": [2, 2],
+         "unit_cap": "Cap_12f",
+         "dummy": True
+         }
+        ]
+    mod_const = [
+        {"constraint": "group_caps", "name": "cap_group1",
+         "instances": ["C1", "C2"],
+         "num_units": [2, 2],
+         "unit_cap": "Cap_12f",
+         "dummy": True
+         }
+    ]
+    example = build_example(name, netlist, constraints)
+    ckt_library, _ = compiler_input(example, name, pdk_dir, config_path)
+    assert ckt_library.find(name).constraints.dict()['__root__'] == mod_const
+    clean_data(name)
