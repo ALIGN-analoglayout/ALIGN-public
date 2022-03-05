62ccc74bdae76e3bb39fcaaea1c9cfc7de004393
kkunal1408 <kkunal1408@gmail.com>
2022-03-05 07:05:52 -0800

    remove default models.sp

--- align/compiler/read_library.py
+++ align/compiler/read_library.py
@@ -20,19 +20,11 @@ def read_models(pdk_dir: pathlib.Path, config_path=None):
     pdk_models = get_generator('pdk_models', pdk_dir)
     library = Library(loadbuiltins=True, pdk_models=pdk_models)
     ckt_parser = SpiceParser(library=library)
-    # Read model file to map devices
-    if config_path is None:
-        config_path = pathlib.Path(__file__).resolve().parent.parent / "config"
 
     model_statements = pdk_dir / "models.sp"
 
     if not model_statements.exists():
-        if not pdk_dir.exists():
-            logger.warning(f"Missing pdk directory for reading model {pdk_dir} ")
-        else:
-            logger.warning(f"Missing models.sp file in PDK directory {model_statements}")
-        model_statements = config_path / "models.sp"
-
+        logger.warning(f"Missing {model_statements}, only basic models will be used")
     logger.info(f"Using model file from {model_statements}")
     with open(model_statements, 'r') as f:
         lines = f.read()
