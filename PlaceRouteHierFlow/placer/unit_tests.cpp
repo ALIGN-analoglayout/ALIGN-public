adf5c4883d83c68eda8ad94650195c4bb957923f
Steven Burns <smburns47@gmail.com>
2022-03-16 13:31:21 -0700

    Add C++ unit test for SeqPair

--- PlaceRouteHierFlow/placer/unit_tests.cpp
+++ PlaceRouteHierFlow/placer/unit_tests.cpp
@@ -1,7 +1,31 @@
+#include "spdlog/spdlog.h"
 
 #include <gtest/gtest.h>
+#include "SeqPair.h"
+#include "design.h"
 
 TEST(PlacerTest, True) {
     EXPECT_TRUE( 1);
 };
 
+TEST(PlacerTest, False) {
+    EXPECT_FALSE( 0);
+};
+
+TEST(SeqPairTest, Constructor) {
+    spdlog::set_level(spdlog::level::debug);
+    SeqPair sp;
+    sp.PrintSeqPair();
+};
+
+TEST(SeqPairTest, KeepOrdering) {
+    spdlog::set_level(spdlog::level::debug);
+    SeqPair sp;
+    sp.PrintSeqPair();
+
+    design d;
+
+    sp.KeepOrdering(d);
+
+
+};
