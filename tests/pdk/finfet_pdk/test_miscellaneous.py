055df1dfbeaee0376d514db83755a77a1e49662e
soneryal <soner.yaldiz@intel.com>
2022-04-07 11:21:49 -0700

    logic fix; flake8; monkeypath test_integration

--- tests/pdk/finfet_pdk/test_miscellaneous.py
+++ tests/pdk/finfet_pdk/test_miscellaneous.py
@@ -29,3 +29,29 @@ def test_floating_pin():
     ]
     example = build_example(name, netlist, constraints)
     run_example(example, cleanup=False, n=1)
+
+
+def test_floating_power_pin():
+    name = f'ckt_{get_test_id()}'
+    netlist = textwrap.dedent(f"""\
+    .subckt sw d g s vss vcc
+    qn0 d g s vss n m=1 nf=4 w=180e-9
+    qn1 d g s vss n m=2 nf=2 w=90e-9
+    .ends sw
+    .subckt {name} d g s vccx vssx
+    xi<0> d g s vssx vccx sw
+    xi<1> d g s vssx vccx sw
+    xi<2> d g s vssx vccx sw
+    xi<3> d g s vssx vccx sw
+    xi<4> d g s vssx vccx sw
+    .ends {name}
+    .END
+    """)
+    constraints = [
+        {"constraint": "PowerPorts", "ports": ["vccx"]},
+        {"constraint": "GroundPorts", "ports": ["vssx"]},
+        {"constraint": "SameTemplate", "instances": [f"xi<{i}>" for i in range(5)]},
+        {"constraint": "AlignInOrder", "direction": "horizontal", "instances": [f"xi<{i}>" for i in range(5)]}
+    ]
+    example = build_example(name, netlist, constraints)
+    run_example(example, cleanup=False, n=1)
