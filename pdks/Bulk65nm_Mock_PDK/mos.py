caf4ccf3f927d99471c84fb4d225b3650c5ac67c
Arvind <aksharma@umn.edu>
2022-05-19 21:45:24 -0500

    modifying M1 length

--- pdks/Bulk65nm_Mock_PDK/mos.py
+++ pdks/Bulk65nm_Mock_PDK/mos.py
@@ -1,7 +1,7 @@
 from align.primitive.default.canvas import DefaultCanvas
 from align.cell_fabric.generators import *
 from align.cell_fabric.grid import *
-from math import floor
+from math import floor, ceil
 
 import logging
 logger = logging.getLogger(__name__)
@@ -29,9 +29,9 @@ class MOSGenerator(DefaultCanvas):
         assert gateDummy > 0
         unitCellLength = self.gatesPerUnitCell* self.pdk['Poly']['Pitch']
         activeOffset = self.unitCellHeight//2 - self.pdk['Fin']['Pitch']//2
-        activeWidth =  self.pdk['Fin']['Pitch']*fin
+        self.activeWidth =  self.pdk['Fin']['Pitch']*fin
         activePitch = self.unitCellHeight
-        RVTWidth = activeWidth + 2*self.pdk['Active']['active_enclosure']
+        RVTWidth = self.activeWidth + 2*self.pdk['Active']['active_enclosure']
 
 
         stoppoint = self.pdk['Active']['activePolyExTracks']*self.pdk['M2']['Pitch']
@@ -39,7 +39,7 @@ class MOSGenerator(DefaultCanvas):
                                      clg=UncoloredCenterLineGrid( pitch= self.pdk['Poly']['Pitch'], width= self.pdk['Poly']['Width'], offset= self.pdk['Poly']['Offset']),
                                      spg=EnclosureGrid( pitch=self.unitCellHeight, offset=self.pdk['M2']['Offset'], stoppoint=stoppoint, check=True)))
 
-        stoppoint = (self.unitCellHeight-activeWidth)//2 - self.pdk['Layer1']['Active_enclosure']
+        stoppoint = (self.unitCellHeight-self.activeWidth)//2 - self.pdk['Layer1']['Active_enclosure']
         self.layer1 = self.addGen( Wire( 'layer1', 'Layer1', 'v',
                                      clg=UncoloredCenterLineGrid( pitch= self.gatesPerUnitCell*self.pdk['Poly']['Pitch'], width= self.pdk['Poly']['Pitch']+2*self.pdk['Layer1']['Offset'], offset= (self.gateDummy+1)*self.pdk['Poly']['Pitch']),
                                      spg=EnclosureGrid( pitch=self.unitCellHeight, offset=-self.pdk['Fin']['Pitch']//2, stoppoint=stoppoint, check=True)))
@@ -50,7 +50,7 @@ class MOSGenerator(DefaultCanvas):
 
         stoppoint = ((self.gateDummy-1)* self.pdk['Poly']['Pitch'] +  self.pdk['Poly']['Offset'])*(1-self.shared_diff)
         self.active = self.addGen( Wire( 'active', 'Active', 'h',
-                                         clg=UncoloredCenterLineGrid( pitch=activePitch, width=activeWidth, offset=activeOffset),
+                                         clg=UncoloredCenterLineGrid( pitch=activePitch, width=self.activeWidth, offset=activeOffset),
                                          spg=EnclosureGrid( pitch=unitCellLength, offset=0, stoppoint=stoppoint, check=True)))
 
         self.RVT = self.addGen( Region( 'RVT', 'Rvt',
@@ -84,7 +84,7 @@ class MOSGenerator(DefaultCanvas):
                                             h_grid=self.fin.clg))
 
         self.active_diff = self.addGen( Wire( 'active_diff', 'Active', 'h',
-                                         clg=UncoloredCenterLineGrid( pitch=activePitch, width=activeWidth, offset=activeOffset),
+                                         clg=UncoloredCenterLineGrid( pitch=activePitch, width=self.activeWidth, offset=activeOffset),
                                          spg=SingleGrid( pitch=self.pdk['Poly']['Pitch'], offset=(self.gateDummy-1)*self.pdk['Poly']['Pitch']+self.pdk['Poly']['Pitch']//2)))
         
         stoppoint = unitCellLength//2-self.pdk['Active']['activebWidth_H']//2
@@ -111,8 +111,8 @@ class MOSGenerator(DefaultCanvas):
 
         self.v0.h_clg.addCenterLine( 0,                 self.pdk['V0']['WidthY'], False)
         v0pitch = self.pdk['V0']['WidthY'] + self.pdk['V0']['SpaceY']
-        v0Offset = activeOffset - activeWidth//2 + self.pdk['V0']['VencA_L'] + self.pdk['V0']['WidthY']//2
-        v0_number = floor((activeWidth-2*self.pdk['V0']['VencA_L']-self.pdk['V0']['WidthY'])/v0pitch + 1)
+        v0Offset = activeOffset - self.activeWidth//2 + self.pdk['V0']['VencA_L'] + self.pdk['V0']['WidthY']//2
+        v0_number = floor((self.activeWidth-2*self.pdk['V0']['VencA_L']-self.pdk['V0']['WidthY'])/v0pitch + 1)
         assert v0_number > 0, "V0 can not be placed in the active region"
         v0_number = v0_number if v0_number < 4 else v0_number - 1 ## To avoid voilation of V0 enclosure by M1 DRC
         for i in range(v0_number):
@@ -125,7 +125,7 @@ class MOSGenerator(DefaultCanvas):
         self.subinsts[fullname].parameters.update(parameters)
 
         def _connect_diffusion(i, pin):
-            self.addWire( self.m1, None, i, (grid_y0, -1), (grid_y1, 1))
+            self.addWire( self.m1, None, i, (grid_y0-m1_length//2, -1), (grid_y0+m1_length//2, 1))
             for j in range(1,self.v0.h_clg.n): ## self.v0.h_clg.n??
                 self.addVia( self.v0, f'{fullname}:{pin}', i, (y, j))
             self._xpins[name][pin].append(i)
@@ -147,7 +147,9 @@ class MOSGenerator(DefaultCanvas):
             self.addWire( self.pl, None, i+self.gatesPerUnitCell*x+self.gateDummy,   (y,1), (y+1,-1))
 
         # Source, Drain, Gate Connections
-        grid_y0 = y*self.m2PerUnitCell + 4
+        grid_y0 = y*self.m2PerUnitCell + self.m2PerUnitCell//2
+        m1_length = ceil(self.activeWidth/self.pdk['M2']['Pitch'])
+        print(m1_length)
         grid_y1 = (y+1)*self.m2PerUnitCell-5
         gate_x = self.gateDummy*self.shared_diff + x * self.gatesPerUnitCell + self.gatesPerUnitCell // 2
         # Connect Gate (gate_x)
@@ -175,7 +177,8 @@ class MOSGenerator(DefaultCanvas):
         center_track = y * self.m2PerUnitCell + self.m2PerUnitCell // 2 # Used for m1 extension
         grid_y1 = (y+1)*self.m2PerUnitCell-5
         gate_track = 0
-        diff_track = 4
+        diff_track = self.m2PerUnitCell // 2 - ceil(self.activeWidth/self.pdk['M2']['Pitch'])-1
+        print(diff_track)
         body_v0_track = (self.lFin*self.pdk['Fin']['Pitch'])//(2*self.pdk['M2']['Pitch'])
         for (net, conn) in connections.items():
             contactsx = {(track, pin) for inst, pins in self._xpins.items()
