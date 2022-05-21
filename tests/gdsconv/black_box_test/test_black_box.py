9a73f062bffac6bc3100440e65e5d87277a579d8
kkunal1408 <kkunal1408@gmail.com>
2022-05-21 16:56:27 +0530

    mark black box as nightly

--- tests/gdsconv/black_box_test/test_black_box.py
+++ tests/gdsconv/black_box_test/test_black_box.py
@@ -3,6 +3,8 @@ import align
 import os
 import shutil
 import align
+import pytest
+
 
 from align.gdsconv.gds2primitive import GEN_PRIMITIVE_FROM_GDS
 
@@ -17,9 +19,11 @@ ALIGN_WORK_DIR = ALIGN_HOME / 'tests' / 'tmp'
 if 'ALIGN_WORK_DIR' in os.environ:
     ALIGN_WORK_DIR = pathlib.Path(os.environ['ALIGN_WORK_DIR']).resolve()
 
-def test_black_box_dp_in_hsc():
+@pytest.mark.nightly
+def test_black_box_dp_in_hsc ():
     nm = 'black_box_test'
     run_dir = ALIGN_WORK_DIR / f'{nm}'
+    print(run_dir)
     if run_dir.exists():
         assert run_dir.is_dir()
         shutil.rmtree(run_dir)
@@ -28,10 +32,10 @@ def test_black_box_dp_in_hsc():
     primdir = run_dir / '2_primitives'
     topodir = run_dir / '1_topology'
     os.chdir(run_dir)
-    args = ['-p', str(pdkdir), '--flow_stop', '2_primitives', str(spdir)]
+    args = ['-p', pdkdir.as_posix(), '--flow_stop', '2_primitives', spdir.as_posix()]
     results = align.CmdlineParser().parse_args(args)
     assert results is not None
-    _ = GEN_PRIMITIVE_FROM_GDS(str(gdsdir), str(pdkdir / 'layers.json'), str(primdir), str(topodir))
-    args = ['-p', str(pdkdir), '--flow_start', '3_pnr', '--place_using_ILP', '--skipGDS', str(spdir)]
+    genprim = GEN_PRIMITIVE_FROM_GDS(gdsdir.as_posix(), (pdkdir / 'layers.json').as_posix(), primdir.as_posix(), topodir.as_posix())
+    args = ['-p', pdkdir.as_posix(), '--flow_start', '3_pnr', '--place_using_ILP', '--skipGDS', spdir.as_posix()]
     results = align.CmdlineParser().parse_args(args)
     assert results is not None
