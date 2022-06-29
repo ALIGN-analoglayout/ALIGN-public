31adf737f36e0ffb4449a4e85d37012b63876b30
soneryal <soner.yaldiz@intel.com>
2022-06-29 13:56:28 -0700

    refactor tests/pdks

--- tests/pdks/test_cap.py
+++ tests/pdks/test_cap.py
@@ -1,15 +1,23 @@
+import os
 import pathlib
-import sys
 import pytest
+from .utils import get_test_id, WORK_DIR
+import importlib
 
 pdks = []
 for pdk in (pathlib.Path(__file__).parent.parent.parent / 'pdks').iterdir():
+    if pdk.stem == "Bulk45nm_Mock_PDK":
+        continue
     if pdk.is_dir() and (pdk / 'fabric_Cap.py').exists():
         pdks.append(pdk)
 
 
-def check_shorts(cmdlist):
-    from fabric_Cap import main, gen_parser
+def check_shorts(pdk, cmdlist):
+    spec = importlib.util.spec_from_file_location("fabric_Cap", pdk / 'fabric_Cap.py')
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    main = getattr(module, "main")
+    gen_parser = getattr(module, "gen_parser")
     parser = gen_parser()
     args = parser.parse_args(cmdlist)
     uc = main(args)
@@ -19,22 +27,25 @@ def check_shorts(cmdlist):
 
 
 def build_test(pdk, b, *, w, l):
-    sys.path.insert(0, str(pdk))
-    # print(str(pdk))
-    check_shorts(['-b', b, '-w', f"{w}", '-l',f"{l}"])
-    sys.path.pop(0)
+    check_shorts(pdk, ['-b', b, '-w', f"{w}", '-l', f"{l}"])
 
 
 @pytest.mark.parametrize("pdk", pdks, ids=lambda x: x.name)
 def test_cap_smoke(pdk):
+    test_dir = WORK_DIR / "cap_smoke" / get_test_id()
+    test_dir.mkdir(parents=True, exist_ok=True)
+    os.chdir(test_dir)
     l = 200
     w = 200
     build_test(pdk, f'cap_{w}_{l}', w=w, l=l)
 
 
 @pytest.mark.nightly
-@pytest.mark.parametrize("l", range(200, 1000, 400), ids=lambda x: f'n{x}')
-@pytest.mark.parametrize("w", range(200, 1000, 400), ids=lambda x: f'n{x}')
+@pytest.mark.parametrize("l", range(200, 1000, 400), ids=lambda x: f'l{x}')
+@pytest.mark.parametrize("w", range(200, 1000, 400), ids=lambda x: f'w{x}')
 @pytest.mark.parametrize("pdk", pdks, ids=lambda x: x.name)
 def test_cap_full(pdk, l, w):
+    test_dir = WORK_DIR / "cap_smoke" / get_test_id()
+    test_dir.mkdir(parents=True, exist_ok=True)
+    os.chdir(test_dir)
     build_test(pdk, f'cap_{w}_{l}', w=w, l=l)
