a7d572ba36f216522b6c5c70944a0484ed87696b
soneryaldiz <56893713+soneryaldiz@users.noreply.github.com>
2022-10-31 12:41:03 -0700

    Revise LDO amplifier example and tests (#956)
    * move ldo_error_amp test case to finfet_pdk
    
    * revise test
    
    * remove ldo_error_amp_v2
    
    * remove redundant skip

--- tests/pdk/finfet_pdk/test_circuits.py
+++ tests/pdk/finfet_pdk/test_circuits.py
@@ -196,7 +196,18 @@ def test_tia():
     run_example(example, cleanup=CLEANUP)
 
 
-@pytest.mark.skip
+def test_ldo_amp_simple():
+    name = f'ckt_{get_test_id()}'
+    netlist = circuits.ldo_amp_simple(name)
+    constraints = [
+        {"constraint": "PowerPorts", "ports": ["vccx"]},
+        {"constraint": "GroundPorts", "ports": ["vssx"]},
+        {"constraint": "DoNotUseLib", "libraries": ["CASCODED_CMC_NMOS", "CMB_PMOS_2", "LSB_PMOS_2", "LSB_NMOS_2"]}
+    ]
+    example = build_example(name, netlist, constraints)
+    run_example(example, cleanup=CLEANUP, log_level=LOG_LEVEL, max_errors=1, additional_args=["--placer_sa_iterations", "100"])
+
+
 def test_ldo_amp():
     name = f'ckt_{get_test_id()}'
     netlist = circuits.ldo_amp(name)
@@ -206,7 +217,7 @@ def test_ldo_amp():
         {"constraint": "DoNotUseLib", "libraries": ["CASCODED_CMC_NMOS", "CMB_PMOS_2", "LSB_PMOS_2", "LSB_NMOS_2"]}
     ]
     example = build_example(name, netlist, constraints)
-    run_example(example, cleanup=CLEANUP, log_level=LOG_LEVEL)
+    run_example(example, cleanup=CLEANUP, log_level=LOG_LEVEL, additional_args=["--placer_sa_iterations", "100"])
 
 
 def test_ro_simple():
@@ -427,6 +438,7 @@ def test_niwc_opamp_split():
     if CLEANUP:
         shutil.rmtree(run_dir)
         shutil.rmtree(ckt_dir)
+
 
 def test_niwc_opamp_split_reuse():
     # Tests legal size and exact_patterns restrictions
