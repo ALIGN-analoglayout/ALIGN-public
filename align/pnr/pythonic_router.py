fd180cb6224a599e0c94761796e4860ed84e4385
Steven Burns <smburns47@gmail.com>
2023-01-10 09:50:14 -0800

    Schur decomposition

--- align/pnr/pythonic_router.py
+++ align/pnr/pythonic_router.py
@@ -72,6 +72,43 @@ def draw_routing_problems(module, flat_tbl):
         fig.add_trace(go.Scatter(x=x_lst, y=y_lst, text=nm_lst, mode="text", textfont=dict(color="black", size=16)))
         fig.show()
 
+def draw_routing_problems_grid(module, grid_tbl):
+    colorscale = px.colors.qualitative.Alphabet
+
+    _, _, width, height = module['bbox']
+    for idx, (k, v) in enumerate(grid_tbl.items()):
+        # color = colorscale[idx % len(colorscale)]
+
+        fig = go.Figure()
+        fig.update_xaxes(range=[0,width])
+        fig.update_yaxes(
+            range=[0,height],
+            scaleanchor='x',
+            scaleratio=1
+        )
+
+        x_lst, y_lst, nm_lst = [], [], []
+
+        layer_colors = {
+            'M1': 'red',
+            'M2': 'blue',
+            'M3': 'green',
+            'M4': 'plum',
+            'M5': 'brown'
+        }
+
+        for layer, (center, bgn, end) in v:
+            color = layer_colors[layer]
+            llx, lly, urx, ury = rect
+            fig.add_shape(type="rect", x0=llx, y0=lly, x1=urx, y1=ury, line=dict(color="RoyalBlue", width=1), fillcolor=color)
+
+            x_lst.append(urx)
+            y_lst.append(ury)
+            nm_lst.append(k)
+
+        fig.add_trace(go.Scatter(x=x_lst, y=y_lst, text=nm_lst, mode="text", textfont=dict(color="black", size=16)))
+        fig.show()
+
 
 def rect_to_grid(canvas, layer, rect):
     gen = canvas._find_generator(layer)
@@ -118,6 +155,8 @@ def collect_pins(map_d_in, scaled_placement_verilog_d, top_level_args):
 
     for module in scaled_placement_verilog_d['modules']:
         flat_tbl = defaultdict(list)
+        grid_tbl = defaultdict(list)
+
         for instance in module['instances']:
             ctn = instance['concrete_template_name']
             tr = transformation.Transformation(**instance['transformation'])
@@ -128,13 +167,12 @@ def collect_pins(map_d_in, scaled_placement_verilog_d, top_level_args):
                 for layer, rect in pin_tbl[ctn][formal]:
                     newRect = tr.hitRect(transformation.Rect(*rect)).canonical().toList()
 
-                    rect_to_grid(canvas, layer, newRect)
-
-
+                    grid_tbl[actual].append((layer, rect_to_grid(canvas, layer, newRect)))
                     flat_tbl[actual].append((layer, newRect))
 
         flat_tbl = route_by_stretch(flat_tbl, Canvas)
-
+        
+        print(grid_tbl)
         draw_routing_problems(module, flat_tbl)
 
 
---
+++ dev/Experimental/max_point_to_point_resistance/test_A.py
@@ -0,0 +1,406 @@
+import pytest
+
+import numpy as np
+import scipy.linalg as la
+
+from scipy import sparse
+import scipy.sparse.linalg as sla
+
+from collections import defaultdict
+import itertools
+
+
+def par(a, b):
+    return a*b/(a+b)
+
+def wye2del(a, b, c):
+    s = a*b + a*c + b*c
+    # bc, ac, ab
+    return s/a, s/b, s/c
+
+def grid5():
+
+    bc, ac, ab = wye2del(0.5, 0.5, 1)
+    bc = par(bc, 0.5)
+
+    tmp = ac
+
+    a = ab
+    b = bc
+    c = 0.75
+
+    bc, ac, ab = wye2del(a, b, c)
+
+    return 2*par(par(tmp, ab) + par(0.25, bc), ac)
+
+def grid6():
+
+    x_uv, x_ov, x_ou = wye2del(0.5, 0.5, 0.5)
+    
+    q_vt, q_uv, q_ut = wye2del(0.5, 0.25, 0.5)
+    
+    ou = x_ou
+    ov = par(1, x_ov)
+    uv = par(x_uv, q_uv)
+    ut = par(0.75, q_ut)
+    vt = par(1, q_vt)
+
+    u_ot, u_ov, u_tv = wye2del(uv, ut, ou)
+
+    return 2*(par(par(ov, u_ov) + par(vt, u_tv), u_ot) + 0.5)
+
+def stamp(lst, ref, ports):
+    # lst is a list of triples two nodes and a conductance
+
+    all_nodes = set()
+    for (a, b, g) in lst:
+        all_nodes.add(a)
+        all_nodes.add(b)
+
+    mapping = {}
+
+    for a in all_nodes:
+        if a == ref:
+            pass
+        elif a in ports:
+            pass
+        else:
+            if a not in mapping:
+                mapping[a] = len(mapping)
+
+    split_point = len(mapping)
+
+    for a in all_nodes:
+        if a == ref:
+            pass
+        elif a in ports:
+            if a not in mapping:
+                mapping[a] = len(mapping)
+
+    nports = len(mapping) - split_point
+
+    mapping[ref] = None
+
+
+    new_lst = []
+    for (a, b, g) in lst:
+        new_lst.append((mapping[a], mapping[b], g))
+
+
+    A = defaultdict(float)
+    B = defaultdict(float)
+    C = defaultdict(float)
+    D = defaultdict(float)
+
+    def update(i, j, incr):
+        if i < split_point and j < split_point:
+            A[(i,j)] += incr
+        elif i < split_point and j >= split_point:
+            B[(i,j-split_point)] += incr
+        elif i >= split_point and j < split_point:
+            C[(i-split_point,j)] += incr
+        elif i >= split_point and j >= split_point:
+            D[(i-split_point,j-split_point)] += incr
+
+    for (a, b, g) in new_lst:
+        if a is not None:
+            update(a, a, g)
+
+        if a is not None and b is not None:            
+            update(a, b, -g)
+            update(b, a, -g)
+
+        if b is not None:
+            update(b, b, g)
+
+    def build_sparse(M, rows, cols):
+        i = [i for ((i, j), g) in M.items()]
+        j = [j for ((i, j), g) in M.items()]
+        g = [g for ((i, j), g) in M.items()]
+        return sparse.coo_array((g, (i,j)), shape=(rows,cols)).tocsc()
+
+    return \
+        build_sparse(A, split_point, split_point), \
+        build_sparse(B, split_point, nports), \
+        build_sparse(C, nports, split_point), \
+        build_sparse(D, nports, nports)
+
+def solve(A, B, C, D, b1):
+    # A x0 + B x1 = 0
+    # C x0 + D x1 = b1
+    # x0 = -A^-1 B x1
+    # (D - C A^-1 B) x1 = b1
+
+    S = D - C @ sla.spsolve(A, B)
+    # print(f'S: {S} {S.shape}')
+    return sla.spsolve(S, b1)
+
+def test_stamp():
+    # 0   1   2   3   4
+    # T---x---G---x---T
+
+    A, B, C, D = stamp([(0,1,1), (1,2,1), (2,3,1), (3,4,1)], 2, set([0,4]))
+    # print(f'A: {A} {A.shape}')
+    # print(f'B: {B} {B.shape}')
+    # print(f'C: {C} {C.shape}')
+    # print(f'D: {D} {D.shape}')
+
+    print(solve(A, B, C, D, np.eye(2)))
+
+
+def test_stamp1():
+    # 0   1   2   3   4   5
+    # G---T---x---T---x---T
+
+    A, B, C, D = stamp([(0,1,1), (1,2,1), (2,3,1), (3,4,1), (4,5,1)], 0, set([1,3,5]))
+    # print(f'A: {A} {A.shape}')
+    # print(f'B: {B} {B.shape}')
+    # print(f'C: {C} {C.shape}')
+    # print(f'D: {D} {D.shape}')
+
+    print(solve(A, B, C, D, np.eye(3)))
+
+@pytest.mark.parametrize("n", [2,3,4,5,6])
+def test_stamp_square_corners(n):
+    lst = []
+    for i in range(n):
+        for j in range(1, n):
+            lst.append((n*i+j-1,n*i+j,1))
+    for j in range(n):
+        for i in range(1, n):
+            lst.append((n*(i-1)+j,n*i+j,1))
+
+    A, B, C, D = stamp(lst, n*n//2, set([0,(n-1)*n+n-1]))
+    # print(f'A: {A} {A.shape}')
+    # print(f'B: {B} {B.shape}')
+    # print(f'C: {C} {C.shape}')
+    # print(f'D: {D} {D.shape}')
+
+    solution = solve(A, B, C, D, np.eye(2))
+    print(solution)
+
+    # S^-1 (u_i-u_j)
+    computed_max_pin_to_pin_resistance = max(solution[i,i] - solution[i,j] - solution[j,i] + solution[j,j] for (i,j) in itertools.combinations(range(2), 2))
+    print(computed_max_pin_to_pin_resistance)
+
+    if n == 2:
+        gold = 1
+
+    if n == 3:
+        gold = 3/2
+
+    if n == 4:
+        gold = 13/7
+
+    if n == 5:
+        gold = grid5()
+
+    if n == 6:
+        gold = grid6()
+
+
+    print(n, computed_max_pin_to_pin_resistance - gold)
+
+    assert abs(computed_max_pin_to_pin_resistance-gold) < 0.001
+
+def test_stamp2():
+    #    0   1   2   3   4
+    #  0 T---x---x---x---T
+    #    |   |   |   |   |
+    #  5 x---x---x---x---x
+    #    |   |   |   |   |
+    # 10 x---x---G---x---x
+    #    |   |   |   |   |
+    # 15 x---x---x---x---x
+    #    |   |   |   |   |
+    # 20 T---x---x---x---T
+    #
+
+    A, B, C, D = stamp([
+        (0,1,1), (1,2,1), (2,3,1), (3,4,1),
+        (5,6,1), (6,7,1), (7,8,1), (8,9,1),
+        (10,11,1), (11,12,1), (12,13,1), (13,14,1),
+        (15,16,1), (16,17,1), (17,18,1), (18,19,1),
+        (20,21,1), (21,22,1), (22,23,1), (23,24,1),
+        (0,5,1), (1,6,1), (2,7,1), (3,8,1), (4,9,1),
+        (5,10,1), (6,11,1), (7,12,1), (8,13,1), (9,14,1),
+        (10,15,1), (11,16,1), (12,17,1), (13,18,1), (14,19,1),
+        (15,20,1), (16,21,1), (17,22,1), (18,23,1), (19,24,1)
+    ], 12, set([0,4,20,24]))
+    # print(f'A: {A} {A.shape}')
+    # print(f'B: {B} {B.shape}')
+    # print(f'C: {C} {C.shape}')
+    # print(f'D: {D} {D.shape}')
+
+    solution = solve(A, B, C, D, np.eye(4))
+    print(solution)
+
+    # S^-1 (u_i-u_j)
+    print(max(solution[i,i] - solution[i,j] - solution[j,i] + solution[j,j] for (i,j) in itertools.combinations(range(4), 2)))
+
+
+
+def test_stamp3():
+    #    0   1
+    #  0 T---x
+    #    |   |
+    #  2 x---T
+
+    A, B, C, D = stamp([
+        (0,1,1),
+        (2,3,1),
+        (0,2,1),
+        (1,3,1)
+    ], 1, set([0,3]))
+    # print(f'A: {A} {A.shape}')
+    # print(f'B: {B} {B.shape}')
+    # print(f'C: {C} {C.shape}')
+    # print(f'D: {D} {D.shape}')
+
+    solution = solve(A, B, C, D, np.eye(2))
+    print(solution)
+
+    # S^-1 (u_i-u_j)
+    print(max(solution[i,i] - solution[i,j] - solution[j,i] + solution[j,j] for (i,j) in itertools.combinations(range(2), 2)))
+
+
+def test_stamp4():
+    #    0   1
+    #  0 T---x
+    #    |   |
+    #  2 x   x
+    #    |   |
+    #  4 x---T
+
+    A, B, C, D = stamp([
+        (0,1,1),
+        (2,3,1),
+        (4,5,1),
+        (0,2,1),
+        (1,3,1),
+        (2,4,1),
+        (3,5,1)
+    ], 1, set([0,5]))
+    # print(f'A: {A} {A.shape}')
+    # print(f'B: {B} {B.shape}')
+    # print(f'C: {C} {C.shape}')
+    # print(f'D: {D} {D.shape}')
+
+    solution = solve(A, B, C, D, np.eye(2))
+    print(solution)
+
+    # S^-1 (u_i-u_j)
+    print(max(solution[i,i] - solution[i,j] - solution[j,i] + solution[j,j] for (i,j) in itertools.combinations(range(2), 2)))
+
+
+
+
+def power_method(B, x):
+
+    n = B.shape[0]
+
+    def normalize(x):
+        #fac = np.abs(x).max()
+        fac = np.sqrt(x.dot(x))
+        x_n = x / fac
+        return fac, x_n
+
+    for i in range(8):
+        x = B.dot(x)
+        lambda_1, x = normalize(x)
+        print(lambda_1, x)
+
+    return lambda_1, x
+
+
+def max_point_to_point_resistance(T):
+    n = T.shape[0]
+
+    factors, pivot = la.lu_factor(T)
+    lu, d, perm = la.ldl(T)
+
+    print('factors, pivot:')
+    print(factors, pivot)
+    print('lu, d, perm:')
+    print(lu, d, perm)
+
+    print('c, lower:')
+    c, lower = la.cho_factor(T)
+    print(c, lower)
+
+    new_factors = factors[2:, 2:]
+    new_pivot = np.array([0, 1, 2], dtype=np.int32)
+    print( factors[2:, 2:].shape)
+    b = np.zeros(3)
+    XX = []
+    
+    for i in range(3):
+        b[i] = 1
+        XX.append(la.lu_solve((new_factors, new_pivot), b))
+        b[i] = 0
+
+    print('XX', XX)
+
+
+
+    X = []
+    X2 = []
+    b = np.zeros(n)
+    for i in range(n):
+        b[i] = 1
+        X.append(la.lu_solve( (factors, pivot), b))
+        X2.append(la.cho_solve( (c, lower), b))
+        b[i] = 0
+
+    print(b, X)
+    print(b, X2)
+
+    B = la.inv(T)
+    print('B')
+    print(B)
+
+    return
+
+    result = la.eig(B)
+    print('Eigensystem:')
+    print(result)
+
+    x = np.array([1,1,1,1,1],dtype=np.float64)
+    power_method(B, x)
+
+    for i, j in itertools.combinations(range(n+1), 2):
+        x = np.zeros(shape=(n,))
+        if i > 0:
+            x[i-1] -= 1
+        if j > 0:
+            x[j-1] += 1
+        print(i, j, x, x.dot(B.dot(x)))
+
+
+def test_A():
+
+    T = np.array([[1, -1, 0, 0, 0], [-1, 2, -1, 0, 0], [0, -1, 2, 0, 0], [0, 0, 0, 2, -1], [0, 0, 0, -1, 1]])
+
+    max_point_to_point_resistance(T)
+
+def test_B():
+
+    T = np.array([[1, -1, 0, 0, 0], [-1, 2, -1, 0, 0], [0, -1, 2, -1, 0], [0, 0, -1, 2, -1], [0, 0, 0, -1, 2]])
+
+    max_point_to_point_resistance(T)
+
+def test_C():
+  
+    # G   2   0   3   1   4
+    # x---T---x---T---x---T
+
+
+    #               0  1  2  3  4
+    T = np.array([[ 2, 0,-1,-1, 0],
+                  [ 0, 2, 0,-1,-1],
+                  [-1, 0, 2, 0, 0],
+                  [-1,-1, 0, 2, 0],
+                  [ 0,-1, 0, 0, 1]])
+
+    max_point_to_point_resistance(T)
+
