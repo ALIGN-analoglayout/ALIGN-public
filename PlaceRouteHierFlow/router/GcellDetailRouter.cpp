566e681ffbdfe60dae15b050d14aacf81b4665cd
soneryaldiz <56893713+soneryaldiz@users.noreply.github.com>
2023-01-13 08:09:21 -0800

    Failing Unit Router Tests (#1236)
    * failing and suboptimal router tests
    
    * rename to prevent conflict
    
    * duplicate metal spacing
    
    * more duplicate metal spacing
    
    * routing center track
    
    * revised failing test
    
    * include source pin distance to grid center
    
    * remove source pin distance to grid center
    
    * skip net center when all pins are floating
    
    Co-authored-by: Yishuang Lin <854768750@qq.com>

--- PlaceRouteHierFlow/router/GcellDetailRouter.cpp
+++ PlaceRouteHierFlow/router/GcellDetailRouter.cpp
@@ -302,10 +302,23 @@ void GcellDetailRouter::SortPinsOrder() {
   //order by the manhattan distance to LL
   for (unsigned int i = 0; i < Nets.size(); i++) {
     if (Nets[i].connected.size() == 0) continue;
-    //order by the manhattan distance to LL
+    //calculate net pin center
+    int center_x_count = 0, center_y_count = 0;
+    for (unsigned int j = 0; j < Nets[i].connected.size();j++){
+      if (Nets[i].connected[j].type == RouterDB::TERMINAL || Blocks[Nets[i].connected[j].iter2].pins[Nets[i].connected[j].iter].pinContacts.size() == 0)
+        continue;
+      Nets[i].center_x += Blocks[Nets[i].connected[j].iter2].pins[Nets[i].connected[j].iter].pinContacts[0].placedCenter.x;
+      Nets[i].center_y += Blocks[Nets[i].connected[j].iter2].pins[Nets[i].connected[j].iter].pinContacts[0].placedCenter.y;
+      center_x_count++;
+      center_y_count++;
+    }
+    if (center_x_count) Nets[i].center_x /= center_x_count;
+    if (center_y_count) Nets[i].center_y /= center_y_count;
+
+    // order by the manhattan distance to LL
     std::sort(Nets[i].connected.begin(), Nets[i].connected.end(), [&](RouterDB::connectNode &a, RouterDB::connectNode &b) {
       if (a.type == RouterDB::TERMINAL || b.type == RouterDB::TERMINAL || Blocks[a.iter2].pins[a.iter].pinContacts.size() == 0 ||
-              Blocks[b.iter2].pins[b.iter].pinContacts.size() == 0)
+          Blocks[b.iter2].pins[b.iter].pinContacts.size() == 0)
         return true;
       return Blocks[a.iter2].pins[a.iter].pinContacts[0].placedCenter.x + Blocks[a.iter2].pins[a.iter].pinContacts[0].placedCenter.y <
              Blocks[b.iter2].pins[b.iter].pinContacts[0].placedCenter.x + Blocks[b.iter2].pins[b.iter].pinContacts[0].placedCenter.y;
@@ -1012,7 +1025,9 @@ Grid GcellDetailRouter::Generate_Grid_Net(int i) {
   // Grid grid(Gcell, global_path, drc_info, chip_LL, chip_UR, temp_lowest_metal, temp_highest_metal, grid_scale);
   Grid grid(Gcell, global_path, drc_info, chip_LL, chip_UR, temp_lowest_metal, temp_highest_metal, grid_scale);
   grid.Full_Connected_Vertex();
-
+  grid.center_x = Nets[i].center_x;
+  grid.center_y = Nets[i].center_y;
+  
   return grid;
 };
 
@@ -4367,8 +4382,8 @@ void GcellDetailRouter::InactivateRect2GridPoints(int mIdx, int LLx, int LLy, in
       if (mIdx != obs_l) {
         int nexlayer_unit = cross_layer_drc_info.Metal_info.at(mIdx - 1).grid_unit_x;
 
-        int newLLx = LLx - drc_info.Metal_info.at(mIdx).dist_ee - enclose_length;
-        int newURx = URx + drc_info.Metal_info.at(mIdx).dist_ee + enclose_length;
+        int newLLx = LLx  - enclose_length;//- drc_info.Metal_info.at(mIdx).dist_ee
+        int newURx = URx + enclose_length;//+ drc_info.Metal_info.at(mIdx).dist_ee ;
         int boundX = ceil((double)newLLx / nexlayer_unit) * nexlayer_unit;
         if (boundX > newURx) {
           newLLx = floor((double)newLLx / nexlayer_unit) * nexlayer_unit;
@@ -4386,8 +4401,8 @@ void GcellDetailRouter::InactivateRect2GridPoints(int mIdx, int LLx, int LLy, in
       if (mIdx != obs_h) {
         int nexlayer_unit = cross_layer_drc_info.Metal_info.at(mIdx + 1).grid_unit_x;
 
-        int newLLx = LLx - drc_info.Metal_info.at(mIdx).dist_ee - enclose_length;
-        int newURx = URx + drc_info.Metal_info.at(mIdx).dist_ee + enclose_length;
+        int newLLx = LLx  - enclose_length;//- drc_info.Metal_info.at(mIdx).dist_ee;
+        int newURx = URx + enclose_length;  //+ drc_info.Metal_info.at(mIdx).dist_ee;
         int boundX = ceil((double)newLLx / nexlayer_unit) * nexlayer_unit;
         if (boundX > newURx) {
           newLLx = floor((double)newLLx / nexlayer_unit) * nexlayer_unit;
