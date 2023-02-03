4ed3fc5a371d8e2c23eb298e14193f10b18fc217
srini229 <srini229@umn.edu>
2023-02-03 12:28:04 +0530

    install.sh

---
+++ install.sh
@@ -0,0 +1,2 @@
+pip install 'networkx>=2.4' python-gdsii gdspy pyyaml pybind11 pydantic z3-solver mip more-itertools colorlog plotly numpy pandas werkzeug dash memory_profiler flatdict pytest pytest-cov pytest-xdist pytest-timeout pytest-cpp setuptools wheel pybind11 numpy scikit-build cmake ninja
+pip install -v --no-build-isolation -e . --no-deps --install-option='--build-type=Release'
