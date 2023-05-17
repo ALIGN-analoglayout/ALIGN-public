dc950123932d70e7508edfe2b2efa227a72c976b
srini229 <srini229@umn.edu>
2023-05-18 05:16:16 +0530

    rerun failing tests one more time in CI for random worker thread crashes

--- setup.py
+++ setup.py
@@ -97,6 +97,7 @@ setup(name='align',
               'pytest-cov',
               'pytest-xdist',
               'pytest-timeout',
+              'pytest-rerunfailures',
               'pytest-cpp'
           ]
       },
